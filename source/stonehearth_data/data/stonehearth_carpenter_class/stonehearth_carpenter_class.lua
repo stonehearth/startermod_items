588e2ba87bf2cacddf62a3e1d691a91ec9562139
Stephanie <stephanie@radiant-entertainment.com>
2013-06-10 14:26:00 -0700

    change api to classname

--- source/stonehearth_data/data/stonehearth_carpenter_class/stonehearth_carpenter_class.lua
+++ source/stonehearth_data/data/stonehearth_carpenter_class/stonehearth_carpenter_class.lua
@@ -2,10 +2,10 @@
    The carpenter api implements all the functionality that other files will need. 
 ]]
 
-local api = {}
+local carpenter_class = {}
 
-function api.promote(entity)   
+function carpenter_class.promote(entity)   
     radiant.entities.inject_into_entity(entity, 'mod://stonehearth_carpenter_class/class_info/')
 end
 
-return api
+return carpenter_class
