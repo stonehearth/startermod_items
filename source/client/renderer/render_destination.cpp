8fa5ab9df301b589f94f638970215e8090a5a134
Tony Cannon <tony@radiant-entertainment.com>
2013-06-10 11:07:18 -0700

    checkpoint

---
+++ source/client/renderer/render_destination.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+#include "render_entity.h"
+#include "renderer.h"
+#include "render_destination.h"
+#include "om/components/stockpile_designation.h"
+
+using namespace ::radiant;
+using namespace ::radiant::client;
+
+RenderDestination::RenderDestination(const RenderEntity& entity, om::DestinationPtr stockpile) :
+   entity_(entity)
+{
+   H3DNode parent = entity_.GetNode();
+   std::string parentName = h3dGetNodeParamStr(parent, H3DNodeParams::NameStr);
+
+   auto create_debug_tracker = [=](std::string regionName, H3DNode& shape, om::RegionPtr region, math3d::color4 const& color) {
+      shape = h3dRadiantAddDebugShapes(parent, (parentName + regionName, std::string(" destination debug shape")).c_str());
+      auto update = std::bind(&RenderDestination::UpdateShape, this, std::placeholders::_1, shape, color);
+      guards_ += region->Trace("rendering destination debug region", update);
+      update(**region);
+   };
+
+   create_debug_tracker("region", regionDebugShape_, stockpile->GetRegion(), math3d::color4(0, 128, 255, 128));
+   create_debug_tracker("reserved", reservedDebugShape_, stockpile->GetReserved(), math3d::color4(255, 128, 0, 128));
+   create_debug_tracker("adjacent", adjacentDebugShape_, stockpile->GetAdjacent(), math3d::color4(255, 255, 255, 128));
+}
+
+RenderDestination::~RenderDestination()
+{
+}
+
+void RenderDestination::UpdateShape(csg::Region3 const& region, H3DNode shape, math3d::color4 const& color)
+{
+   if (entity_.ShowDebugRegions()) {
+      h3dRadiantClearDebugShape(shape);
+      h3dRadiantAddDebugRegion(shape, region, color);
+      h3dRadiantCommitDebugShape(shape);
+   }
+}
---
+++ source/client/renderer/render_destination.h
@@ -0,0 +1,34 @@
+#ifndef _RADIANT_CLIENT_RENDER_DESTINATION_H
+#define _RADIANT_CLIENT_RENDER_DESTINATION_H
+
+#include <map>
+#include "om/om.h"
+#include "dm/dm.h"
+#include "types.h"
+#include "render_component.h"
+#include "horde3d/extensions/stockpile/stockpile_node.h"
+
+BEGIN_RADIANT_CLIENT_NAMESPACE
+
+class Renderer;
+
+class RenderDestination : public RenderComponent {
+   public:
+      RenderDestination(const RenderEntity& entity, om::DestinationPtr stockpile);
+      ~RenderDestination();
+
+   private:
+      void UpdateShape(csg::Region3 const& region, H3DNode shape, math3d::color4 const& color);
+
+   private:
+      const RenderEntity&        entity_;
+      H3DNode                    regionDebugShape_;
+      H3DNode                    reservedDebugShape_;
+      H3DNode                    adjacentDebugShape_;
+      dm::GuardSet               guards_;
+
+};
+
+END_RADIANT_CLIENT_NAMESPACE
+
+#endif //  _RADIANT_CLIENT_RENDER_DESTINATION_H
