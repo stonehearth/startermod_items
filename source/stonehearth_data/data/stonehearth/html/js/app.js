4518ad577e281d306fb10f61f2dcbc4b284fc7f7
Tom Cannon <tom@radiant-entertainment.com>
2013-08-09 15:09:32 -0700

    Add dynamic loading of mod js, css, and html

--- source/stonehearth_data/data/stonehearth/html/js/app.js
+++ source/stonehearth_data/data/stonehearth/html/js/app.js
@@ -5,7 +5,7 @@ App = Ember.Application.createWithMixins({
   	init: function() {
   		this.deferReadiness();
   		this._super();
-  		this._loadTemplates();
+      this._loadModules();
   	},
 
   	ready: function() {
@@ -14,18 +14,106 @@ App = Ember.Application.createWithMixins({
   	},
 
 
-  	_loadTemplates: function() {
+    _loadModules: function() {
+      var self = this;
+
+      var deferreds = [];
+
+      $.ajax( {
+        url: '/stonehearth/html/init.json'
+      }).done( function(data) {
+        console.log(data);
+
+        deferreds = deferreds.concat(self._loadJavaScripts(data.modules));
+        deferreds = deferreds.concat(self._loadCsss(data.modules));
+        deferreds = deferreds.concat(self._loadTemplates(data.modules));
+
+        // when all the tempalates are loading, contune loading the app
+        $.when.apply($, deferreds).then(function(data) {
+          self.advanceReadiness();
+        });
+      });
+    },
+
+    _loadJavaScripts: function(modules) {
+      var deferreds = [];
+      var self = this;
+
+      $.each( modules, function( k, v ) {
+        var url = '/' + k + '/html/js/' + k + ".js";
+        deferreds.push(self._loadJavaScript(url));
+      });
+
+      return deferreds;
+    },
+
+    _loadJavaScript: function(url) {
+      console.log('loading js ' + url);
+
+      var deferred = $.Deferred();
+
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+      script.onload = function () {
+        deferred.resolve();
+      };
+      script.onerror = function () {
+         throw ('failed to load script at ' + url);
+      };
+      document.getElementsByTagName('head')[0].appendChild(script);
+
+      return deferred;
+    },
+
+    _loadCsss: function(modules) {
+      var deferred = $.Deferred();
+
+      // collect the css urls from all the modules
+      var urls = [];
+      $.each(modules, function( k, v ) {
+        var url = '/' + k + '/html/css/' + k + ".less";
+        urls.push(url);
+      });
+
+      // grab the root stylesheet
+      $.get('css/style.less', function(data) {
+        // append imports for all the module style sheets
+        $.each(urls, function(i, url) {
+          data = data + '\n' + '@import "' + url + '";';
+        })
+
+        // parse everything all at once and add the css to the page
+        var parser = new(less.Parser)({
+          paths: ['css/']
+        }).parse(data, function (err, t) {
+            if (err) {
+              console.error(err) 
+            }
+
+            css = t.toCSS();
+            $("<style/>").html(css).appendTo("body");
+
+            // css has been added. Resolve the deferred
+            deferred.resolve();
+        });
+      })
+
+      return deferred;
+    },
+
+  	_loadTemplates: function(modules) {
   		var self = this;
 
-  		var templateUrls = [
+      var templateUrls = [
          'stonehearth/stonehearth.html',
          'unitframe/unitframe.html',
-         'objectbrowser/objectbrowser.html',
-         '/stonehearth_title_screen/html/stonehearth_title_screen.html',
-         '/stonehearth_crafter/html/stonehearth_crafter.html',
-         '/stonehearth_calendar/html/stonehearth_calendar.html',
-         '/stonehearth_main_actionbar/html/stonehearth_main_actionbar.html'
-  			];
+         'objectbrowser/objectbrowser.html'
+        ];
+
+      $.each( modules, function( k, v ) {
+        templateUrls.push('/' + k + '/html/' + k + ".html");
+      });
 
   		var deferreds = [];
 
@@ -34,24 +122,19 @@ App = Ember.Application.createWithMixins({
   			deferreds.push(self._loadTemplate(templateUrl));
   		});
 
-  		// when all the tempalates are loading, contune loading the app
-  		$.when.apply($, deferreds).then(function(data) {
-			self.advanceReadiness();
-  		});
+      return deferreds;
   	},
 
   	_loadTemplate: function(url) {
-		return $.ajax({
-	      	url: url,
-	      	dataType: 'text',
-	      	success: function (response) {
-				$(response).filter('script[type="text/x-handlebars"]').each(function() {
-			    	templateName = $(this).attr('data-template-name');
+		  return $.ajax({
+        url: url,
+      	dataType: 'text',
+      	success: function (response) {
+  				$(response).filter('script[type="text/x-handlebars"]').each(function() {
+	 		    	templateName = $(this).attr('data-template-name');
 			    	Ember.TEMPLATES[templateName] = Ember.Handlebars.compile($(this).html());
 			    	console.log('  loaded template:' + templateName)
 			  	});
-
-
 	    	}
 	  	});
   	},
