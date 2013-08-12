d6482947f60c6304a26bcd96ba36c172de662de8
Stephanie <stephanie@radiant-entertainment.com>
2013-08-12 15:51:05 -0700

    Added a file that was accidentally deleted, fixed an event bug

--- source/stonehearth_data/data/radiant/modules/effects/trigger_effect.lua
+++ source/stonehearth_data/data/radiant/modules/effects/trigger_effect.lua
@@ -1,10 +1,11 @@
 local Effect = require 'radiant.modules.effects.effect'
 local TriggerEffect = class(Effect)
 
-function TriggerEffect:__init(start_time, handler, info, effect)
+function TriggerEffect:__init(start_time, handler, info, effect, entity)
    self[Effect]:__init(info)
    --May be redundant with above?
    self._info = info
+   self._entity = entity
 
    self._effect = effect
    local proposed_start = self:_get_start_time()
@@ -14,7 +15,7 @@ end
 
 function TriggerEffect:update(now)
    if self._trigger_time and self._trigger_time <= now then
-      radiant.events.broadcast_msg('radiant.animation.on_trigger', self._info, self._effect)
+      radiant.events.broadcast_msg('radiant.animation.on_trigger', self._info, self._effect, self._entity)
       self._trigger_time = nil
    end
    return self._trigger_time ~= nil
--- source/stonehearth_data/data/stonehearth_tests/promotion_test.lua
+++ source/stonehearth_data/data/stonehearth_tests/promotion_test.lua
@@ -24,31 +24,17 @@ function PromoteTest:__init()
    workshop_component:set_saw_entity(saw)
 
    local tree = self:place_tree(-12, 0)
+   local tree2 = self:place_tree(-12, 12)
 
    saw:add_component('unit_info'):set_faction(faction)
    saw:get_component('stonehearth_classes:profession_info'):set_workshop(workshop_component)
 
-   --self:at(5000, function()
-   --   radiant.events.broadcast_msg('stonehearth_item.promote_citizen', {talisman = saw, target_person = 14})
-   --   end)
+   --Human TODO
+   --Test that the dude can chop down the tree
+   --Test that the dude can be promoted to carpenter
+   --Test that he can craft a sword
+   --Test that he can no longer chop down the tree
 
-   --initialize the outbox
-   --user places both workshop and outbox
-   --TODO: make a private stockpile
-   --[[
-   local outbox_entity = radiant.entities.create_entity('/stonehearth_inventory/entities/stockpile')
-   local outbox_location = RadiantIPoint3(-8, 1, -12)
-   radiant.terrain.place_entity(outbox_entity, outbox_location)
-   local outbox_component = outbox_entity:get_component('radiant:stockpile')
-   outbox_component:set_size({3, 3})
-   outbox_entity:get_component('unit_info'):set_faction(faction)
-   workshop_component:set_outbox(outbox_entity)
-   ]]
-   -- end TODO
-
-   -- put some items in the world
-   --self:place_item_cluster('/stonehearth_trees/entities/oak_tree/oak_log', -10, 10, 3, 3)
-   --self:place_item_cluster('/stonehearth_items/cloth_bolt', -7, 10, 2, 2)
 end
 
 return PromoteTest
