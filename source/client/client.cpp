8892cb576cbaf14d234f433eb2cb34e4fe87c1c1
Chris Klochek <klochek@radiant-entertainment.com>
2013-09-18 19:32:06 -0700

    Revert "Basic work on ssao."
    This reverts commit 3d1d17df10ddef8d905beac3a0dc2dd78642ab95.

--- source/client/client.cpp
+++ source/client/client.cpp
@@ -84,7 +84,7 @@ void Client::run()
    octtree_ = std::unique_ptr<Physics::OctTree>(new Physics::OctTree());
       
    Renderer& renderer = Renderer::GetInstance();
-   renderer.SetCurrentPipeline("pipelines/deferred_pipeline_static.xml");
+   //renderer.SetCurrentPipeline("pipelines/deferred_pipeline_static.xml");
    //renderer.SetCurrentPipeline("pipelines/forward.pipeline.xml");
 
    Horde3D::Modules::log().SetNotifyErrorCb([=](om::ErrorBrowser::Record const& r) {
