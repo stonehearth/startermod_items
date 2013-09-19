87bcf79711fd4e7250e17edb4700578d5b847752
Tony Cannon <tony@radiant-entertainment.com>
2013-09-19 10:10:02 -0700

    don't require msbuild in the path!

--- make/settings.mk
+++ make/settings.mk
@@ -14,7 +14,7 @@ endif
 
 # default commands and such
 7ZA=7za
-MSBUILD=msbuild.exe -nologo -maxcpucount -p:PlatformToolset=v110 
+MSBUILD='/c/windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe' -nologo -maxcpucount -p:PlatformToolset=v110 
 
 CMAKE_C_FLAG_OVERRIDE=-DCMAKE_USER_MAKE_RULES_OVERRIDE=${MAKE_ROOT_DOS}/c_flag_overrides.cmake
 CMAKE_CXX_FLAG_OVERRIDE=-DCMAKE_USER_MAKE_RULES_OVERRIDE_CXX=${MAKE_ROOT_DOS}/cxx_flag_overrides.cmake
