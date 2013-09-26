61f6cf9b2654a7d40f4c8d4939bf1bbe65d29d0a
Tom Cannon <tom@radiant-entertainment.com>
2013-09-26 12:07:59 -0700

    Fix race when leasing beds.

--- source/stonehearth_data/data/stonehearth/lib/ai/actions/sleep_action.lua
+++ source/stonehearth_data/data/stonehearth/lib/ai/actions/sleep_action.lua
@@ -16,6 +16,8 @@ function SleepAction:__init(ai, entity)
    self._ai = ai
 
    self._sleepiness = 0
+   self._looking_for_a_bed = false
+   self._sleeping = false
 
    radiant.events.listen('radiant.events.calendar.hourly', self)
 end
@@ -29,6 +31,24 @@ end
    on the ground (not in a bed) from exhaustion.
 --]]
 SleepAction['radiant.events.calendar.hourly'] = function(self, calendar)
+   -- if it's after 8pm or before 6am and we're not already asleep got find a bed.
+   if (calendar.hour > 20 or calendar.hour < 6) then
+      if not self._looking_for_a_bed and not self._sleeping then
+         self:start_looking_for_bed()
+      end
+   else 
+      self:stop_looking_for_bed()
+      self._sleeping = false
+      self._ai:set_action_priority(self, 0)
+   end
+end
+
+function SleepAction:start_looking_for_bed()
+   assert(not self._pathfinder)
+   assert(not self._looking_for_a_bed)
+   
+   self._ai:set_action_priority(self, 0)
+   self._looking_for_a_bed = true;
 
    --[[
       Pathfinder callback. When it's time to go to sleep, the pathfinder
@@ -36,24 +56,24 @@ SleepAction['radiant.events.calendar.hourly'] = function(self, calendar)
       jack up the action's priority so the dude will go find his bed
       and go to sleep.
    --]]
-   local solved_cb = function(bed, path)
+   local found_bed_cb = function(bed, path)
       self._bed = bed
       self._path_to_bed = path
       self._ai:set_action_priority(self, 100)
    end
 
-   -- if it's after 8pm or before 6am and we're not already asleep got find a bed.
-   if calendar.hour > 20 or calendar.hour < 6 then
-      if not self._pathfinder and not self._sleeping then
-         self:_start_looking_for_bed(solved_cb)
-      end
-   else 
-      -- it's not time to go to sleep. Stop looking for a bed
-      if self._pathfinder then
-         self:stop()
-      end
-      self._ai:set_action_priority(self, 0)
+   if not self._pathfinder then
+      self:find_a_bed(found_bed_cb) 
    end
+end
+
+function SleepAction:stop_looking_for_bed()
+   -- tell the pathfinder to stop searching in the background
+   if self._pathfinder then
+      self._pathfinder:stop()
+      self._pathfinder = nil
+   end
+   self._looking_for_a_bed = false;
 end
 
 --[[
@@ -64,7 +84,7 @@ end
    only look for unleased beds to sleep in. Once an actor has leased a bed, he will
    always go back to that bed when it's time to sleep.
 --]]
-function SleepAction:_start_looking_for_bed(result_cb)
+function SleepAction:find_a_bed(result_cb)
    assert(not self._pathfinder)
 
    -- find a bed
@@ -84,6 +104,7 @@ function SleepAction:_start_looking_for_bed(result_cb)
       local solved_cb = function(path)
          result_cb(bed, path)
       end
+      radiant.log.info('%s creating pathfinder to my bed', tostring(self._entity));
       self._pathfinder = radiant.pathfinder.find_path_to_entity(desc, self._entity, bed, solved_cb)
    else
       -- find a bed and lease it
@@ -106,6 +127,7 @@ function SleepAction:_start_looking_for_bed(result_cb)
       end
 
       -- go find the path to the bed
+      radiant.log.info('%s creating pathfinder to find a bed', tostring(self._entity));
       local desc = string.format('finding bed for %s', tostring(self._entity))
       self._pathfinder = radiant.pathfinder.find_path_to_closest_entity(desc, self._entity, solved_cb, filter_fn)
    end
@@ -125,38 +147,37 @@ function SleepAction:run(ai, entity)
    assert(self._pathfinder)   
    assert(self._path_to_bed)
 
-   -- tell the pathfinder to stop searching in the background
-   self._pathfinder:stop()
-   self._pathfinder = nil
+   -- we found a bed, so stop looking for one
+   self:stop_looking_for_bed()
 
    -- renew our lease on the bed.
    local bed_component = self._bed:get_component('stonehearth_sleep_system:bed')
    local bed_owner = bed_component:get_owner()
    if bed_owner and bed_owner:get_id() ~= entity:get_id() then
-      -- xxx. if there's another lease on the bed that doesn't belong to us,
+      -- There's another lease on the bed that doesn't belong to us,
       -- we need to bail.  this can happen if two people both try to sleep in
       -- a bed simultaneously
-      assert(false)
+      --
+      -- So just start looking for another bed
+      self:start_looking_for_bed()
+      return
    end
+   
+   radiant.log.info('leasing %s to %s', tostring(self._bed), tostring(self._entity))
    bed_component:lease_bed_to(entity)
    entity:get_component('stonehearth_sleep_system:bed_lease'):set_bed(self._bed)
 
    -- go to sleep!
-   self._sleeping = true
+   self._sleeping = true;
    ai:execute('stonehearth.activities.sleep_in_bed', self._bed, self._path_to_bed)
-   self._sleeping = false
+   
 end
 
 --[[
    If the action is stopped, stop the pathfinder
 --]]
 function SleepAction:stop()
-   if self._pathfinder then
-      self._pathfinder:stop()
-      self._pathfinder = nil
-   end
-   
-   self._sleeping = false
+   self:stop_looking_for_bed();
 end
 
 return SleepAction
