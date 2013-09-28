888043ea433d19d9c818fb8ebb2a257ce2d8d567
Tom Cannon <tom@radiant-entertainment.com>
2013-09-27 17:15:08 -0700

    bug fixes.

--- source/stonehearth_data/data/stonehearth/api.lua
+++ source/stonehearth_data/data/stonehearth/api.lua
@@ -4,8 +4,10 @@ local api = {}
 function api.get_service(name)
    -- xxx: put this in a pcall so we can return a friendly
    -- error when an invalid name is passed in
-
-   return require(string.format('services.%s.%s_service', name, name))
+   
+   local service_name = string.format('services.%s.%s_service', name, name)
+   local service = require(service_name)
+   return service
 end
 
 return api
