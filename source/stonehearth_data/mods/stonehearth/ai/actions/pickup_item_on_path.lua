86911ff08ce1ce343be239cf13e37bbdbe268d18
Stephanie <stephanie@radiant-entertainment.com>
2013-10-02 12:23:37 -0700

    Cleanup and todos

--- source/stonehearth_data/mods/stonehearth/ai/actions/pickup_item_on_path.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/pickup_item_on_path.lua
@@ -5,14 +5,14 @@ PickupItemOnPath.priority = 5
 
 function PickupItemOnPath:run(ai, entity, path)
    local item = path:get_destination()
+   --TODO: if the destination item changes, we should just abort from inside the pf
    ai:execute('stonehearth.follow_path', path)
 
    --There's always a chance the item may no longer be there. Fail gracefully
    --TODO: Put up a confused animation
    if not item then
       local name = entity:get_component('unit_info'):get_display_name()
-      local item_name = item:get_component('unit_info'):get_display_name()
-      radiant.log.info('Worker %s: Huh? Where is %s?', name, item_name)
+      radiant.log.info('Worker %s: Huh? Where is the thing I was looking for?', name)
       ai:abort()
    end
 
