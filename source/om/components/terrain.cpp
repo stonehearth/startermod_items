stonehearth/startermod_items

commit 828e8e2ae591a0d6f836362cb7b7df791ff1184a

Fix O(n) search in Terrain::GetZone()

source/om/components/terrain.cpp
@@ -15,6 +15,7 @@ void Terrain::InitializeRecordFields()
    Component::InitializeRecordFields();
    AddRecordField("zones", zones_);
    AddRecordField("zone_size", zone_size_);
+   AddRecordField("zone_offset", zone_offset_);
 }
 
 int Terrain::GetZoneSize()
@@ -27,10 +28,16 @@ void Terrain::SetZoneSize(int zone_size)
    zone_size_ = zone_size;
 }
 
-void Terrain::AddZone(csg::Point3 const& zone_offset, BoxedRegion3Ptr region3)
+// translation required to transform a zone into world space
+void Terrain::SetZoneOffset(csg::Point3 offset)
+{
+   zone_offset_ = offset;
+}
+
+void Terrain::AddZone(csg::Point3 const& region_origin, BoxedRegion3Ptr region3)
 {
    // zones are stored using the location of their 0, 0 coordinate in the world
-   zones_[zone_offset] = region3;
+   zones_[region_origin] = region3;
 }
 
 void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
@@ -38,9 +45,9 @@ void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
    auto entity = e.lock();
    if (entity) {
       int max_y = INT_MIN;
-      csg::Point3 zone_offset;
-      BoxedRegion3Ptr region_ptr = GetZone(location, zone_offset);
-      csg::Point3 const& regionLocalPt = location - zone_offset;
+      csg::Point3 region_origin;
+      BoxedRegion3Ptr region_ptr = GetZone(location, region_origin);
+      csg::Point3 const& region_local_pt = location - region_origin;
 
       if (!region_ptr) {
          throw std::invalid_argument(BUILD_STRING("point " << location << " is not in world"));
@@ -49,8 +56,8 @@ void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
 
       // O(n) search - consider optimizing
       for (csg::Cube3 const& cube : region) {
-         if (regionLocalPt.x >= cube.GetMin().x && regionLocalPt.x < cube.GetMax().x &&
-             regionLocalPt.z >= cube.GetMin().z && regionLocalPt.z < cube.GetMax().z) {
+         if (region_local_pt.x >= cube.GetMin().x && region_local_pt.x < cube.GetMax().x &&
+             region_local_pt.z >= cube.GetMin().z && region_local_pt.z < cube.GetMax().z) {
             max_y = std::max(cube.GetMax().y, max_y);
          }
       }
@@ -63,15 +70,35 @@ void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
    }
 }
 
-BoxedRegion3Ptr Terrain::GetZone(csg::Point3 const& location, csg::Point3& zone_offset)
+BoxedRegion3Ptr Terrain::GetZone(csg::Point3 const& pt, csg::Point3& region_origin)
 {
-   // O(n) search - consider optimizing
-   for (auto& entry : zones_) {
-      zone_offset = entry.first;
-      if (location.x >= zone_offset.x && location.x < zone_offset.x + zone_size_ &&
-          location.z >= zone_offset.z && location.z < zone_offset.z + zone_size_) {
-         return entry.second;
+   // transform from world coordinates to zone coordinates
+   csg::Point3 const& pt_in_zone_space = pt - zone_offset_;
+
+   // determine the origin of the region containing the point
+   csg::Point3 region_origin_in_zone_space;
+   for (int i = 0; i < 3; i++) {
+      int c = pt_in_zone_space[i];
+      if (c >= 0) {
+         region_origin_in_zone_space[i] = c / zone_size_ * zone_size_;
+      } else {
+         // floor towards -infinity, not towards origin
+         region_origin_in_zone_space[i] = (c-(zone_size_-1)) / zone_size_ * zone_size_;
       }
+   }
+
+   // transform back to world coordinates
+   csg::Point3 region_origin_in_world_space;
+   region_origin_in_world_space = region_origin_in_zone_space + zone_offset_;
+
+   // get the region from the ZoneMap
+   auto entry = zones_.find(region_origin_in_world_space);
+
+   if (entry != zones_.end()) {
+      region_origin = entry->first;
+      ASSERT(pt.x >= region_origin.x && pt.x < region_origin.x + zone_size_ &&
+             pt.z >= region_origin.z && pt.z < region_origin.z + zone_size_);
+      return entry->second;
    }
    return nullptr;
 }

source/om/components/terrain.h
@@ -34,6 +34,7 @@ public:
 
    int GetZoneSize();
    void SetZoneSize(int zone_size);
+   void SetZoneOffset(csg::Point3 offset);
    void AddZone(csg::Point3 const& zone_offset, BoxedRegion3Ptr region3);
    void PlaceEntity(EntityRef e, const csg::Point3& location);
    void CreateNew();
@@ -43,6 +44,7 @@ public:
 
 private:
    dm::Boxed<int> zone_size_;
+   dm::Boxed<csg::Point3> zone_offset_;
    ZoneMap zones_;
    void InitializeRecordFields() override;
 };

source/stonehearth_data/mods/stonehearth/services/world_generation/height_map_renderer.lua
@@ -15,6 +15,9 @@ function HeightMapRenderer:__init(zone_size)
    self.zone_size = zone_size
    self._terrain = radiant._root_entity:add_component('terrain')
    self._terrain:set_zone_size(zone_size)
+
+   -- eww, gross - will go away with new terrain chunking code
+   self._terrain:set_zone_offset(Point3(-self.zone_size/2, 0, -self.zone_size/2));
 end
 
 -- delegate to C++ to "tesselate" heightmap into rectangles

source/om/lua/lua_terrain_component.cpp
@@ -14,6 +14,7 @@ scope LuaTerrainComponent::RegisterLuaTypes(lua_State* L)
          .def(tostring(self))
          .def("create_new",            &Terrain::CreateNew)
          .def("set_zone_size",         &Terrain::SetZoneSize)
+         .def("set_zone_offset",       &Terrain::SetZoneOffset)
          .def("add_zone",              &Terrain::AddZone)
          .def("place_entity",          &Terrain::PlaceEntity)
          .enum_("TerrainTypes")
@@ -21,7 +22,7 @@ scope LuaTerrainComponent::RegisterLuaTypes(lua_State* L)
             value("BEDROCK",              Terrain::Bedrock),
             value("TOPSOIL",              Terrain::Topsoil),
             value("TOPSOIL_DETAIL",       Terrain::TopsoilDetail),
-            value("FOOTHILLS",                Terrain::Foothills),
+            value("FOOTHILLS",            Terrain::Foothills),
             value("PLAINS",               Terrain::Plains),
             value("DARK_WOOD",            Terrain::DarkWood),
             value("DIRT_PATH",            Terrain::DirtPath)

commit 44678f9bc04e5a526d2a1f95ea1bf9071203e5e4

Backout change 828e8e2, which broke placing items on the terrain

source/om/components/terrain.cpp
@@ -15,7 +15,6 @@ void Terrain::InitializeRecordFields()
    Component::InitializeRecordFields();
    AddRecordField("zones", zones_);
    AddRecordField("zone_size", zone_size_);
-   AddRecordField("zone_offset", zone_offset_);
 }
 
 int Terrain::GetZoneSize()
@@ -28,16 +27,10 @@ void Terrain::SetZoneSize(int zone_size)
    zone_size_ = zone_size;
 }
 
-// translation required to transform a zone into world space
-void Terrain::SetZoneOffset(csg::Point3 offset)
-{
-   zone_offset_ = offset;
-}
-
-void Terrain::AddZone(csg::Point3 const& region_origin, BoxedRegion3Ptr region3)
+void Terrain::AddZone(csg::Point3 const& zone_offset, BoxedRegion3Ptr region3)
 {
    // zones are stored using the location of their 0, 0 coordinate in the world
-   zones_[region_origin] = region3;
+   zones_[zone_offset] = region3;
 }
 
 void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
@@ -45,9 +38,9 @@ void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
    auto entity = e.lock();
    if (entity) {
       int max_y = INT_MIN;
-      csg::Point3 region_origin;
-      BoxedRegion3Ptr region_ptr = GetZone(location, region_origin);
-      csg::Point3 const& region_local_pt = location - region_origin;
+      csg::Point3 zone_offset;
+      BoxedRegion3Ptr region_ptr = GetZone(location, zone_offset);
+      csg::Point3 const& regionLocalPt = location - zone_offset;
 
       if (!region_ptr) {
          throw std::invalid_argument(BUILD_STRING("point " << location << " is not in world"));
@@ -56,8 +49,8 @@ void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
 
       // O(n) search - consider optimizing
       for (csg::Cube3 const& cube : region) {
-         if (region_local_pt.x >= cube.GetMin().x && region_local_pt.x < cube.GetMax().x &&
-             region_local_pt.z >= cube.GetMin().z && region_local_pt.z < cube.GetMax().z) {
+         if (regionLocalPt.x >= cube.GetMin().x && regionLocalPt.x < cube.GetMax().x &&
+             regionLocalPt.z >= cube.GetMin().z && regionLocalPt.z < cube.GetMax().z) {
             max_y = std::max(cube.GetMax().y, max_y);
          }
       }
@@ -70,35 +63,15 @@ void Terrain::PlaceEntity(EntityRef e, const csg::Point3& location)
    }
 }
 
-BoxedRegion3Ptr Terrain::GetZone(csg::Point3 const& pt, csg::Point3& region_origin)
+BoxedRegion3Ptr Terrain::GetZone(csg::Point3 const& location, csg::Point3& zone_offset)
 {
-   // transform from world coordinates to zone coordinates
-   csg::Point3 const& pt_in_zone_space = pt - zone_offset_;
-
-   // determine the origin of the region containing the point
-   csg::Point3 region_origin_in_zone_space;
-   for (int i = 0; i < 3; i++) {
-      int c = pt_in_zone_space[i];
-      if (c >= 0) {
-         region_origin_in_zone_space[i] = c / zone_size_ * zone_size_;
-      } else {
-         // floor towards -infinity, not towards origin
-         region_origin_in_zone_space[i] = (c-(zone_size_-1)) / zone_size_ * zone_size_;
+   // O(n) search - consider optimizing
+   for (auto& entry : zones_) {
+      zone_offset = entry.first;
+      if (location.x >= zone_offset.x && location.x < zone_offset.x + zone_size_ &&
+          location.z >= zone_offset.z && location.z < zone_offset.z + zone_size_) {
+         return entry.second;
       }
-   }
-
-   // transform back to world coordinates
-   csg::Point3 region_origin_in_world_space;
-   region_origin_in_world_space = region_origin_in_zone_space + zone_offset_;
-
-   // get the region from the ZoneMap
-   auto entry = zones_.find(region_origin_in_world_space);
-
-   if (entry != zones_.end()) {
-      region_origin = entry->first;
-      ASSERT(pt.x >= region_origin.x && pt.x < region_origin.x + zone_size_ &&
-             pt.z >= region_origin.z && pt.z < region_origin.z + zone_size_);
-      return entry->second;
    }
    return nullptr;
 }

source/om/components/terrain.h
@@ -34,7 +34,6 @@ public:
 
    int GetZoneSize();
    void SetZoneSize(int zone_size);
-   void SetZoneOffset(csg::Point3 offset);
    void AddZone(csg::Point3 const& zone_offset, BoxedRegion3Ptr region3);
    void PlaceEntity(EntityRef e, const csg::Point3& location);
    void CreateNew();
@@ -44,7 +43,6 @@ public:
 
 private:
    dm::Boxed<int> zone_size_;
-   dm::Boxed<csg::Point3> zone_offset_;
    ZoneMap zones_;
    void InitializeRecordFields() override;
 };

source/stonehearth_data/mods/stonehearth/services/world_generation/height_map_renderer.lua
@@ -15,9 +15,6 @@ function HeightMapRenderer:__init(zone_size)
    self.zone_size = zone_size
    self._terrain = radiant._root_entity:add_component('terrain')
    self._terrain:set_zone_size(zone_size)
-
-   -- eww, gross - will go away with new terrain chunking code
-   self._terrain:set_zone_offset(Point3(-self.zone_size/2, 0, -self.zone_size/2));
 end
 
 -- delegate to C++ to "tesselate" heightmap into rectangles