abe604dd9ecddc42cf2d67c9c974d476ab0ccdda
Tony Cannon <tony@radiant-entertainment.com>
2013-10-28 18:57:07 -0700

    more progress on better blueprint/terrain rendering

---
+++ source/csg/edge_tools.h
@@ -0,0 +1,106 @@
+#ifndef _RADIANT_CSG_EDGE_TOOLS_H
+#define _RADIANT_CSG_EDGE_TOOLS_H
+
+#include <vector>
+
+BEGIN_RADIANT_CSG_NAMESPACE
+
+template <typename S, int C>
+struct EdgeInfo
+{
+   Point<S, C>    min;
+   Point<S, C>    max;
+   Point<S, C>    normal;
+};
+
+template <typename S, int C>
+class EdgePoint
+{
+public:
+   EdgePoint(Point<S, C> const& l, Point<S, C> const& a) : location(l), accumulated_normals(a) { }
+
+   EdgePoint& AccumulateNormal(Point<S, C> const& n) {
+      for (int i = 0; i < C; i++) {
+         if (n[i]) {
+            if (accumulated_normals[i]) {
+               ASSERT(n[i] == accumulated_normals[i]);
+            } else {
+               accumulated_normals[i] = n[i];
+            }
+            break;
+         }
+      }
+      return *this;
+   }
+
+public:
+   Point<S, C>    location;
+   Point<S, C>    accumulated_normals;
+};
+
+template <typename S, int C>
+class Edge
+{
+public:
+   Edge(EdgePoint<S, C> const* a, EdgePoint<S, C> const* b, Point<S, C> const& n) : min(a), max(b), normal(n) { }
+
+public:
+   EdgePoint<S, C> const*  min;
+   EdgePoint<S, C> const*  max;
+   Point<S, C>             normal;
+};
+
+template <typename S, int C>
+class EdgeMap
+{
+public:
+   EdgeMap& AddEdge(Point<S, C> const& min, Point<S, C> const& max, Point<S, C> const& normal) {
+      edges.emplace_back(Edge<S, C>(AddPoint(min, normal),
+                                    AddPoint(max, normal),
+                                    normal));
+      return *this;
+   }
+   ~EdgeMap() {
+      for (auto point : points) {
+         delete point;
+      }
+   }
+
+   typename std::vector<Edge<S, C>>::const_iterator begin() { return edges.begin(); }
+   typename std::vector<Edge<S, C>>::const_iterator end() { return edges.end(); }
+
+   bool FindEdgePoint(Point<S, C> const& pt, EdgePoint<S, C>** edge_point) {
+      for (EdgePoint<S, C>* ep : points) {
+         if (ep->location == pt) {
+            *edge_point = ep;
+            return true;
+         }
+      }
+      return false;
+   }
+
+private:
+   EdgePoint<S, C>* AddPoint(Point<S, C> const& p, Point<S, C> const& normal) {
+      for (auto& point : points) {
+         if (point->location == p) {
+            point->AccumulateNormal(normal);
+            return point;
+         }
+      }
+      points.emplace_back(new EdgePoint<S, C>(p, normal));
+      return points.back();
+   }
+
+private:
+   std::vector<Edge<S, C>>          edges;
+   std::vector<EdgePoint<S, C>*>    points;
+};
+
+typedef EdgeMap<int, 2> EdgeMap2;
+typedef EdgeMap<int, 3> EdgeMap3;
+typedef EdgeInfo<int, 2> EdgeInfo2;
+typedef EdgeInfo<int, 3> EdgeInfo3;
+
+END_RADIANT_CSG_NAMESPACE
+
+#endif // _RADIANT_CSG_EDGE_TOOLS_H
--- source/lib/lua/client/open_client.cpp
+++ source/lib/lua/client/open_client.cpp
@@ -23,6 +23,67 @@ using namespace ::radiant;
 using namespace ::radiant::client;
 using namespace luabind;
 
+static H3DNode CreateBlueprintOutline(H3DNode parent,
+                                      float line_width,
+                                      csg::Region3 const& model,
+                                      std::string const& material_path)
+{
+   H3DNode mesh_node;
+   csg::mesh_tools::mesh mesh = csg::mesh_tools().ConvertRegionToOutline(model, line_width, csg::Color3(255, 0, 0));
+
+   H3DNodeUnique model_node = Pipeline::GetInstance().AddMeshNode(parent, mesh, &mesh_node);
+   H3DRes material = h3dAddResource(H3DResTypes::Material, material_path.c_str(), 0);
+   h3dSetNodeParamI(mesh_node, H3DMesh::MatResI, material);
+
+   H3DNode node = model_node.get();
+   model_node.release();
+
+   return node;
+}
+
+static H3DNode CreateBlueprintPanels(H3DNode parent,
+                                     float line_width,
+                                     csg::Region3 const& model,
+                                     std::string const& material_path)
+{
+   H3DNode mesh_node;
+   csg::mesh_tools::mesh mesh = csg::mesh_tools().ConvertRegionToMesh(model);
+
+#if 0
+   csg::mesh_tools m;
+   m.ForEachRegionPlane(model, 0, [&](csg::Region2 const& r2, csg::mesh_tools::PlaneInfo const& pi) {
+      csg::Region2f inset = m.InsetRegion(r2, line_width);
+      AddRegionToMesh(r2, pi, m);
+   });
+#endif
+
+   H3DNodeUnique model_node = Pipeline::GetInstance().AddMeshNode(parent, mesh, &mesh_node);
+   H3DRes material = h3dAddResource(H3DResTypes::Material, material_path.c_str(), 0);
+   h3dSetNodeParamI(mesh_node, H3DMesh::MatResI, material);
+
+   H3DNode node = model_node.get();
+   model_node.release();
+
+   return node;
+}
+
+H3DNodeUnique Client_CreateBlueprintNode(lua_State* L, 
+                                         H3DNode parent,
+                                         csg::Region3 const& model,
+                                         std::string const& material_path)
+{
+   static const float line_width = 0.10f;
+   static int unique = 1;
+
+   H3DNode group = h3dAddGroupNode(parent, BUILD_STRING("blueprint node " << unique++).c_str());
+
+   CreateBlueprintOutline(group, line_width, model, material_path);
+   CreateBlueprintPanels(group, line_width, model, material_path);
+
+   return group;
+}
+
+
 H3DNodeUnique Client_CreateVoxelRenderNode(lua_State* L, 
                                            H3DNode parent,
                                            csg::Region3 const& model,
@@ -317,6 +378,7 @@ void lua::client::open(lua_State* L)
             def("trace_render_frame",              &Client_TraceRenderFrame),
             def("set_cursor",                      &Client_SetCursor),
             def("create_voxel_render_node",        &Client_CreateVoxelRenderNode),
+            def("create_blueprint_node",           &Client_CreateBlueprintNode),
             def("alloc_region",                    &Client_AllocObject<om::Region3Boxed>),
             def("create_data_store",               &Client_CreateDataStore),
             def("is_valid_standing_region",        &Client_IsValidStandingRegion),
