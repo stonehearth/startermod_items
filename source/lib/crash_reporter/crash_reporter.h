ba457c3ec2c215bd59dcbf17c973fe7866e2d69e
Albert Wang <albert@radiant-entertainment.com>
2013-11-08 02:12:44 -0800

    Incorporating feedback from code review.

--- source/lib/crash_reporter/crash_reporter.h
+++ source/lib/crash_reporter/crash_reporter.h
@@ -2,28 +2,40 @@
 #define _RADIANT_LIB_CRASH_REPORTER_CRASH_REPORTER_H
 
 #include "resource.h"
-#include "client/windows/crash_generation/crash_generation_server.h" // google_breakpad
+#include "radiant.h"
+#include "core/singleton.h"
+#include <functional>
 
-class CrashReporter
+// The google headers are ugly so forward declare the types instead
+namespace google_breakpad {
+   class ClientInfo;
+   class CrashGenerationServer;
+}
+
+using namespace radiant;
+
+class CrashReporter : public core::Singleton<CrashReporter>
 {
 public:
-   CrashReporter(std::string const& pipe_name, std::string const& dump_path, std::string const& uri);
-   void SendCrashReport(std::string const& dump_filename);
+   void Run(std::string const& pipe_name, std::string const& dump_path, std::string const& uri,
+            std::function<void()> const& exit_process_function);
 
 private:
    bool StartCrashGenerationServer();
+   void SendCrashReport(std::string const& dump_filename);
    void CreateZip(std::string const& zip_filename, std::string const& dump_filename);
+   void ExitProcess();
 
    // Breakpad callbacks
    static void OnClientConnected(void* context, google_breakpad::ClientInfo const* client_info);
    static void OnClientCrashed(void* context, google_breakpad::ClientInfo const* client_info, std::wstring const* dump_filename_w);
    static void OnClientExited(void* context, google_breakpad::ClientInfo const* client_info);
-   static void RequestApplicationExit();
 
-   std::string const pipe_name_;
-   std::string const dump_path_;
-   std::string const uri_;
+   std::string pipe_name_;
+   std::string dump_path_;
+   std::string uri_;
    std::unique_ptr<google_breakpad::CrashGenerationServer> crash_server_;
+   std::function<void()> exit_process_function_;
 };
 
 #endif // _RADIANT_LIB_CRASH_REPORTER_CRASH_REPORTER_H
