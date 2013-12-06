4d67e1e6fee2036985ea0595d976e2807bcba93e
Tom Cannon <tom@radiant-entertainment.com>
2013-12-05 18:03:25 -0800

    Add hints for the camera controls.

--- source/stonehearth_data/mods/stonehearth/call_handlers/camera_call_handler.lua
+++ source/stonehearth_data/mods/stonehearth/call_handlers/camera_call_handler.lua
@@ -2,9 +2,36 @@ local camera = require 'services.camera.camera_service'
 
 local CameraCallHandler = class()
 
+local camera_tracker = nil
+local trace
 function CameraCallHandler:camera_look_at_entity(session, request, entity)
    camera:look_at_entity(entity)
    request:resolve({})
+end
+
+function CameraCallHandler:get_camera_tracker(session, request)
+   if not camera_tracker then
+      camera_tracker = _radiant.client.create_data_store()
+      trace = camera_tracker:trace_data('checking')
+                      :on_changed(function()
+                        radiant.log.info('boo!')
+                      end)
+      
+      radiant.events.listen(camera, 'stonehearth:camera:update', self, self.update_camera_tracker)
+      self:update_camera_tracker()
+   end
+
+   return { camera_tracker = camera_tracker }
+end
+
+function CameraCallHandler:update_camera_tracker(e)
+   if e then
+       camera_tracker:update({
+          pan = e.pan,
+          zoom = e.zoom,
+          orbit = e.orbit
+       })
+   end
 end
 
 return CameraCallHandler
