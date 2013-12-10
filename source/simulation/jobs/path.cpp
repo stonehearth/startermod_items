2642db3a153bab941f9414d19596ee95dd300b3c
Tony Cannon <tony@radiant-entertainment.com>
2013-12-09 16:32:39 -0800

    give each path an id.  makes the log prettier

--- source/simulation/jobs/path.cpp
+++ source/simulation/jobs/path.cpp
@@ -4,9 +4,22 @@
 using namespace ::radiant;
 using namespace ::radiant::simulation;
 
+static int next_path_id_ = 1;
+
+#define PF_LOG(level)   LOG_CATEGORY(simulation.pathfinder, level, "path " << id_)
+
+Path::Path(const std::vector<csg::Point3>& points, om::EntityRef source, om::EntityRef destination, csg::Point3 const& finish) :
+   points_(points),
+   source_(source),
+   destination_(destination),
+   finish_pt_(finish),
+   id_(next_path_id_++)
+{
+}
+
 std::ostream& Path::Format(std::ostream& os) const
 {
-   os << "[Path " << ((void*)this);
+   os << "[path " << id_;
    if (!points_.empty()) {
       os << " " << points_.front() << " -> " << points_.back();
    } else {
