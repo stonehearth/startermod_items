80a32a0ccf086ef69cf31acacea5daf3da7ce2e8
Tony Cannon <tony@radiant-entertainment.com>
2013-12-12 12:47:11 -0800

    Remove dead code.

--- source/simulation/actions/pch.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RADIANT_SIMULATION_ACTIONS_PCH_H
-#define _RADIANT_SIMULATION_ACTIONS_PCH_H
-
-#include "radiant.h"
-#include "radiant_stdutil.h"
-#include "lib/lua/bind.h"
-
-#endif // _RADIANT_CLIENT_ACTIONS_PCH_H
