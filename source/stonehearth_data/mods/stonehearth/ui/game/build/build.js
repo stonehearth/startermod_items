c54ae04f7be794242e6f69ba57d9347022c5a260
Tom Cannon <tom@radiant-entertainment.com>
2013-12-18 12:53:20 -0800

    Add a hint when building walls.

--- source/stonehearth_data/mods/stonehearth/ui/game/build/build.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/build/build.js
@@ -10,6 +10,10 @@ $(document).ready(function(){
       })
 
    $(top).on("radiant_create_wall", function (_, e) {
+      $(top).trigger('radiant_show_tip', { 
+         title : 'Click to place wall segments',
+         description : 'Hold down SHIFT while clicking to draw connected walls!'
+      });
       radiant.call_obj(build_editor, 'place_new_wall');
    });
    $(top).on("radiant_create_room", function (_, e) {
