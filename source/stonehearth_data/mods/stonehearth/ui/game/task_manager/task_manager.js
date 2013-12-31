6b53dd44c0fcc181d5a989982f39700a3eb18e93
Tom Cannon <tom@radiant-entertainment.com>
2013-12-31 13:25:44 -0800

    First pass at bars.
    green = idle
    yellow = pathfinder
    orange = collision
    purple = network
    blue = lua
    gray = unaccounted

--- source/stonehearth_data/mods/stonehearth/ui/game/task_manager/task_manager.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/task_manager/task_manager.js
@@ -9,8 +9,44 @@ App.StonehearthTaskManagerView = App.View.extend({
       radiant.call('radiant:game:start_task_manager')
             .progress(function (response) {
                var foo = JSON.stringify(response)
-               self.set('context.data', foo);
+               console.log(foo)
+               self._refresh(response)
             })
+   },
+
+   didInsertElement: function () {
+      this.bars = $('#taskManager');
+
+   },
+
+   _refresh: function(data) {
+      var self = this;
+      var totalWidth = 400;
+      var scale = totalWidth / data.total_time;
+      var sum = 0;
+
+      this.bars.css('width', totalWidth);
+      
+      $.each(data.counters, function(i, counter) {
+         if(counter.name != 'idle') {
+            var className = counter.name.replace(/ /g, '_');
+            var bar = self.bars.find('.' + className);
+
+            if (bar.length == 0) {
+               bar = $('<div>')
+                  .addClass('counter')
+                  .addClass(className);
+
+               self.bars.append(bar)
+            } 
+
+            var width = counter.time * scale
+            bar.css('width', width);
+            sum += width;
+         }
+      });
+
+      //this.bars.find('.idle').css('width', totalWidth - sum);
    },
 
    destroy: function() {
@@ -19,3 +55,4 @@ App.StonehearthTaskManagerView = App.View.extend({
    }
 
 });
+   
