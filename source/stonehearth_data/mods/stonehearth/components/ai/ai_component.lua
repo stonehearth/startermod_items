33606cd97b55805302b8dbd15667a555e3d02370
Tony Cannon <tony@radiant-entertainment.com>
2014-01-03 11:22:20 -0800

    Create a new ExecutionUnit interface to wrap actions.
    Provide V1 and (currently empty) V2 implementations to handle the different action interfaces.
    Now I'm free to start gutting the AI system without having to re-write every action.  Muhahahahahha!

--- source/stonehearth_data/mods/stonehearth/components/ai/ai_component.lua
+++ source/stonehearth_data/mods/stonehearth/components/ai/ai_component.lua
@@ -25,7 +25,7 @@ function AIComponent:destroy()
    self._ai_system:stop_ai(self._entity:get_id(), self)
 
    self._dead = true
-   self:_clear_stack()
+   self:_terminate_thread()
 
    for obs, _ in pairs(self._observers) do
       if obs.destroy then
@@ -45,19 +45,21 @@ function AIComponent:add_action(uri, action)
    assert(not self._actions[uri])
    self._actions[uri] = action
 
-   local action_index = self._action_index[action.does]
+   local does = action:get_activity()
+   local action_index = self._action_index[does]
    if not action_index then
       action_index = {}
-      self._action_index[action.does] = action_index
+      self._action_index[does] = action_index
    end
-   self._action_index[action.does][uri] = action
+   self._action_index[does][uri] = action
 end
 
 function AIComponent:remove_action(uri)
    local action = self._actions[uri]
+   local does = action:get_activity()
    if action then
       self._actions[uri] = nil
-      self._action_index[action.does][uri] = nil
+      self._action_index[does][uri] = nil
 
       if action.destroy then
          action:destroy()
@@ -79,12 +81,19 @@ function AIComponent:remove_observer(observer)
    end
 end
 
+-- shouldn't need this...  threads should restart themselves... but
+-- what if we die while trying to restart?  ug!
+function AIComponent:restart_if_terminated()
+   if not self._co then
+      self:restart()
+   end
+end
+
 function AIComponent:restart()
    assert(not self._dead)
 
    radiant.check.is_entity(self._entity)
-   self:_clear_stack()
-
+   self:_terminate_thread()
    self._co = self._ai_system:_create_thread(self, function()
       while not self._dead do
          self:execute('stonehearth:top')
@@ -94,100 +103,68 @@ end
 
 function AIComponent:_create_execution_frame(activity)
    local activity_name = select(1, unpack(activity))
-   local action_index = self._action_index[activity_name]
-   local actions = {}
-
-   log:spam('%s creating execution frame for %s', self._entity, activity_name)
-   for uri, action in pairs(action_index) do
-      if self:_stack_contains(action) then
-         log:spam('  ignoring action %s (already in stack)', action.name)
-      else
-         log:spam('  adding action %s', action.name)
-         table.insert(actions, action)
+   
+   -- make sure this activity isn't in the stack
+   for _, frame in ipairs(self._stack) do
+      if frame:get_activity_name() == activity_name then
+         self:abort('cannot run activity "%s".  already in the stack!', activity_name)
       end
    end
-   return ExecutionFrame(self, actions)
+
+   -- create a new frame and return it   
+   local actions = self._action_index[activity_name]
+
+   log:spam('%s creating execution frame for %s', self._entity, activity_name)
+   return ExecutionFrame(self, actions, activity)
 end
 
-function AIComponent:_clear_stack()
+function AIComponent:_terminate_thread()
    for i = #self._stack, 1, -1 do
-      self._stack[i]:do_stop(false)
+      self._stack[i]:destroy(false)
    end
    self._stack = {}
+   
    if self._co then
-      self._ai_system:_terminate_thread(self._co)
+      local co = self._co
       self._co = nil
+      
+      -- If we're calling _terminate_thread() from the co-routine itself,
+      -- this function MAY NOT RETURN!
+      self._ai_system:_terminate_thread(co)      
    end
 end
 
 function AIComponent:abort(reason)
    -- xxx: assert that we're running inthe context of the coroutine
    if reason == nil then reason = 'no reason given' end
-   log:info('%s Aborting current action because: %s', self._entity, reason)
-   
-   self:_clear_stack()   
-   -- all actions have had their stop method called on them.  yield
-   -- KILL_THREAD to get the ai service to call restart() next time,
-   -- which will start us over at stonehearth:top.   
-   
-   self._ai_system:_complete_thread_termination(self._co)   
-end
-
-function AIComponent:_stack_contains(action)
-   for _, execution_frame in ipairs(self._stack) do
-      if execution_frame:get_running_action() == action then
-         return true
-      end
-   end
-   return false
-end
-
-function AIComponent:_for_each_execution_frame(fn)
-   local execution_count = self._execution_count
-   for _, ef in ipairs(self._stack) do
-      fn(ef)
-      if execution_count ~= self._execution_count then
-         break
-      end
-   end
-end
-
-function AIComponent:set_action_priority(action, n)
-   action.priority = n
-   self:_for_each_execution_frame(function(ef)
-      if ef:contains_action(action) then
-         ef:on_set_action_priority(action)
-      end
-   end)
+   log:info('%s Aborting current action because: %s', self._entity, reason)  
+   self:_terminate_thread()
 end
 
 function AIComponent:execute(...)
    -- get a set of valid actions for this excution frame 
    local activity = {...}
-   local execution_frame = self:_create_execution_frame(activity)
+   local frame = self:_create_execution_frame(activity)
    
    self._execution_count = self._execution_count + 1
    table.insert(self._stack, frame)
    local stack_len = #self._stack
 
-   local result = execution_frame:do_run(unpack(activity))   
-   execution_frame:do_stop(true)
-
+   local result = frame:execute(unpack(activity))   
+   
    assert(#self._stack == stack_len)
-   assert(self._stack[stack_len] == execution_frame)
-   table.pop(self._stack)
+   assert(self._stack[stack_len] == frame)
+   frame:destroy()
+   
+   table.remove(self._stack)
    return unpack(result)
 end
 
-function AIComponent:wait_until(obj)
-   coroutine.yield(obj)
-end
-
-function AIComponent:suspend()
+function AIComponent:suspend_thread()
    coroutine.yield(self._ai_system.SUSPEND_THREAD)
 end
 
-function AIComponent:resume()
+function AIComponent:resume_thread()
    self._ai_system:_resume_thread(self._co)
 end
 
--- source/stonehearth_data/mods/stonehearth/components/ai/execution_frame.lua
+++ source/stonehearth_data/mods/stonehearth/components/ai/execution_frame.lua
@@ -6,147 +6,68 @@ local THINKING = 0
 local WAITING = 1
 local RUNNING = 2
 
-function ExecutionFrame:__init(ai, actions)
+function ExecutionFrame:__init(ai, execution_units, activity)
    self._ai = ai
-   self._actions = actions
-   self._action_run_args = {}
-end
-
-function ExecutionFrame:_do_run(activity_name, ...)
-   self._activity_name = activity_name
-   local args = { ... }
-
-   self._state = THINKING
-   self:_do_think(args)
-   self._state = WAITING
-   local action = self:_choose_action()
-   if action then
-      self._state = RUNNING
-      self._running_action = action
-      log:debug('%s coroutine starting action: %s for activity %s(%s)', self._entity, tostring(action.name), self._activity_name, self:_format_args(args))
-      local result = { action:run(self, self._entity, unpack(args)) }
-      log:debug('%s coroutine finished: %s', self._entity, tostring(action.name))
+   self._entity = self._ai:get_entity()
+   self._activity_name = activity[1]   
+   self._execution_units = execution_units
+   
+   local args = { select(2, unpack(activity)) }
+   for uri, unit in pairs(self._execution_units) do
+      unit:join_execution_frame(self, args)
    end
-   return result
 end
 
-function ExecutionFrame:contains_action(action)
-   for _, a in ipairs(self._actions) do
-      if action == a then
-         return true
-      end
+function ExecutionFrame:destroy()
+   for uri, unit in pairs(self._execution_units) do
+      unit:leave_execution_frame(self)
    end
-   return false
+   self._execution_units = {}
 end
 
-function ExecutionFrame:activate(action, args)
-   self._action_run_args[action] = args
-   -- UGGGGGGGGGGGGGGGGGGGGGG
-   -- what happens when a action is in multiple frames?
-   -- For example,
-   --   [a b, c, d, e, f]
-   --         |
-   --      [g, h, i, j ]
-   --             |
-   --        [a, b, d, e, f] <- c removed to prevent looping, but a, b, d, e, f repeated
-   --
-   -- The fix is!
-   --   1) Make an ExecutionUnit wrapper for each action
-   --   2) ExecutionUnit have back-pointers to the frames they're in
-   --   3) They only call think once (by counting how many frames their in)
-   --   4) The route set_priority, activate, etc to the proper frame (probably the one at the top
-   --      of the stack).
-   --
-   -- Pass the ExecutionUnit into the action at creation time instead of the AiComponent!
+function ExecutionFrame:get_activity_name()
+   return self._activity_name
 end
 
-function ExecutionFrame:on_set_action_priority(action)
-   if not self._running_action then
+function ExecutionFrame:execute()
+   self._current_unit = self:_get_next_execution_unit()
+   
+   assert(self._current_unit) -- this is totally valid... just need to figure out what to do here...
+   if self._current_unit then      
+      return self._current_unit:execute_frame(self)
+   end
+end
+
+function ExecutionFrame:on_action_priority_changed(unit)
+   if not self._current_unit then
       -- still initializing the think
       return
    end
    
    local abort = false
-   if action == self._running_action then
-      -- if the current action is trying to change its priority, we need to check the
-      -- current frame if that might cause some other action to be run.  yes, this is
-      -- a weird thing for an action to do, but it's easy to be robust here.
-      if priority < action.priority then
-         -- going lower doesn't mean we're the lowest!  check now...
-         local best_action = self._choose_action()
-         abort = action ~= best_action
-      end
+   local new_priority = unit:get_priority()
+   if unit == self._current_unit then
+      -- if the current unit is trying to change its priority, we need to check the
+      -- current frame if that might cause some other unit to be run.  yes, this is
+      -- a weird thing for an unit to do, but it's easy to be robust here.
+      local best_unit = self:_get_next_execution_unit()
+      abort = unit ~= best_unit
    else
       -- if another action is elevating it's priority above the current action
       -- abort.
-      abort = priority > self._running_action.priority
+      abort = new_priority > self._current_unit:get_priority()
    end
 
-   action.priority = priority
    if abort then
-      self._ai:abort(string.format('action %s elevated priority above running action %s (%d > %d)',
-                                   action.name, priority, self._running_action.name,
-                                   self._running_action.priority))
+      self._ai:abort(string.format('unit priority change resulted in new best unit'))
    end
 end
-]]
 
-function ExecutionFrame:execute(...)
-   self._ai:execute(...)
+function ExecutionFrame:get_current_unit()
+   return self._current_unit
 end
 
-function ExecutionFrame:abort(reason)
-   self._ai:abort(reason)
-end
-
-function ExecutionFrame:wait_until(obj)
-   self._ai:wait_until(obj)
-end
-
-function ExecutionFrame:suspend()
-   self._ai:suspend()
-end
-
-function ExecutionFrame:resume()
-   self._ai:resume()
-end
-
-function ExecutionFrame:wait_for_path_finder(pf)
-   local path
-   pf:set_solved_cb(
-      function(solution)
-         path = solution
-      end
-   )
-   log:debug('%s blocking until pathfinder finishes', self._entity)
-   self:wait_until(function()
-      if path ~= nil then
-         log:debug('%s pathfinder completed!  resuming action', self._entity)
-         return true
-      end
-      if pf:is_idle() then
-         log:debug('%s pathfinder went idle.  aborting!', self._entity)
-         self:abort('pathfinder unexpectedly went idle while finding path')
-      end
-      log:debug('%s waiting for pathfinder: %s', self._entity, pf:describe_progress())
-      return false
-   end)
-   return path
-end
-
-
-function ExecutionFrame:get_running_action()
-   return self._running_action
-end
-
-function ExecutionFrame:do_stop(success)
-   if self._running_action and self._running_action.stop then
-      self._running_action:stop(success)
-   end
-   self._running_action = nil
-end
-
-function ExecutionFrame:_format_args(args)
+function ExecutionFrame:format_args(args)
    local msg = ''
    if args then
       for _, arg in ipairs(args) do
@@ -159,46 +80,40 @@ function ExecutionFrame:_format_args(args)
    return msg
 end
 
-function ExecutionFrame:_do_think(args)
-   for _, action in ipairs(self._actions) do
-      if action.think then
-         self:activate(action, action.think(self, args))
-      else
-         self:activate(action, args)
-      end
-   end
-end
-
-function ExecutionFrame:_choose_action()
-   local entity = self._ai:get_entity()
+function ExecutionFrame:_get_next_execution_unit()  
    local best_priority = 0
-   local best_actions = nil
+   local best_units = nil
    
-   log:spam('%s looking for best action for %s', _entity, self._activity_name)
-   for _, action in ipairs(self._actions) do
-      log:spam('  action %s has priority %d', tostring(action.name), action.priority)
+   log:spam('%s looking for best execution unit for %s', self._entity, self._activity_name)
+   for uri, unit in pairs(self._execution_units) do
+      if unit:is_runnable(self) then
+         local name = unit:get_name()
+         local priority = unit:get_priority()
+         log:spam('  unit %s has priority %d', name, priority)
 
-      if action.priority >= best_priority and self._action_run_args[action] ~= nil then
-         if action.priority > best_priority then
-            -- new best_priority found, wipe out the old list of candidate actions
-            best_actions = {}
-         end
-         best_priority = action.priority
-         local weight = a.weight or 1
-         for i = 1, weight do
-            table.insert(best_actions, a)
+         if priority >= best_priority then
+            if priority > best_priority then
+               -- new best_priority found, wipe out the old list of candidate actions
+               best_units = {}
+            end
+            best_priority = priority
+            for i = 1, unit:get_weight() do
+               table.insert(best_units, unit)
+            end
          end
       end
    end
 
-   if not best_actions then
-      log:debug('no action ready to run yet.  chillin.')
-      return nil, 0
+   if not best_units then
+      log:debug('no unit ready to run yet.  chillin.')
+      return nil
    end
    
-   -- choose a random action amoung all the actions with the highest priority (they all tie)
-   best_action = best_actions[math.random(#best_actions)]
-   log:spam('%s  best action for %s is %s (priority: %d)', self._entity, activity_name, tostring(best_action.name), best_priority)
+   -- choose a random unit amoung all the units with the highest priority (they all tie)
+   local best_unit = best_units[math.random(#best_units)]
+   log:spam('%s  best unit for %s is %s (priority: %d)', self._entity, self._activity_name, best_unit:get_name(), best_priority)
    
-   return best_action
+   return best_unit
 end
+
+return ExecutionFrame
