44a199416e412fcb30e43d2ff65b8fb3c0a30ac9
Chris Klochek <klochek@radiant-entertainment.com>
2014-01-16 14:08:21 -0800

    Isolate perf_test mod from stonehearth, so that it doesn't run if it isn't the mod being explicitly loaded.

--- source/stonehearth_data/mods/perf_test/perf_test_call_handler.lua
+++ source/stonehearth_data/mods/perf_test/perf_test_call_handler.lua
@@ -1,14 +1,28 @@
 local PerfTestCallHandler = class()
 
-function PerfTestCallHandler:get_world_generation_done(session, response)
-  local wgs = radiant.mods.load('stonehearth').world_generation
-  local wg = wgs:create_world(true, 1337)
+local is_running = false
 
-  radiant.events.listen(radiant.events, 'stonehearth:generate_world_progress', self, function(s, e)
-    if e.progress and e.progress >= 100 then
-      response:resolve(true)
-    end
-  end)
+function PerfTestCallHandler:__init()
+end
+
+function PerfTestCallHandler:set_is_running()
+  is_running = true
+end
+
+function PerfTestCallHandler:get_world_generation_done(session, response)
+
+  if is_running then
+    local wgs = radiant.mods.load('stonehearth').world_generation
+    local wg = wgs:create_world(true, 1337)
+
+    radiant.events.listen(radiant.events, 'stonehearth:generate_world_progress', self, function(s, e)
+      if e.progress and e.progress >= 100 then
+        response:resolve(true)
+      end
+    end)
+  else
+    response:resolve(false)
+  end
 end
 
 return PerfTestCallHandler
