62737690c0c5113327c9899967b2f3a6bcbbcaa5
Tom Cannon <tom@radiant-entertainment.com>
2014-01-28 14:49:50 -0800

    Distinguish between a right click vs. right drag, and only move units on right click.

--- source/stonehearth_data/mods/stonehearth/services/unit_control/unit_control_service.lua
+++ source/stonehearth_data/mods/stonehearth/services/unit_control/unit_control_service.lua
@@ -1,4 +1,5 @@
 local Point3 = _radiant.csg.Point3
+local input_constants = require('constants').input
 
 local UnitControlService = class()
 
@@ -21,61 +22,44 @@ function UnitControlService:_on_input(e)
 end
 
 function UnitControlService:_on_mouse_input(e)
-   if e:down(2) then
-      local selected_entity = _radiant.client.get_selected_entity()
-      if selected_entity then
-         local move_location
-         local ray = _radiant.renderer.scene.cast_screen_ray(e.x, e.y)
-         local screen_ray = _radiant.renderer.scene.get_screen_ray(e.x, e.y)
+  
+   if e:up(2) then
+      -- determine if the mouse has been clicked or dragged
+      local dx = math.abs(e.x - self._mouse_down_x)
+      local dy = math.abs(e.y - self._mouse_down_y)
 
-         if ray.is_valid then
-            move_location = ray.point
-         else 
-            move_location = screen_ray.origin
-         end
-
-         _radiant.call('stonehearth:move_unit', selected_entity:get_id(), move_location)
-         :done(function(r)
-               --response:resolve(true)
-            end)
-         :always(function()
-               --cleanup()
-            end)
-
+      if dx < input_constants.mouse.dead_zone_size and dy < input_constants.mouse.dead_zone_size then
+        self:_move_unit(e)
       end
+
+   elseif e:down(2) then
+      self._mouse_down_x = e.x
+      self._mouse_down_y = e.y
    end
-
---[[
-  if drag_key_down and not self._dragging then
-    local r = _radiant.renderer.scene.cast_screen_ray(e.x, e.y)
-    local screen_ray = _radiant.renderer.scene.get_screen_ray(e.x, e.y)
-    self._dragging = true
-    self._drag_origin = screen_ray.origin;
-
-    if r.is_valid then
-      self._drag_start = r.point
-    else
-      local d = -self._drag_origin.y / screen_ray.direction.y
-      screen_ray.direction:scale(d)
-      self._drag_start = screen_ray.origin + screen_ray.direction
-    end
-
-    local root = _radiant.client.get_entity(1)
-    local terrain_comp = root:get_component('terrain')
-    local bounds = terrain_comp:get_bounds():to_float()
-    if not bounds:contains(self._drag_start) then
-      self._dragging = false
-    end
-  elseif not drag_key_down and self._dragging then
-    self._dragging = false
-  end
-
-  if not self._dragging then
-    return
-  end
-
-  ]]  
 end
+
+function UnitControlService:_move_unit(e)
+   local selected_entity = _radiant.client.get_selected_entity()
+   if selected_entity then
+      local move_location
+      local ray = _radiant.renderer.scene.cast_screen_ray(e.x, e.y)
+      local screen_ray = _radiant.renderer.scene.get_screen_ray(e.x, e.y)
+
+      if ray.is_valid then
+         move_location = ray.point
+      else 
+         move_location = screen_ray.origin
+      end
+
+      _radiant.call('stonehearth:move_unit', selected_entity:get_id(), move_location)
+      :done(function(r)
+            --response:resolve(true)
+         end)
+      :always(function()
+            --cleanup()
+         end)
+   end
+end  
 
 UnitControlService:__init()
 return UnitControlService
