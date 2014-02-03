b3989d374849942893c9fa23689737abbd4b4462
Tony Cannon <tony@radiant-entertainment.com>
2014-02-03 10:23:31 -0800

    make idle actions preemptable

--- source/stonehearth_data/mods/stonehearth/ai/actions/wait_for_attribute_above.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/wait_for_attribute_above.lua
@@ -12,6 +12,7 @@ WaitForAttributeAbove.priority = 1
 
 function WaitForAttributeAbove:start_thinking(ai, entity, args)
    self._ai = ai
+   self._log = ai:get_log()
    self._above_value = args.value
    self._signaled = false
    local attribute = 'stonehearth:attribute_changed:' .. args.attribute
@@ -28,11 +29,16 @@ function WaitForAttributeAbove:stop_thinking(ai, entity, args)
    radiant.events.unlisten(entity, attribute, self, self._attribute_changed)
 end
 
-function WaitForAttributeAbove:_attribute_changed(e)
+function WaitForAttributeAbove:_attribute_changed(e)   
+   self._log:spam('attribute:%s value:%d threshold:%d signaled:%s',
+                  e.name, e.value, self._above_value, self._signaled)
+
    if e.value >= self._above_value and not self._signaled then
+      self._log:detail('marking ready!')
       self._signaled = true
       self._ai:set_think_output()
    elseif e.value < self._above_value and self._signaled then
+      self._log:detail('unmarking ready.')
       self._signaled = false
       self._ai:clear_think_output()
    end
