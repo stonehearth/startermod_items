077b002015aa876f71d496480d62378cfbee3dbf
Tony Cannon <tony@radiant-entertainment.com>
2014-02-04 13:29:21 -0800

    add and remove hunger buffs / thoughts immediately instead of hourly.

--- source/stonehearth_data/mods/stonehearth/ai/observers/hunger_observer.lua
+++ source/stonehearth_data/mods/stonehearth/ai/observers/hunger_observer.lua
@@ -18,6 +18,29 @@ function HungerObserver:__init(entity)
    self._entity = entity
    self._attributes_component = entity:add_component('stonehearth:attributes')
    radiant.events.listen(calendar, 'stonehearth:hourly', self, self.on_hourly)
+   radiant.events.listen(entity, 'stonehearth:attribute_changed:hunger', self, self._hunger_changed)
+end
+
+function HungerObserver:_hunger_changed()
+   local hunger = self._attributes_component:get_attribute('hunger')
+
+   local starving = hunger >= 120
+   local hungry = hunger >= 80
+
+   if starving and not self._starving then
+      self._starving = true
+      radiant.entities.add_buff(self._entity, 'stonehearth:buffs:starving')
+   elseif not starving and self._starving then
+      self._hungry = false
+      radiant.entities.remove_buff(self._entity, 'stonehearth:buffs:starving')
+   end
+   if hungry and not self._hungry then
+      self._hungry = true
+      radiant.entities.think(self._entity, '/stonehearth/data/effects/thoughts/hungry')
+   elseif not hungry and self._hungry then
+      self._hungry = false
+      radiant.entities.unthink(self._entity, '/stonehearth/data/effects/thoughts/hungry')
+   end
 end
 
 function HungerObserver:on_hourly(e)
@@ -26,19 +49,7 @@ function HungerObserver:on_hourly(e)
 
    -- TODO consider slowing hunger while sleepy
    hunger = hunger + ( 100 / hours_till_hungry)
-
    self._attributes_component:set_attribute('hunger', hunger)
-
-   if hunger >= 120 then
-      radiant.entities.add_buff(self._entity, 'stonehearth:buffs:starving')
-   else
-      radiant.entities.remove_buff(self._entity, 'stonehearth:buffs:starving')
-   end
-   if hunger >= 80 then
-      radiant.entities.think(self._entity, '/stonehearth/data/effects/thoughts/hungry')
-   else
-      radiant.entities.unthink(self._entity, '/stonehearth/data/effects/thoughts/hungry')
-   end
 end
 
 return HungerObserver
