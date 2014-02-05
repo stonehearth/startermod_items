737a00f5289e7f484f86ce455cbcd77c07cb8252
Tom Cannon <tom@radiant-entertainment.com>
2014-02-04 17:24:07 -0800

    Drop what you're carrying before trying to pick up food.

--- source/stonehearth_data/mods/stonehearth/ai/actions/eat_on_ground.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/eat_on_ground.lua
@@ -10,7 +10,8 @@ EatOnGround.priority = 1
 local ai = stonehearth.ai
 return ai:create_compound_action(EatOnGround)
             :execute('stonehearth:wait_for_attribute_above', { attribute = 'hunger', value = 120 })
-            :execute("stonehearth:get_food_from_container")
+            :execute('stonehearth:drop_carrying_now')
+            :execute('stonehearth:get_food_from_container')
             :execute('stonehearth:wander', { radius = 5 })
             :execute('stonehearth:sit_on_ground')
             :execute('stonehearth:eat_carrying')
