276efce6e7ee90778d0d7d238e099faf0f224510
Stephanie <stephanie@radiant-entertainment.com>
2014-02-05 16:58:48 -0800

    Fixed some issues with beds

--- source/stonehearth_data/mods/stonehearth/ai/actions/sleep_in_new_bed_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/sleep_in_new_bed_action.lua
@@ -7,18 +7,34 @@ SleepInNewBed.args = { }
 SleepInNewBed.version = 2
 SleepInNewBed.priority = 1
 
+function currently_has_bed(entity)
+   -- if we don't own any leases, clearly we don't have a bed yet.  
+   local lease_component = entity:get_component('stonehearth:lease_holder')
+   if not lease_component then
+      return false
+   end
+   -- if the bed that we've leased is still valid, we should not try
+   -- to look for a new bed.
+   local bed = lease_component:get_first_lease('stonehearth:bed')
+   return bed and bed:is_valid()
+end
+
+function is_available_bed(target, ai, entity)
+   -- make sure it's a bed and no one else has a lease on it
+   if radiant.entities.get_entity_data(target, 'stonehearth:bed') then
+      local lease_component = target:get_component('stonehearth:lease')
+      if not lease_component then
+         return true
+      end
+      return lease_component:can_acquire('stonehearth:bed', entity)
+   end
+   return false
+end
+
 local ai = stonehearth.ai
 return ai:create_compound_action(SleepInNewBed)
-         :when(function (ai, entity) 
-               -- only run if we don't have a current bed lease
-               local lease_component = entity:get_component('stonehearth:lease_holder')
-               if not lease_component then
-                  return true
-               end
-               local bed = lease_component:get_first_lease('stonehearth:bed')
-               return not bed or not bed:is_valid()
-            end)
+         :when(function (ai, entity) return not currently_has_bed(entity) end)
          :execute('stonehearth:drop_carrying_now')
-         :execute('stonehearth:goto_entity_with_entity_data', { key = 'stonehearth:bed' })
-         :execute('stonehearth:reserve_entity', { entity = ai.PREV.entity })
+         :execute('stonehearth:goto_entity_type', { filter_fn = is_available_bed })
+         :execute('stonehearth:reserve_entity', { entity = ai.PREV.destination_entity })
          :execute('stonehearth:sleep_in_bed_adjacent', { bed = ai.PREV.entity })
