29b741cd84fd819c437052cebbec9c00b751c774
Stephanie <stephanie@radiant-entertainment.com>
2014-02-12 17:42:02 -0800

    Removing the workshop borked the text on the carpenter promote scroll

--- source/stonehearth_data/mods/stonehearth/professions/carpenter/carpenter.lua
+++ source/stonehearth_data/mods/stonehearth/professions/carpenter/carpenter.lua
@@ -13,6 +13,15 @@ function carpenter_class.promote(entity)
    --Slap a new outfit on the crafter
    local equipment = entity:add_component('stonehearth:equipment')
    equipment:equip_item('stonehearth:carpenter:outfit')
+
+   --Make sure that the build workshop command has a reference to this file
+   --Would an alias work below?
+   local command_component = entity:get_component('stonehearth:commands')
+   if command_component then
+      command_component:modify_command('build_workshop', function(command) 
+            command.event_data.profession_info = "/stonehearth/entities/professions/carpenter/profession_description.json"
+         end)
+   end
 end
 
 function carpenter_class.demote(entity)
