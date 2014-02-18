2896a94dc3abd617c124ae0d06c5e6b7460741b4
Albert Wang <albert@radiant-entertainment.com>
2014-02-18 14:28:40 -0800

    Created easy method to place clusters of wildlife for scenario modders. Use the method for rabbits and sheep.

--- source/stonehearth_data/mods/stonehearth/scenarios/bunnies/bunny_nest.lua
+++ source/stonehearth_data/mods/stonehearth/scenarios/bunnies/bunny_nest.lua
@@ -5,27 +5,9 @@ end
 
 function BunnyNest:initialize(properties, services)
    local rabbit_name = 'stonehearth:rabbit'
-   local rng = services.rng
-   local width = properties.size.width
-   local length = properties.size.length
-   local offset = width/4
-   local increment = offset*2
-   local margin = 2
-   local child_density = 0.7
-   local dx, dy
-
-   -- place_entity is valid from (1, 1) to (width, length) inclusive
-   services:place_entity(rabbit_name, math.floor(width/2), math.floor(length/2))
-
-   for j=offset, length, increment do
-      for i=offset, length, increment do
-         if rng:get_real(0, 1) < child_density then
-            dx = rng:get_int(0, offset-margin)
-            dy = rng:get_int(0, offset-margin)
-            services:place_entity(rabbit_name, i+dx, j+dy)
-         end
-      end
-   end
+   local num_rabbits = services.rng:get_int(2, 6)
+   local rabbit_radius = 0
+   services:place_entity_cluster(rabbit_name, num_rabbits, rabbit_radius)
 end
 
 return BunnyNest
--- source/stonehearth_data/mods/stonehearth/services/scenario/scenario_modder_services.lua
+++ source/stonehearth_data/mods/stonehearth/services/scenario/scenario_modder_services.lua
@@ -1,5 +1,7 @@
 local MathFns = require 'services.world_generation.math.math_fns'
+local PerturbationGrid = require 'services.world_generation.perturbation_grid'
 local Point3 = _radiant.csg.Point3
+
 local log = radiant.log.create_logger('scenario_service')
 
 local ScenarioModderServices = class()
@@ -15,6 +17,35 @@ function ScenarioModderServices:place_entity(uri, x, y)
    return entity
 end
 
+function ScenarioModderServices:place_entity_cluster(uri, quantity, entity_radius)
+   local rng = self.rng
+   local size = self._properties.size
+   local grid_spacing = self:_get_perturbation_grid_spacing(size.width, size.length, quantity)
+   local grid = PerturbationGrid(size.width, size.length, grid_spacing, self.rng)
+   local num_cells_x, num_cells_y = grid:get_dimensions()
+   local cells_left = num_cells_x * num_cells_y
+   local num_selected = 0
+   local x, y, probability
+
+   for j=1, num_cells_y do
+      for i=1, num_cells_x do
+         -- this algorithm guarantees that each cell has an equal probability of being selected
+         probability = (quantity - num_selected) / cells_left
+
+         if rng:get_real(0, 1) < probability then
+            x, y = grid:get_perturbed_coordinates(i, j, entity_radius)
+            self:place_entity(uri, x, y)
+            num_selected = num_selected + 1
+
+            if num_selected == quantity then
+               return
+            end
+         end
+         cells_left = cells_left - 1
+      end
+   end
+end
+
 -- Private API starts here
 function ScenarioModderServices:__init(rng)
    self.rng = rng
@@ -27,13 +58,23 @@ function ScenarioModderServices:_set_scenario_properties(properties, offset_x, o
 end
 
 function ScenarioModderServices:_bounds_check(x, y)
-   return MathFns.bound(x, 1, self._properties.size.width),
-          MathFns.bound(y, 1, self._properties.size.length)
+   local size = self._properties.size
+
+   return MathFns.bound(x, 1, size.width),
+          MathFns.bound(y, 1, size.length)
 end
 
 function ScenarioModderServices:_set_random_facing(entity)
    local facing = self.rng:get_int(0, 3)
    entity:add_component('mob'):turn_to(90*facing)
+end
+
+-- current algorithm returns suboptimal results for rectangular regions
+function ScenarioModderServices:_get_perturbation_grid_spacing(width, length, quantity)
+   local min_dimension = math.min(width, length)
+   local num_divisions = math.ceil(math.sqrt(quantity))
+   local grid_spacing = math.floor(min_dimension / num_divisions)
+   return grid_spacing
 end
 
 return ScenarioModderServices
