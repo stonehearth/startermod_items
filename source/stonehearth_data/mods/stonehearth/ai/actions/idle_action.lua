8fc460294703d461c16d19e46a0804ce0e886b6d
Tom Cannon <tom@radiant-entertainment.com>
2014-02-21 09:23:54 -0800

    More tuning to wander frequency

--- source/stonehearth_data/mods/stonehearth/ai/actions/idle_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/idle_action.lua
@@ -9,17 +9,11 @@ Idle.priority = 1
 Idle.preemptable = true
 
 function Idle:run(ai, entity)
-   local countdown = rng:get_int(0, 1)
+   local countdown = rng:get_int(1, 2)
    while true do
       if countdown <= 0 then
-
-         if rng:get_int(0, 1) == 1 then
-            ai:execute('stonehearth:wander', { radius = 5, radius_min = 2 })
-         else 
-            ai:execute('stonehearth:idle:bored')
-         end
-
-         countdown = rng:get_int(0, 1)
+         ai:execute('stonehearth:idle:bored')
+         countdown = rng:get_int(1, 2)
       else
          ai:execute('stonehearth:idle:breathe')
          if not radiant.entities.is_carrying(entity) then
