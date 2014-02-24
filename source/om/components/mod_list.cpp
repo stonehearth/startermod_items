0f0251ca848297ca03f2bfc23737f66fe32b5bbc
Tony Cannon <tony@radiant-entertainment.com>
2014-02-24 11:13:06 -0800

    a new component to track all mods running in the current game

---
+++ source/om/components/mod_list.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "mod_list.ridl.h"
+
+using namespace ::radiant;
+using namespace ::radiant::om;
+
+std::ostream& operator<<(std::ostream& os, ModList const& o)
+{
+   return (os << "[ModList]");
+}
+
+
+void ModList::ConstructObject()
+{
+}
+
+void ModList::ExtendObject(json::Node const& obj)
+{
+}
---
+++ source/om/components/mod_list.ridl.py
@@ -0,0 +1,16 @@
+from ridl.om_types import *
+import ridl.ridl as ridl
+import ridl.c_types as c
+import ridl.std_types as std
+import ridl.dm_types as dm
+
+class DataStore(dm.Record):
+   name = 'DataStore'
+
+class ModList(Component):
+   _generate_construct_object = True
+
+   mods = dm.Map(std.string(), std.shared_ptr(DataStore()), singular_name='mod', add='define', remove=None, iterate='define')
+
+   _includes = [ "om/components/data_store.ridl.h" ]
+
