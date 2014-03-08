dc06914276084bb3c2452ed7ced93a888ee7cb33
Tony Cannon <tony@radiant-entertainment.com>
2014-03-07 16:10:05 -0800

    fix the inventory component

--- source/stonehearth_data/mods/stonehearth/components/inventory/inventory_component.lua
+++ source/stonehearth_data/mods/stonehearth/components/inventory/inventory_component.lua
@@ -2,39 +2,41 @@ local InventoryComponent = class()
 
 function InventoryComponent:__create(entity, json)
    self._entity = entity
-   self._item_entities = {}
+   self._items = {
+      __numeric = true
+   }
    self._capacity = json.capacity or 4
-   self.__save_state = radiant.create_datastore({
-         items = self._item_entities,
+   self.__savestate = radiant.create_datastore({
+         items = self._items,
          capacity = self._capacity,
       })
 end
    
 function InventoryComponent:add_item(item)
    if not self:is_full() then
-      table.insert(self._item_entities, item)
-      self.__save_state:mark_changed()
+      table.insert(self._items, item)
+      self.__savestate:mark_changed()
    end
 end
 
 function InventoryComponent:get_items()
-   return self._item_entities
+   return self._items
 end
 
 function InventoryComponent:is_empty()
-   return #self._item_entities == 0
+   return #self._items == 0
 end
 
 function InventoryComponent:is_full()
-   return #self._item_entities == self._data.capacity
+   return #self._items == self._capacity
 end
 
 function InventoryComponent:remove_first_item()
    local item = nil
 
    if not self:is_empty() then
-      item = table.remove(self._item_entities, 1)
-      self.__save_state:mark_changed()
+      item = table.remove(self._items, 1)
+      self.__savestate:mark_changed()
    end
    
    return item
