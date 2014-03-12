0557079735762dce3e5f3042ebe31040eb892faa
Albert Wang <albert@radiant-entertainment.com>
2014-03-11 17:52:54 -0700

    Remove log spam from the fixed trapped_beast_call_handler.

--- source/stonehearth_data/mods/stonehearth/call_handlers/unit_control/trapped_beast_call_handler.lua
+++ source/stonehearth_data/mods/stonehearth/call_handlers/unit_control/trapped_beast_call_handler.lua
@@ -4,7 +4,6 @@ local log = radiant.log.create_logger('pets')
 local TrappedBeastCallHandler = class()
 
 function TrappedBeastCallHandler:harvest_trapped_beast(session, response, entity, trap_entity)
-   log:info('harvest_trapped_beast called')
    if not radiant.util.is_a(entity, Entity) or not radiant.util.is_a(trap_entity, Entity) then
       -- entity no longer exists
       return false
@@ -19,7 +18,6 @@ function TrappedBeastCallHandler:harvest_trapped_beast(session, response, entity
 end
 
 function TrappedBeastCallHandler:tame_trapped_beast(session, response, entity, trap_entity)
-   log:info('tame_trapped_beast called')
    if not radiant.util.is_a(entity, Entity) or not radiant.util.is_a(trap_entity, Entity) then
       -- entity no longer exists
       return false
