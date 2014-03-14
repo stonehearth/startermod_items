1d4e129bfad1deb5be78762023ef7c91cd82eb0c
Stephanie <stephanie@radiant-entertainment.com>
2014-03-14 12:43:02 -0700

    Get an overlay when planting. Overlay is specific to the type of plant.

--- source/stonehearth_data/mods/stonehearth/services/farming/farming_service.lua
+++ source/stonehearth_data/mods/stonehearth/services/farming/farming_service.lua
@@ -2,6 +2,7 @@ FarmingService = class()
 
 function FarmingService:__init()
    self:_load_dirt_descriptions()
+   self._crop_overlays = {}
 end
 
 function FarmingService:initialize()
@@ -35,16 +36,27 @@ function FarmingService:plant_crop(faction, soil_plots, crop_type)
    local town = stonehearth.town:get_town(faction)
    for i, plot in ipairs(soil_plots) do
       --TODO: store these tasks, so they can be cancelled
+      local overlay_effect = self:_get_overlay_for_crop(crop_type)
       town:create_farmer_task('stonehearth:plant_crop', {target_plot = plot, 
                                                          dirt_plot_component = plot:get_component('stonehearth:dirt_plot'),
                                                          crop_type = crop_type})
                               :set_source(plot)
-                              --TODO: :add_entity_effect(plot, effect_name)
+                              :add_entity_effect(plot, overlay_effect)
                               :set_name('plant_crop')
                               :once()
                               :start()
    end
    return true
+end
+
+function FarmingService:_get_overlay_for_crop(crop_type)
+   local overlay = self._crop_overlays[crop_type]
+   if not overlay then
+      local crop_data = radiant.resources.load_json(crop_type)
+      self._crop_overlays[crop_type] = crop_data.components['stonehearth:crop'].plant_overlay_effect
+      overlay = self._crop_overlays[crop_type]
+   end
+   return overlay
 end
 
 function FarmingService:harvest_crops(faction, soil_plots)
