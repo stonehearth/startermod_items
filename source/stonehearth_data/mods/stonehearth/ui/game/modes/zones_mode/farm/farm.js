2616a06803904ddd736669d20e4751effe308293
Tom Cannon <tom@radiant-entertainment.com>
2014-03-14 16:21:33 -0700

    Add crop rotation widget.

--- source/stonehearth_data/mods/stonehearth/ui/game/modes/zones_mode/farm/farm.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/modes/zones_mode/farm/farm.js
@@ -11,14 +11,8 @@ App.StonehearthFarmView = App.View.extend({
       this._super();
       this.set('foo', 'hello world');
 
-      this.set('crop_rotation', [
-         {
-            crop: 'corn',
-         },
-         {
-            crop: 'turnips',
-         }         
-      ]);
+      //xxx placeholder
+      this.set('crop_rotation', []);
    },
 
    didInsertElement: function() {
@@ -53,15 +47,31 @@ App.StonehearthFarmView = App.View.extend({
       this._super();
    },
 
+   addCropToRotation: function(cropId) {
+      var crops = this.get('context.crop_rotation') || [];
+
+      crops.push({ crop: cropId});
+
+      this.set('context.crop_rotation', crops);
+      console.log(this.get('crop_rotation'));
+   },
+
    actions :  {
-      addCrop: function() {
+      addCropButtonClicked: function() {
+         var self = this;
          palette = App.gameView.addView(App.StonehearthFarmCropPalette, { 
+                     click: function(item) {
+                        var cropId = $(item).attr('crop')
+                        self.addCropToRotation($(item).attr('crop'));
+                     },
                      context: {
                         items : [
                            {
+                              crop: "corn",
                               icon: "..."
                            },
                            {
+                              crop: "turnip",
                               icon: "..."
                            }
                         ]
@@ -74,142 +84,21 @@ App.StonehearthFarmView = App.View.extend({
                   });
       },
    },
-
-   _selectAll : function() {
-      //this.taxonomyGrid.find('#all').click();
-      this.items.addClass('on');
-      this._setStockpileFilter();
-   },
-
-   _selectNone : function() {
-      //this.taxonomyGrid.find('#none').click();
-      this.items.removeClass('on');
-      this._setStockpileFilter();
-   },
-
-   _refreshGrids : function() {
-      if (!this.items) {
-         return;
-      }
-
-      var self = this;
-      var stockpileFilter = this.get('context.stonehearth:stockpile.filter');
-
-      $('.category').removeClass('on');
-
-      if (stockpileFilter) {
-         this.items.each(function(i, element) {
-            var button = $(element)
-            var buttonFilter = button.attr('filter')
-
-            if (!buttonFilter) {
-               console.log('button ' + button.attr('id') + " has no filter!")
-            } else {
-               $.each(stockpileFilter, function(i, filter) {
-                  if (buttonFilter == filter) {
-                     button.addClass('on');
-                  }
-               });
-            }
-
-         });
-
-         this._updateGroups();
-      } else {
-         this.allButton.attr('checked', 'checked');
-         this.groups.attr('checked', 'checked');
-         this.items.addClass('on');
-      }
-
-   }.observes('context.stonehearth:stockpile.filter'),
-
-   _toggleGroup : function(element) {
-      var foo = element.attr('checked');
-      var on = !(element.attr('checked') == 'checked');
-
-      element.parent().siblings().each(function(i, sibling) {
-         var category = $(sibling);
-
-         if (category.hasClass('category')) {
-            if (on) {
-               category.addClass('on');
-            } else {
-               category.removeClass('on');
-            }
-         }
-      });
-
-      this._setStockpileFilter();
-   },
-
-   _itemClick : function(element) {
-      this._setStockpileFilter();
-   },
-
-   _setStockpileFilter : function() {
-      var values = [];
-      $('#taxonomyGrid').find('.on').each(function(i, element) {
-         if ($(element).attr('filter')) {
-            values.push($(element).attr('filter'));
-         }
-      });
-
-      radiant.call('stonehearth:set_stockpile_filter', this.uri, values)
-         .done(function(response) {
-            console.log(response)
-         });
-   },
-
-   _updateGroups : function() {
-      var self = this;
-
-      var allGroupsOn = true;
-      var noGroupsOn = true;
-
-      this.groups.each(function (i, group) {
-         var allOn = true;
-         $(group).parent().siblings().each(function(i, sibling) {
-            var category = $(sibling);
-
-            if (category.hasClass('category')) {
-               if (category.hasClass('on')) {
-                  noGroupsOn = false;
-               } else {
-                  allOn = false;
-                  allGroupsOn = false;
-               }
-            }
-         });
-
-         if (allOn) {
-            $(group).attr('checked', 'checked')
-         } else {
-            $(group).removeAttr('checked')
-         }         
-
-      });
-
-      if (allGroupsOn) {
-         self.allButton.attr('checked', 'checked')
-         //self.items.removeClass('on');
-         //self.groups.removeClass('on');
-      } else {
-         self.allButton.removeAttr('checked')
-      }
-
-      if (noGroupsOn) {
-         self.noneButton.attr('checked', 'checked')
-         //self.items.removeClass('on');
-         //self.groups.removeClass('on');
-      } else {
-         self.noneButton.removeAttr('checked')
-      }
-   }
-
 });
 
 App.StonehearthFarmCropPalette = App.View.extend({
    templateName: 'stonehearthCropPalette',
    modal: true,
 
+   didInsertElement: function() {
+      this._super();
+      var self = this;
+
+      this.$('.item').click(function() {
+         if (self.click) {
+            self.click($(this));
+         }
+         self.destroy();
+      });
+   }
 });
