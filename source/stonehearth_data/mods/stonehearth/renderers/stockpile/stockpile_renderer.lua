4dc6ff5ce53175daeee1c2d7819c176a3632067b
Chris Klochek <klochek@radiant-entertainment.com>
2014-03-25 17:22:56 -0700

    Items go semi-transparent when entering zone view!  Woohoo!

--- source/stonehearth_data/mods/stonehearth/renderers/stockpile/stockpile_renderer.lua
+++ source/stonehearth_data/mods/stonehearth/renderers/stockpile/stockpile_renderer.lua
@@ -7,16 +7,39 @@ local Point2 = _radiant.csg.Point2
 local StockpileRenderer = class()
 
 function StockpileRenderer:__init()
-   self._ui_view_mode = 'normal'
    self._color = Color3(0, 153, 255)
+   self._ui_view_mode = 'normal'
+   self._stockpile_items = {}
 
    radiant.events.listen(radiant.events, 'stonehearth:ui_mode_changed', function(e)
       if self._ui_view_mode ~= e.mode then
-         self._color = Color3(255 - self._color.r, self._color.g, self._color.b)
-         self:_regenerate_node()
-         self._ui_view_mode = e.mode
+         if e.mode == 'normal' or e.mode == 'zones' then
+            self._ui_view_mode = e.mode
+
+            self:_update_item_renderers(e.mode, self._stockpile_items)
+
+            self._color = Color3(255 - self._color.r, self._color.g, self._color.b)
+            self:_regenerate_node()
+         end
       end
    end)
+end
+
+function StockpileRenderer:_update_item_renderers(mode, item_map)
+   if item_map == nil then
+      return
+   end
+   for id, item in pairs(item_map) do
+      local f = _radiant.client.get_render_entity(item)
+      f:set_material_override(self:_mode_to_material_kind(mode))
+   end
+end
+
+function StockpileRenderer:_mode_to_material_kind(mode)
+   if mode == 'normal' then
+      return 'default'
+   end
+   return mode
 end
 
 function StockpileRenderer:update(render_entity, saved_variables)
@@ -37,6 +60,28 @@ function StockpileRenderer:update(render_entity, saved_variables)
    self:_update()
 end
 
+function StockpileRenderer:_diff_and_update_item_renderers(updated_items)
+   local added_items = {}
+   local temp_items = {}
+
+   for id, item in pairs(self._stockpile_items) do
+      temp_items[id] = item
+   end
+
+   for id, item in pairs(updated_items) do
+      if temp_items[id] == nil then
+         added_items[id] = item
+      end
+      temp_items[id] = nil
+   end
+
+   -- Rename for clarity!
+   local removed_items = temp_items
+
+   self:_update_item_renderers(self._ui_view_mode, added_items)
+   self:_update_item_renderers('', removed_items)
+end
+
 --- xxx: someone call destroy please!!
 function StockpileRenderer:destroy()
    self:_clear()
@@ -45,6 +90,9 @@ end
 function StockpileRenderer:_update()
    local data = self._savestate:get_data()
    if data and data.size then
+      self:_diff_and_update_item_renderers(data.stocked_items);
+      self._stockpile_items = data.stocked_items
+
       local size = data.size
       if self._size ~= size then
          self._size = size
