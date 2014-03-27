c887a296d6a1eae2d81bb3ff6742a4cc3ae10df0
Tony Cannon <tony@radiant-entertainment.com>
2014-03-27 11:20:00 -0700

    fix typo.  oops!

--- source/stonehearth_data/mods/stonehearth/services/client/renderer/renderer_service.lua
+++ source/stonehearth_data/mods/stonehearth/services/client/renderer/renderer_service.lua
@@ -23,20 +23,20 @@ function Renderer:_install_regions()
    if not self._installing_regions then
       self._installing_regions = true
       radiant.events.listen(radiant, 'stonehearth:gameloop', function()
-         self._installing_regions = true
-         if not self._visible_region_installed then
-            self._visible_region_installed = _radiant.renderer.visibility.set_visible_region(self._sv.visible_region_uri)
-         end
-         if not self._explored_region_installed then
-            self._explored_region_installed = _radiant.renderer.visibility.set_explored_region(self._sv.explored_region_uri)
-         end
+            self._installing_regions = true
+            if not self._visible_region_installed then
+               self._visible_region_installed = _radiant.renderer.visibility.set_visible_region(self._sv.visible_region_uri)
+            end
+            if not self._explored_region_installed then
+               self._explored_region_installed = _radiant.renderer.visibility.set_explored_region(self._sv.explored_region_uri)
+            end
 
-         -- both registered!  unregister the callback.
-         if self._visible_region_installed and self._explored_region_installed then
-            self._installing_regions = false
-            return radiant.events.UNLISTEN
-         end
-      end
+            -- both registered!  unregister the callback.
+            if self._visible_region_installed and self._explored_region_installed then
+               self._installing_regions = false
+               return radiant.events.UNLISTEN
+            end
+         end)
    end
 end
 
