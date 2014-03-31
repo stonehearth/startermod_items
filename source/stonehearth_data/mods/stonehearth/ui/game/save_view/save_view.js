3e2510014c8fb5e89b69a8016fd868b762c05470
Tom Cannon <tom@radiant-entertainment.com>
2014-03-31 12:54:21 -0700

    Add support for deleting saves, and refresh the save list properly.

--- source/stonehearth_data/mods/stonehearth/ui/game/save_view/save_view.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/save_view/save_view.js
@@ -1,5 +1,23 @@
 
-App.StonehearthSaveView = App.View.extend({
+// Common functionality between the save and load views
+App.StonehearthSaveLoadView = App.View.extend({
+
+   getListView: function() {
+      // a bit of a hack. grab the Ember View for the list from its element
+      var viewEl = this.$('#saves').children()[0]
+      var id = $(viewEl).attr('id');
+
+      return Ember.View.views[id];
+   },
+
+   refreshList: function() {
+      var list = this.getListView();
+      list.refresh();
+   }
+
+});
+
+App.StonehearthSaveView = App.StonehearthSaveLoadView.extend({
    templateName: 'saveView',
    classNames: ['flex'],
    modal: true,
@@ -16,23 +34,50 @@ App.StonehearthSaveView = App.View.extend({
             of: '#modalOverlay'
          });
 
+      this.$('.saveList').on( 'click', '.row', function() {
+         console.log($(this).attr('key'));
+      });
    },
 
    actions: {
       saveGame: function() {
-         radiant.call("radiant:client:save_game", "slot_0", { 
+         var self = this;
+         var d = new Date();
+         var t = d.getTime();
+         var gameDate = App.gameView.getDate().date 
+         var gameTime = App.gameView.getDate().time;
+
+         radiant.call("radiant:client:save_game", String(t), { 
                name: "Save game name",
                town_name: "Town name goes here",
-               game_time: "game time goes here",
-               time: 1337
+               game_date: gameDate,
+               game_time: gameTime,
+               time: d.toLocaleString()
             })
+            .always(function() {
+               self.refreshList();
+            });
       },
+
+      deleteSaveGame: function() {
+         var self = this;
+         var key = this.$('.saveList')
+                       .find('.selected')
+                       .attr('key');
+
+         if (key) {
+            radiant.call("radiant:client:delete_save_game", String(key))
+               .always(function() {
+                  self.refreshList();
+               });
+         }
+      }
    }
 
 });
 
 
-App.StonehearthLoadView = App.View.extend({
+App.StonehearthLoadView = App.StonehearthSaveLoadView.extend({
    templateName: 'loadView',
    classNames: ['flex'],
    modal: true,
@@ -53,7 +98,15 @@ App.StonehearthLoadView = App.View.extend({
 
    actions: {
       loadGame: function() {
-         radiant.call("radiant:client:load_game", "slot_0");
+         var self = this;
+         var key = this.getListView().getSelectedKey();
+
+         if (key) {
+            radiant.call("radiant:client:load_game", key)
+               .always(function() {
+                     self.destroy();
+               });
+         }
       },
    }
 
@@ -70,6 +123,11 @@ App.StonehearthSaveListView = App.View.extend({
 
       this.set('context', {});
 
+      this.refresh();
+   },
+
+   refresh: function() {
+      var self = this;
       var saveKey = App.stonehearthClient.gameState.saveKey;
 
       radiant.call("radiant:client:get_save_games")
@@ -90,9 +148,22 @@ App.StonehearthSaveListView = App.View.extend({
             });
 
             self.set('context.saves', vals);
-         })
+         })      
    },
 
+   getSelectedKey: function() {
+      return this.$().find('.selected').attr('key');
+   },
+
+   didInsertElement: function() {
+      var self = this;
+      this._super();
+
+      this.$().on( 'click', '.row', function() {
+         self.$().find('.row').removeClass('selected');
+         $(this).addClass('selected');
+      });
+   },
 
    actions: {
 
