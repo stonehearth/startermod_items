d4f9371dbebb38b57985d6aa9fff902f3f8364db
Albert Wang <albert@radiant-entertainment.com>
2014-04-03 11:48:09 -0700

    Fix drop_carrying_action. Parameterize go to/toward location to optionally stop on the adjacent cell.

--- source/stonehearth_data/mods/stonehearth/ai/actions/create_proxy_entity.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/create_proxy_entity.lua
@@ -4,13 +4,17 @@ local Region3 = _radiant.csg.Region3
 local Entity = _radiant.om.Entity
 local CreateProxyEntity = class()
 
-CreateProxyEntity.name = 'create proxy'
+CreateProxyEntity.name = 'create proxy entity'
 CreateProxyEntity.does = 'stonehearth:create_proxy_entity'
 CreateProxyEntity.args = {
-   location = Point3,     -- location to stick it
+   location = Point3,
+   use_default_adjacent_region = {
+      type = 'boolean',
+      default = false,
+   }
 }
 CreateProxyEntity.think_output = {
-   entity = Entity        -- the proxy entity
+   entity = Entity   -- the proxy entity
 }
 CreateProxyEntity.version = 2
 CreateProxyEntity.priority = 1
@@ -33,31 +37,17 @@ end
 local origin_region = create_origin_region()
 
 function CreateProxyEntity:start_thinking(ai, entity, args)
-   -- generally speaking, going directly to a location is a strange
-   -- thing to do.  why did we not path find to an entity?  why is
-   -- this location special?
-   
-   -- Commentary: No it's not. It's useful when coding ambient behavior
-   -- like wandering around, running away, etc. - tom, 9/23/2013
-
-   -- Commenary: Yes it is. =) You don't really know if you can get
-   -- to an arbitray location in the world.  For example, if you try to
-   -- goto a location inside a box, the pathfinder will just bail with
-   -- no solution.  It makes more sense to have a vector based controller
-   -- to implement ambient behavior. - tony, 10/01/2013
-
-   -- anyway, the pathfinder can only find paths between two entities,
-   -- so go ahead and make a new one.
-   
    self:stop()
 
    self._proxy_entity = radiant.entities.create_entity()
-   self._proxy_entity:set_debug_text('go towards location proxy entity')
+   self._proxy_entity:set_debug_text('go to/toward location proxy entity')
 
-   -- make the adjacent the same as the entity location, so that we actually walk to the entity location
-   local destination = self._proxy_entity:add_component('destination')
-   destination:set_region(origin_region)
-   destination:set_adjacent(origin_region)
+   if not args.use_default_adjacent_region then
+      -- make the adjacent the same as the entity location, so that we actually go to the entity location
+      local destination = self._proxy_entity:add_component('destination')
+      destination:set_region(origin_region)
+      destination:set_adjacent(origin_region)
+   end
 
    radiant.terrain.place_entity(self._proxy_entity, args.location)
 
--- source/stonehearth_data/mods/stonehearth/ai/actions/run_to_location_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/run_to_location_action.lua
@@ -7,12 +7,21 @@ local RunToLocation = class()
 RunToLocation.name = 'run to location'
 RunToLocation.does = 'stonehearth:goto_location'
 RunToLocation.args = {
-   location = Point3,     -- location to go to
+   location = Point3,
+   stop_when_adjacent = {
+      type = 'boolean',   -- whether to stop adjacent to destination
+      default = false,
+   }
 }
 RunToLocation.version = 2
 RunToLocation.priority = 1
 
 local ai = stonehearth.ai
 return ai:create_compound_action(RunToLocation)
-         :execute('stonehearth:create_proxy_entity', { location = ai.ARGS.location })
-         :execute('stonehearth:goto_entity', { entity = ai.PREV.entity })
+         :execute('stonehearth:create_proxy_entity', {
+            location = ai.ARGS.location,
+            use_default_adjacent_region = ai.ARGS.stop_when_adjacent,
+         })
+         :execute('stonehearth:goto_entity', {
+            entity = ai.PREV.entity
+         })
