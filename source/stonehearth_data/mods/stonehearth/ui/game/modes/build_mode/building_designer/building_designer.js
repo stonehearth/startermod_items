c7358f723b9c9afc92258f9b9b69ebbfbd3f9eaa
Tom Cannon <tom@radiant-entertainment.com>
2014-04-04 09:59:55 -0700

    Add the building designer and hang it from a new button on the start menu.

---
+++ source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_designer/building_designer.js
@@ -0,0 +1,14 @@
+App.StonehearthBuildingDesignerView = App.View.extend({
+	templateName: 'buildingDesigner',
+   i18nNamespace: 'stonehearth',
+   classNames: ['fullScreen'],
+
+   init: function() {
+      this._super();
+   },
+
+   didInsertElement: function() {
+      this._super();
+   },
+
+});
