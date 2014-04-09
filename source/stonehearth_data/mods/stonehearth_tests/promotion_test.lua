250a1c47ea58ef3e71a27ac1d1950337bcd15f3f
Stephanie <stephanie@radiant-entertainment.com>
2014-04-08 18:41:57 -0700

    added a stockpile to promotion test to provoke the abort bug

--- source/stonehearth_data/mods/stonehearth_tests/promotion_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/promotion_test.lua
@@ -11,7 +11,7 @@ function PromoteTest:__init()
    self:create_world()
 
    --Create the saw 
-   self:place_item('stonehearth:carpenter:saw', 4, 0)
+   self:place_item('stonehearth:carpenter:saw', -12, -12)
 
    self:place_citizen(-1,2)
    local worker = self:place_citizen(12, 12)
@@ -20,6 +20,12 @@ function PromoteTest:__init()
    
    local tree = self:place_tree(-8, 0)
    local tree2 = self:place_tree(-8, 8)
+
+   local player_id = radiant.entities.get_player_id(worker)
+
+   self:at(10,  function()
+         self:place_stockpile_cmd(player_id, 12, 12, 4, 4)
+      end)
 end
 
 return PromoteTest
