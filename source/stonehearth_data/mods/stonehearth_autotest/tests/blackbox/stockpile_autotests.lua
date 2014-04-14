3b1df226152547aa0b596452b5ceb35203f96d6d
Stephanie <stephanie@radiant-entertainment.com>
2014-04-14 15:09:03 -0700

    Test for storing multiple things in the stockpile

--- source/stonehearth_data/mods/stonehearth_autotest/tests/blackbox/stockpile_autotests.lua
+++ source/stonehearth_data/mods/stonehearth_autotest/tests/blackbox/stockpile_autotests.lua
@@ -14,4 +14,26 @@ function stockpile_tests.basic_restock(autotest)
    autotest:fail('worker failed to put log in stockpile')
 end
 
+---[[
+--Fails to add second wood, in the broken bucket now
+function stockpile_tests.restock_twice(autotest)
+   autotest.env:create_person(2, 2, { profession = 'worker' })
+   autotest.env:create_entity(4, 4, 'stonehearth:oak_log')
+   autotest.env:create_entity(4, 5, 'stonehearth:oak_log')
+
+   local stockpile = autotest.env:create_stockpile(-2, -2)
+   local counter = 0
+   radiant.events.listen(stockpile, 'stonehearth:item_added', function()
+         counter = counter + 1
+         if counter == 2 then
+            autotest:success()
+         end
+         return radiant.events.UNLISTEN
+      end)
+
+   autotest:sleep(10000)
+   autotest:fail('worker did not put all the logs in the stockpile')
+end
+--]]
+
 return stockpile_tests
