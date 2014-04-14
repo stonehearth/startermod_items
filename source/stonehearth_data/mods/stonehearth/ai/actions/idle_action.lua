e1871ac25dd201397c7a79402ddfafbf52de7112
Tony Cannon <tony@radiant-entertainment.com>
2014-04-14 09:17:35 -0700

    go back to the old method of idling until I can figure out why "drop carrying when idle" is busted.

--- source/stonehearth_data/mods/stonehearth/ai/actions/idle_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/idle_action.lua
@@ -11,8 +11,28 @@ Idle.preemptable = true
 function Idle:run(ai, entity)
    while true do
       local delay = rng:get_int(1, 2)
+
+      if radiant.entities.is_carrying(entity) then
+         -- we usually only end up carrying something while idling when some
+         -- unexpected circumstance caused an action to exit early (e.g. deleting
+         -- a stockpile while someone is carrying an item to it).  there might not
+         -- be another action that wants to do anything with us while we're carrying
+         -- this thing, so drop it on the first bored cycle.  wait an extra long time,
+         -- just in case there's something we *could* do, but it will take a long time
+         -- to figure out.
+         delay = delay + 5
+      end
+
       for i = 1, delay do
          ai:execute('stonehearth:idle:breathe')
+      end
+
+      if radiant.entities.is_carrying(entity) then
+         -- still carrying?  we must be doomed!  go ahead and drop it.
+         ai:execute('stonehearth:wander_within_leash', { radius = 3 })
+         ai:execute('stonehearth:drop_carrying_now')
+      else
+         ai:execute('stonehearth:idle:bored')
       end
       ai:execute('stonehearth:idle:bored')
    end
