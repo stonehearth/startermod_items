05a7fcc34a29a4a3be2a9229b4ffd52ed7c37ccf
Chris Klochek <klochek@radiant-entertainment.com>
2014-04-21 12:50:26 -0700

    Fix for bad entity object in reserveentity.

--- source/stonehearth_data/mods/stonehearth/ai/actions/reserve_entity.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/reserve_entity.lua
@@ -10,7 +10,7 @@ ReserveEntity.version = 2
 ReserveEntity.priority = 1
 
 function ReserveEntity:start_thinking(ai, entity, args)
-   if not stonehearth.ai:can_acquire_ai_lease(args.entity, self._entity) then
+   if not stonehearth.ai:can_acquire_ai_lease(args.entity, entity) then
       self._log:debug('ignoring %s (cannot acquire ai lease)', args.entity)
       return
    end
