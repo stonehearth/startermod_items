4ef5683f22d76a6e49fffafb26933ae906081404
Tony Cannon <tony@radiant-entertainment.com>
2014-04-22 15:49:37 -0700

    render the adjacent region, too

--- source/client/renderer/render_destination.cpp
+++ source/client/renderer/render_destination.cpp
@@ -16,9 +16,11 @@ RenderDestination::RenderDestination(const RenderEntity& entity, om::Destination
       if (enabled) {
          RenderDestinationRegion(REGION, destination_->TraceRegion("debug rendering", dm::RENDER_TRACES), csg::Color4(128, 128, 128, 128));
          RenderDestinationRegion(RESERVED, destination_->TraceReserved("debug rendering", dm::RENDER_TRACES), csg::Color4(0, 255, 0, 128));
+         RenderDestinationRegion(ADJACENT, destination_->TraceReserved("debug rendering", dm::RENDER_TRACES), csg::Color4(255, 0, 0, 128));
       } else {
          RemoveDestinationRegion(REGION);
          RemoveDestinationRegion(RESERVED);
+         RemoveDestinationRegion(ADJACENT);
       }
    });
 }
