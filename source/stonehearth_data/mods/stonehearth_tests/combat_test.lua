85ae9ee98551cc0e65e6f4a0b5854085983c0243
Albert Wang <albert@radiant-entertainment.com>
2014-04-30 16:52:52 -0700

    Flesh out goblin AI.

--- source/stonehearth_data/mods/stonehearth_tests/combat_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/combat_test.lua
@@ -1,4 +1,5 @@
 local MicroWorld = require 'lib.micro_world'
+local Point3 = _radiant.csg.Point3
 local rng = _radiant.csg.get_default_rng()
 
 local CombatTest = class(MicroWorld)
@@ -6,17 +7,15 @@ local CombatTest = class(MicroWorld)
 function CombatTest:__init()
    self[MicroWorld]:__init()
    self:create_world()
+   self:create_enemy_population()
 
-   local faction = 'civ'
+   -- for i = -10, 10 do
+   --    self:place_item('stonehearth:small_boulder', i, 0)
+   -- end
 
-   local enemy = self:place_citizen(15, 15, 'trapper')
-   enemy:add_component('unit_info'):set_faction('raider')
+   local enemy1 = self:place_enemy(0, 2)
 
-   for i = -10, 10 do
-      self:place_item('stonehearth:small_boulder', i, 0)
-   end
-
-   local worker1 = self:place_citizen(-15, -15, 'carpenter')
+   local worker1 = self:place_citizen(-15, -15)
    local worker2 = self:place_citizen(-15,  15)
    -- local worker3 = self:place_citizen( 15, -15)
    -- local worker4 = self:place_citizen(-10, -10)
@@ -25,7 +24,7 @@ function CombatTest:__init()
    -- local worker7 = self:place_citizen( 10,  10)
    -- local worker8 = self:place_citizen(  1,   1)
 
-   --self:_equip_weapon(enemy)
+   --self:_equip_weapon(enemy1)
    --self:_equip_weapon(worker1)
    --self:_equip_weapon(worker2)
 
@@ -37,9 +36,26 @@ function CombatTest:__init()
    -- )
 end
 
-function CombatTest:_equip_weapon(entity)
+function CombatTest:equip_weapon(entity)
    local equipment = entity:add_component('stonehearth:equipment')
    equipment:equip_item('stonehearth:wooden_sword')
+end
+
+function CombatTest:create_enemy_population()
+   local session = {
+      player_id = 'game_master',
+      faction = 'raider',
+      kingdom = 'stonehearth:kingdoms:golden_conquering_arm'
+   }
+
+   self._enemy_population = stonehearth.population:add_population(session)
+end
+
+function CombatTest:place_enemy(x, z)
+   local enemy = self._enemy_population:create_new_citizen()
+   enemy:add_component('unit_info'):set_faction('raider')
+   radiant.terrain.place_entity(enemy, Point3(x, 1, z))
+   return enemy
 end
 
 return CombatTest
