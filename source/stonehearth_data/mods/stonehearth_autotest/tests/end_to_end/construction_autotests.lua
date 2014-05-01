d634b1c2aa5b29cf2b5751c739e2c8e59c71c12e
Stephanie <stephanie@radiant-entertainment.com>
2014-05-01 16:14:26 -0700

    Fix construction autotest to work with pause in modes menu

--- source/stonehearth_data/mods/stonehearth_autotest/tests/end_to_end/construction_autotests.lua
+++ source/stonehearth_data/mods/stonehearth_autotest/tests/end_to_end/construction_autotests.lua
@@ -41,8 +41,12 @@ function construction_tests.simple_build(autotest)
    autotest.ui:click_dom_element('#startMenu #build_menu')
    autotest.ui:click_dom_element('#startMenu div[hotkey="l"]')
 
+
    autotest.ui:click_terrain(2, -2)
    autotest.ui:click_terrain(-4, -2)
+
+   autotest.ui:click_dom_element('.stonehearthMenu .close')
+
 
    autotest:sleep(3*60*1000)
    autotest:fail('failed to construction')
