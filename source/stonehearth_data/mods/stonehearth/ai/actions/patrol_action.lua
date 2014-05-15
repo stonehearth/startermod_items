84a9f181443b463149384b42604905f5dd6d2c23
Albert Wang <albert@radiant-entertainment.com>
2014-05-15 00:13:54 -0700

    Footmen now pretend to be useful by patrolling stockpiles so they don't have to do real work.

---
+++ source/stonehearth_data/mods/stonehearth/ai/actions/patrol_action.lua
@@ -0,0 +1,24 @@
+local Entity = _radiant.om.Entity
+
+local Patrol = class()
+
+Patrol.name = 'patrol'
+Patrol.does = 'stonehearth:patrol'
+Patrol.args = {
+   waypoint = Entity,
+}
+Patrol.version = 2
+Patrol.priority = 1
+
+local ai = stonehearth.ai
+return ai:create_compound_action(Patrol)
+         :execute('stonehearth:set_posture', { posture = 'combat' })
+         :execute('stonehearth:goto_entity', {
+            entity = ai.ARGS.waypoint,
+            stop_distance = 6,
+         })
+         :execute('stonehearth:walk_around_entity', {
+            target = ai.ARGS.waypoint,
+            distance = 3,
+         })
+         :execute('stonehearth:unset_posture', { posture = 'combat' })
