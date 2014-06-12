4b0f4c1fe1db255f5e0a172249113c2f377b1d88
Tom Cannon <tom@radiant-entertainment.com>
2014-06-12 12:20:21 -0700

    Fix bug in setting uri

--- source/stonehearth_data/mods/stonehearth/ui/game/unit_frame/unit_frame.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/unit_frame/unit_frame.js
@@ -6,7 +6,11 @@ $(document).ready(function(){
 
   $(top).on("radiant_selection_changed.unit_frame", function (_, data) {
      if (!unitFrame.view && App.gameView) {
-        unitFrame.view = App.gameView.addView(App.StonehearthUnitFrameView, { uri: data.selected_entity });
+        unitFrame.view = App.gameView.addView(App.StonehearthUnitFrameView);
+     }
+
+     if (unitFrame.view) {
+      unitFrame.view.set('uri', data.selected_entity);
      }
   });  
 });
