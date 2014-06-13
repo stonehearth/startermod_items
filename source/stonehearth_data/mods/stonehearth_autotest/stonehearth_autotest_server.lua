658ace8040ae0c8e70cfd786ef08a7c56423feba
Tony Cannon <tony@radiant-entertainment.com>
2014-06-13 16:28:10 -0700

    make the world generation guessing code slightly more robust (also works with groups now)!
    remove the entire concept of "world" from the autotest runner, which makes me feel all warm and fuzzy inside.

--- source/stonehearth_data/mods/stonehearth_autotest/stonehearth_autotest_server.lua
+++ source/stonehearth_data/mods/stonehearth_autotest/stonehearth_autotest_server.lua
@@ -1,61 +1,69 @@
 mod = {}
 
 
-function world_from_options(index, options)
-   if options.script then
-      local script_entry = '/' .. options.script
-      for world_name, world in pairs(index.worlds) do
-         for _, group in pairs(world.groups) do
-            if group.scripts ~= nil then
-               for _, script in ipairs(group.scripts) do
-                  if script == script_entry then
-                     return world_name
-                  end
+function parse_options(index, options)
+   local run_world, run_group, run_script
+
+   local function endswith(s, suffix)
+      return s:sub(-suffix:len()) == suffix
+   end
+
+   -- if a group is specified, use that.  otherwise, if a script is specified, use that.
+   -- if neither is specified, use the 'all' group.
+   if options.group then
+      options.script = nil
+   elseif not options.script then
+      options.group = 'all'
+   end
+
+   -- find the world which matches the script or group chosen
+   for world_name, world in pairs(index.worlds) do
+      for group_name, group in pairs(world.groups) do
+         if options.group and group_name == options.group then
+            -- the name of this group matches the one we were looking for.
+            -- run all tests from this group and this world.
+            return world, group, nil
+         end
+
+         if options.script and group.scripts then
+            for _, script in ipairs(group.scripts) do
+               if endswith(script, options.script) then
+                  -- the name of this script matches the one we were looking for.
+                  -- run all tests from this script and this world.
+                  return world, nil, script
                end
             end
          end
       end
-      return 'none'
    end
-
-   local group = options.group
-   if not group then
-      group = 'all'
-   end
-
-   for world_name, world in pairs(index.worlds) do
-      for group_name, group in pairs(world.groups) do
-         if group_name == options.group then
-            return world_name
-         end
-      end
-   end
-
-   return 'none'
 end
 
 radiant.events.listen(mod, 'radiant:new_game', function(args)
       local index = radiant.resources.load_json('stonehearth_autotest/tests/index.json')
       local options = radiant.util.get_config('options', {})
-      local world_kind = world_from_options(index, options)
-      local world_script = index.worlds[world_kind].world_generator
-      autotest_framework.env.create_world(world_script)
+      local world, group, script = parse_options(index, options)
+
+      if not world then
+         radiant.log.write('stonehearth_autotest', 0, 'could not determine world script.  aborting.')
+         radiant.exit(1)
+      end
+      if not group and not script then
+         radiant.log.write('stonehearth_autotest', 0, 'could not determine group or script to execute.  aborting.')
+         radiant.exit(1)
+      end
 
       autotest_framework.set_finish_cb(function(errorcode)
             radiant.exit(errorcode)
          end)
 
+      autotest_framework.env.create_world(world.world_generator)
+
       radiant.set_realtime_timer(3000, function()
-         if options.script then
-            if options['function'] then
-               autotest_framework.run_test(options.script, options['function'])
-            else
-               autotest_framework.run_script(options.script)
-            end
-         elseif options.group then         
-            autotest_framework.run_group(index, options.group)
-         else
-            autotest_framework.run_group(index, 'all')
+         if group then
+            local all_groups = world
+            autotest_framework.run_group(all_groups, group)
+         elseif script then
+            autotest_framework.run_script(script, options['function'])
          end
       end)
    end)
