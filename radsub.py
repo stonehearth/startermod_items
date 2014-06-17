93297a3f0544a3b9e3fdf0957fab26004a2688f7
Chris Klochek <klochek@radiant-entertainment.com>
2014-06-17 11:56:25 -0700

    Push settings flattening into run_remote_autotest (as it should be; removed from the Go slave).  Move arg parsing in radsub over to argparse.

--- radsub.py
+++ radsub.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+import argparse
 import datetime
+import json
 import os
+import re
 import subprocess
 import sys
-import re
-import json
 
 
 def close_test(passed, script, test, time_delta):
@@ -107,13 +108,24 @@ def run_tests():
 
    return_code = 1
 
-   print 'Running Stonehearth autotests with exe at ' + sh_exe_path + '....'
+   print 'Running Stonehearth autotests with exe at ' + sh_exe_path + ' and args: ' + sh_args
 
    return_code = subprocess.call(sh_command, cwd=sh_cwd)
 
    print 'Completed in ' + str((datetime.datetime.now() - t_start).total_seconds()) + ' seconds.'
 
    return return_code
+
+parser = argparse.ArgumentParser(description='Stonehearth Test Runner.')
+parser.add_argument('-d', '--debug', action='store_true', help='runs the tests on the debug version of Stonehearth (local builds only)')
+parser.add_argument('-o', '--official', action='store_true', help='runs the tests assuming the directory structure of an official build')
+parser.add_argument('-f', '--function', help='runs the specified test function (script must be set using --sc!)')
+parser.add_argument('-s', '--script', help='runs the specified test script')
+parser.add_argument('-g', '--group', help='runs the specified test group')
+parser.add_argument('-i', '--interactive', action='store_true', help='run in interactive mode (maximizes the window)')
+parser.add_argument('settings', nargs=argparse.REMAINDER, help='settings to pass to Stonehearth (e.g. --mods.foo.bar=7 --mods.foo.baz=bing)')
+
+args = parser.parse_args(sys.argv[1:])
 
 t_start = datetime.datetime.now()
 
@@ -124,7 +136,7 @@ else:
 
 sh_root = rad_root + 'stonehearth/'
 
-if '-d' in sys.argv:
+if args.debug:
   build_type = 'Debug'
 else:
   build_type = 'RelWithDebInfo'
@@ -133,31 +145,29 @@ sh_build_root = sh_root + 'build/'
 sh_cwd = sh_root + 'source/stonehearth_data/'
 sh_exe_path = sh_build_root + 'source/stonehearth/' + build_type + '/Stonehearth.exe'
 
-if '-o' in sys.argv:
+if args.official:
    # In an official build, look elsewhere for our cwd/exe.
    sh_cwd = './'
    sh_build_root = './'
    sh_exe_path = './Stonehearth.exe'
 
-sh_args = '--game.main_mod=stonehearth_autotest --simulation.game_speed=4 --logging.log_level=1'
+sh_args = '--game.main_mod=stonehearth_autotest --simulation.game_speed=4 --logging.log_level=0'
 
-if '-f' in sys.argv and '-sc' in sys.argv:
-   sh_args += ' --mods.stonehearth_autotest.options.script=' + sys.argv[sys.argv.index('-sc') + 1]
-   sh_args += ' --mods.stonehearth_autotest.options.function=' + sys.argv[sys.argv.index('-f') + 1]
-elif '-sc' in sys.argv:
-   sh_args += ' --mods.stonehearth_autotest.options.script=' + sys.argv[sys.argv.index('-sc') + 1]
-elif '-g' in sys.argv:
-   sh_args += ' --mods.stonehearth_autotest.options.group=' + sys.argv[sys.argv.index('-g') + 1]
+if args.function and args.script:
+   sh_args += ' --mods.stonehearth_autotest.options.script=' + args.script
+   sh_args += ' --mods.stonehearth_autotest.options.function=' + args.function
+elif args.script:
+   sh_args += ' --mods.stonehearth_autotest.options.script=' + args.script
+elif args.group:
+   sh_args += ' --mods.stonehearth_autotest.options.group=' + args.group
 else:
    sh_args += ' --mods.stonehearth_autotest.options.group=all'
 
-if '-i' in sys.argv:
-  print 'Running in interactive mode'
-else:
+if not args.interactive:
   sh_args += ' --renderer.minimized=true'
 
-if '-s' in sys.argv:
-   sh_args += sys.argv[sys.argv.index('-s') + 1]
+if len(args.settings) > 1:
+   sh_args += ' ' + reduce(lambda x,y: x + ' ' + y, args.settings[1:])
 
 return_code = run_tests()
 
--- scripts/test/run_remote_autotest.py
+++ scripts/test/run_remote_autotest.py
@@ -85,9 +85,27 @@ class MultipartPostHandler(urllib2.BaseHandler):
 UPDATE_INTERVAL = 1
 
 
+#NOTE: doesn't handle arrays yet.
+def flatten_json_to_args(json):
+   def flatten_recursive(node, current_path):
+      all_args = ''
+      for key in node:
+         value = node[key]
+         if type(value) is dict:
+            path = ''
+            if current_path != '':
+               path = current_path + '.' + key
+            else:
+               path = key
+            all_args = all_args + flatten_recursive(value, path)
+         else:
+            all_args = all_args + ' --' + current_path + '.' + key + '=' + str(value)
+      return all_args
+   return flatten_recursive(json, '')
+
 
 class TestThread(Thread):
-   def __init__(self, url, machine_name, test_group, test_script, test_function, file_name, settings='{}'):
+   def __init__(self, url, machine_name, test_group, test_script, test_function, file_name, settings={}):
       super(TestThread, self).__init__()
       self.url = url
       self.machine_name = machine_name
@@ -96,7 +114,7 @@ class TestThread(Thread):
       self.test_script = test_script
       self.test_function = test_function
       self.response = None
-      self.settings = settings
+      self.settings = flatten_json_to_args(settings)
 
    def run(self):
       # This should probably be factored, but then this script is also specific to
@@ -105,9 +123,12 @@ class TestThread(Thread):
       if self.test_function:
          radsubArgs += " -f " + self.test_function
       if self.test_script:
-         radsubArgs += " -sc " + self.test_script
+         radsubArgs += " -s " + self.test_script
       if self.test_group:
          radsubArgs += " -g " + self.test_group
+
+      if self.settings != '':
+         radsubArgs += ' -- ' + self.settings
 
       metadata_json = """
       {
@@ -118,14 +139,11 @@ class TestThread(Thread):
       }
       """ % radsubArgs
 
-      settings_json = """%s""" % self.settings
-
       cookies = cookielib.CookieJar()
       opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies),
                                     MultipartPostHandler)
       params = { 
-         "metadata" : metadata_json, 
-         "settings" : settings_json,
+         "metadata" : metadata_json,
          "file" : open(self.file_name, "rb")
       }
 
@@ -150,7 +168,7 @@ def run_perf_tests(slave_json, test_group, test_script, test_func, configs_json,
       config = configs_json[config_name]
       used_slaves = [(slave_json['machines'][machine_name]['ip_address'], machine_name) for machine_name in config['machines'] ]
 
-      threads = [ TestThread('http://' + slave[0] + ':8086/run/', slave[1], test_group, test_script, test_func, file_location, json.dumps(config['settings'])) for slave in used_slaves ]
+      threads = [ TestThread('http://' + slave[0] + ':8086/run/', slave[1], test_group, test_script, test_func, file_location, config['settings']) for slave in used_slaves ]
       
       for thread in threads:
          thread.start()
@@ -178,7 +196,6 @@ def write_perf_results_to_json_file(results, output_file):
             for test_run in test_data['tests_passed']:
 
                new_result = {
-                  'build_number' : 12345,  # for now!
                   'machine_name' : result[1], 
                   'settings' : result[2], 
                   'perf_data' : test_run['perf'],
@@ -245,8 +262,8 @@ test_func = None
 
 if '-g' in sys.argv:
    test_group = sys.argv[sys.argv.index('-g') + 1]
-if '-sc' in sys.argv:
-   test_script = sys.argv[sys.argv.index('-sc') + 1]
+if '-s' in sys.argv:
+   test_script = sys.argv[sys.argv.index('-s') + 1]
 if '-f' in sys.argv:
    test_func = sys.argv[sys.argv.index('-f') + 1]
 
