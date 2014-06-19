71e7f8aee983add718b947f33e0bb37e9107e8de
Stephanie <stephanie@radiant-entertainment.com>
2014-06-18 17:50:13 -0700

    Fixed the flicker in the carpenter UI. WOOO!

--- source/stonehearth_data/mods/stonehearth/components/crafter/crafter_component.lua
+++ source/stonehearth_data/mods/stonehearth/components/crafter/crafter_component.lua
@@ -17,13 +17,14 @@ function CrafterComponent:initialize(entity, json)
       self._sv.work_effect = json.work_effect
 
       local craftable_recipes = {}
+      self._sv._recipe_index = {}
       if json.recipe_list then
-         local recipe_list = radiant.resources.load_json(json.recipe_list)
-         craftable_recipes = recipe_list.craftable_recipes or {}
+         craftable_recipes = self:_build_craftable_recipe_list(json.recipe_list)
       end
 
       -- parts of our save state used by the ui.  careful modifying these     
       self._sv.craftable_recipes = craftable_recipes
+
       self._sv.active = false
    else
       radiant.events.listen_once(radiant, 'radiant:game_loaded', function()
@@ -44,6 +45,69 @@ function CrafterComponent:destroy()
    end
 end
 
+--- Build the list sent to the UI from the json
+--  TODO: add unlock conditions to the recipes
+function CrafterComponent:_build_craftable_recipe_list(recipe_index_url)
+   self._sv.recipe_index = radiant.resources.load_json(recipe_index_url).craftable_recipes
+   local craftable_recipes = {}
+   for category, category_data in pairs(self._sv.recipe_index) do
+      local recipe_array = {}
+      for recipe_name, data in pairs(category_data) do
+         --If there's no unlock condition, then just put in the recipe
+         if not data.unlock_condition then
+            table.insert(recipe_array, 1, data.uri)
+         end
+      end
+      if #recipe_array > 0 then
+         --Make an entry in the recipe table for the UI
+         local category_ui_info = {}
+         category_ui_info.categrory = category
+         category_ui_info.recipes = recipe_array
+         --Make sure we have a pointer to the recipe data for fast access/edit'
+         category_data.ui_info = category_ui_info
+         --table.insert(self._sv.craftable_recipes, category_ui_info)
+         table.insert(craftable_recipes, 1, category_ui_info)
+      end
+   end
+   return craftable_recipes
+end
+
+--TODO: test this with a scenario
+--- Add a recipe to a category, and add it to the UI.  
+--  If the category doesn't yet exist, create a new one.
+--  @param category - the name of the category
+--  @param recipe_uri - the uri to the recipe
+--  @param recipe_data - data about when to activate the recipe
+function CrafterComponent:add_recipe(category, recipe_uri, recipe_data)
+   --TODO: make new category, if necessary
+   local category_data = self._sv._recipe_index[category]
+   if not category_data then
+      category_data = {
+         recipes = {},
+      }
+      self._sv._recipe_index[category] = category_data
+   end
+   category_data.recipes[recipe_uri] = recipe_data
+
+   --If there is no unlock condition then add the recipe to the UI structure
+   if not recipe_data.unlock_condition then
+      --If there is no UI category for this yeat, make one
+      local ui_info = category_data.category_ui_info
+      if not ui_info then
+         ui_info = {
+            category = category, 
+            recipes = {}
+         }
+         category_data.category_ui_info = ui_info
+         table.insert(self._sv.craftable_recipes, ui_info)
+      end
+      table.insert(ui_info.recipes, recipe_uri)
+   end
+   self.__saved_variables:mark_changed()
+end
+
+
+--- Returns the effect to play as the crafter works
 function CrafterComponent:get_work_effect()
    return self._sv.work_effect
 end
