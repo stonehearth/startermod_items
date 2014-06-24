5e4440ca34ff235432b8733c72aefb51fdb29535
Tom Cannon <tom@radiant-entertainment.com>
2014-06-24 10:56:55 -0700

    Move the 7th settler off the firepit.

--- source/stonehearth_data/mods/stonehearth/call_handlers/new_game_call_handler.lua
+++ source/stonehearth_data/mods/stonehearth/call_handlers/new_game_call_handler.lua
@@ -179,10 +179,10 @@ function NewGameCallHandler:create_camp(session, response, pt)
    local worker2 = place_citizen_embark(camp_x+0, camp_z-3)
    local worker3 = place_citizen_embark(camp_x+3, camp_z-3)
    local worker4 = place_citizen_embark(camp_x-3, camp_z+3)
-   local worker6 = place_citizen_embark(camp_x+0, camp_z+3)   
    local worker5 = place_citizen_embark(camp_x+3, camp_z+3)
    local worker6 = place_citizen_embark(camp_x-3, camp_z+0)
-
+   local worker6 = place_citizen_embark(camp_x+3, camp_z+0)   
+   
    self:place_item(pop, 'stonehearth:firepit', camp_x, camp_z+3)
 
    radiant.entities.pickup_item(worker1, pop:create_entity('stonehearth:oak_log'))
