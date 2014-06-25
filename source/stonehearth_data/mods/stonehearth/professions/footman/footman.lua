75e0234782afa68fbe1060be63542f23e97cddd3
Albert Wang <albert@radiant-entertainment.com>
2014-06-25 11:58:44 -0700

    No more blood on hit. Remove talisman glow from equipped tools.

--- source/stonehearth_data/mods/stonehearth/professions/footman/footman.lua
+++ source/stonehearth_data/mods/stonehearth/professions/footman/footman.lua
@@ -5,9 +5,7 @@ function footman_class.promote(entity, json, talisman_uri)
    radiant.entities.equip_item(entity, weapon, 'melee_weapon')
 
    -- HACK: remove the talisman glow effect from the weapon
-   -- might want to remove other talisman related commands as well
-   -- TODO: make the effects and commands specific to the model variant
-   weapon:remove_component('effect_list')
+   radiant.entities.remove_effects(weapon)
 end
 
 function footman_class.demote(entity)
