25287af66b725c28ad6b1467c5e59eb2323da4dd
Albert Wang <albert@radiant-entertainment.com>
2014-07-01 14:33:23 -0700

    Have goblins defend themselves when attacked. Add status text for death.

--- source/stonehearth_data/mods/stonehearth/ai/actions/death_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/death_action.lua
@@ -7,6 +7,7 @@ local DeathAction = class()
 
 DeathAction.name = 'die'
 DeathAction.does = 'stonehearth:top'
+DeathAction.status_text = 'dead'
 DeathAction.args = {}
 DeathAction.version = 2
 DeathAction.priority = stonehearth.constants.priorities.top.DIE
