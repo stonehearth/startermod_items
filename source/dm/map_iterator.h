2b6d3cbb71364450a2cf1b2390f72951ac30b873
Tony Cannon <tony@radiant-entertainment.com>
2014-07-16 09:30:16 -0700

    expose safe dm::Map iterators to lua and add an autotest.

---
+++ source/dm/map_iterator.h
@@ -0,0 +1,40 @@
+#ifndef _RADIANT_DM_MAP_ITERATOR_H_
+#define _RADIANT_DM_MAP_ITERATOR_H_
+
+#include "object.h"
+#include <unordered_map>
+
+BEGIN_RADIANT_DM_NAMESPACE
+
+template <class M>
+class MapIterator
+{
+public:
+   MapIterator(M const& map, typename M::ContainerType::const_iterator);
+
+   typedef typename M::ContainerType::value_type EntryType;
+
+public:
+   bool operator==(MapIterator const& other) const;
+   bool operator!=(MapIterator const& other) const;
+   EntryType operator*() const;
+   EntryType const* operator->() const;
+   MapIterator& operator++();
+
+private:
+   void RefreshIterator() const;
+   void SetCurrentIterator(typename M::ContainerType::const_iterator i) const;
+   bool IsOutOfDate() const;
+
+private:
+   M const&                                  _map;
+   mutable bool                              _isEnd;
+   mutable GenerationId                      _capturedTime;
+   mutable typename M::Key                            _nextKey;
+   mutable typename M::Key                            _currentKey;
+   mutable typename M::ContainerType::const_iterator  _currentIterator;
+};
+
+END_RADIANT_DM_NAMESPACE
+
+#endif // _RADIANT_DM_MAP_ITERATOR_H_
--- source/lib/lua/dm/open_dm.cpp
+++ source/lib/lua/dm/open_dm.cpp
@@ -1,17 +1,19 @@
 #include "lib/lua/pch.h"
 #include "dm/types/all_types.h"
 #include "lib/lua/register.h"
+#include "dm/map.h"
 #include "dm/set_trace.h"
 #include "dm/map_trace.h"
 #include "dm/boxed_trace.h"
 #include "dm/store_save_state.h"
-#include "map_iterator.h"
+#include "map_iterator_wrapper.h"
 #include "dm/tracer_buffered.h"
 #include "set_iterator.h"
 #include "map_trace_wrapper.h"
 #include "set_trace_wrapper.h"
 #include "boxed_trace_wrapper.h"
 #include "trace_wrapper.h"
+#include "dm/lua_types.h"
 #include "open.h"
 
 using namespace luabind;
@@ -19,6 +21,45 @@ using namespace radiant;
 using namespace radiant::dm;
 
 DEFINE_INVALID_JSON_CONVERSION(StoreSaveState);
+
+IMPLEMENT_TRIVIAL_TOSTRING(NumberMap)
+DEFINE_INVALID_JSON_CONVERSION(NumberMapPtr)
+
+template <typename M>
+void LuaMap_Each(lua_State* L, std::shared_ptr<M> map)
+{
+   if (map) {
+      typedef lua::MapIteratorWrapper<typename M::MapType> Iterator;
+      lua_pushcfunction(L, Iterator::NextIteration);   // f
+      object(L, new Iterator(map, *map)).push(L);      // s
+      object(L, 1).push(L);                            // var (ignored)
+   }
+}
+
+template <typename M>
+void LuaMap_Add(M& map, typename M::Key key, luabind::object value)
+{
+   map.Add(key, value);
+}
+
+template <typename M>
+void LuaMap_Remove(M& map, typename M::Key key)
+{
+   map.Remove(key);
+}
+
+template <typename M>
+luabind::object LuaMap_Get(M& map, typename M::Key key)
+{
+   return map.Get(key, luabind::object());
+}
+
+template <typename M>
+bool LuaMap_Contains(M& map, typename M::Key key)
+{
+   return map.Contains(key);
+}
+
 
 void RegisterGenericTrace(lua_State* L)
 {
@@ -40,7 +81,7 @@ void RegisterGenericTrace(lua_State* L)
 template <typename T>
 void RegisterMap(lua_State* L)
 {
-   typedef lua::MapIterator<T> Iterator;
+   typedef lua::MapIteratorWrapper<T> Iterator;
    typedef lua::MapTraceWrapper<MapTrace<T>> Trace;
    module(L) [
       namespace_("_radiant") [
@@ -110,6 +151,14 @@ static void OpenDm(lua_State* L)
    module(L) [
       namespace_("_radiant") [
          namespace_("dm") [
+            lua::RegisterTypePtr<NumberMap>("NumberMap")
+               .def("add",       &LuaMap_Add<NumberMap>)
+               .def("remove",    &LuaMap_Remove<NumberMap>)
+               .def("get",       &LuaMap_Get<NumberMap>)
+               .def("contains",  &LuaMap_Contains<NumberMap>)
+               .def("each",      &LuaMap_Each<NumberMap>)
+               .def("get_size",  &NumberMap::GetSize)
+               ,
             class_<TraceCategories>("TraceCategories")
                .enum_("constants") [
                   value("SYNC_TRACE", dm::LUA_SYNC_TRACES),
