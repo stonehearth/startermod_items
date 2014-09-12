451e0f4e146db9fab4714a937cd4bdd17cbd1941
Tony Cannon <tony@radiant-entertainment.com>
2014-09-12 13:06:02 -0700

    more long running build test robustness

--- source/stonehearth_data/mods/stonehearth_autotest/tests/longrunning/build_longrunning_test.lua
+++ source/stonehearth_data/mods/stonehearth_autotest/tests/longrunning/build_longrunning_test.lua
@@ -2,96 +2,125 @@
 local Point3 = _radiant.csg.Point3
 local Cube3 = _radiant.csg.Cube3
 
-local structures_to_test = {}
+local all_tester_scripts = {
+   'simple_floor',
+   'simple_floor_erase',
+}
+
 local build_longrunning_tests = {}
 
 -- get the area of the blueprint for the given structure
 --
 local function get_blueprint_area(structure)
-   return structure:get_component('destination'):get_region():get():get_area() 
+   return structure:get_component('destination'):get_region():get():get_area()
+end
+
+local function create_workers(autotest)
+   autotest.env:create_person(-8,   8, { profession = 'worker' })   
+   autotest.env:create_person(-8,  10, { profession = 'worker' })   
+   autotest.env:create_person(-10,  8, { profession = 'worker' })   
+   autotest.env:create_person(-10, 10, { profession = 'worker' })   
+end
+
+local function create_buildings(autotest, tester)
+   local buildings = {}
+   local building_listener = radiant.events.listen(radiant, 'radiant:entity:post_create', function(e)
+         local entity = e.entity
+         if entity:is_valid() and entity:get_uri() == 'stonehearth:entities:building' then
+            buildings[entity:get_id()] = entity
+         end
+      end)
+
+   -- now run `cd` to create all the building structures.
+   local session = autotest.env:get_player_session()
+   local steps = tester:get_build_steps(autotest, session)
+   for i, step_cb in ipairs(steps) do
+      stonehearth.build:do_command('step ' .. tostring(i), nil, function()
+            step_cb()
+         end)
+   end
+
+   building_listener:destroy()
+   return buildings
+end
+
+local function mark_buildings_active(autotest, buildings)
+   for _, building in pairs(buildings) do
+      stonehearth.build:set_active(building, true)
+   end
+end
+
+local function succeed_when_buildings_finished(autotest, buildings)
+   for _, building in pairs(buildings) do
+      radiant.events.listen(building, 'stonehearth:construction:finished_changed', function()
+            if building:get_component('stonehearth:construction_progress'):get_finished() then
+               buildings[building:get_id()] = nil
+               if not next(buildings) then
+                  autotest:success()
+               end
+               return radiant.events.UNLISTEN
+            end
+         end)
+   end
+end
+
+local function succeed_when_buildings_destroyed(autotest, buildings)
+   for _, building in pairs(buildings) do
+      radiant.events.listen_once(building, 'radiant:entity:pre_destroy', function()
+            buildings[building:get_id()] = nil
+            if not next(buildings) then
+               autotest:success()
+            end
+         end)
+   end
 end
 
 -- run a build test
 --
-local function do_build(autotest, cb)
+local function do_build(autotest, tester)
    -- setup the environment for the build
-   autotest.env:create_person(-8,   8, { profession = 'worker' })   
-   autotest.env:create_person(-8,  10, { profession = 'worker' })   
-   autotest.env:create_person(-10,  8, { profession = 'worker' })   
-   autotest.env:create_person(-10, 10, { profession = 'worker' })   
+   create_workers(autotest)
    autotest.env:create_entity_cluster(8, 8, 8, 8, 'stonehearth:oak_log')
 
-   -- now run `cd` to create all the building structures.
-   local session = autotest.env:get_player_session()
-   local structure
-   stonehearth.build:do_command('', nil, function()
-         structure = cb(session)
-      end)
+   local buildings = create_buildings(autotest, tester)
 
-   -- fetch the building and start it up!
-   local building = stonehearth.build:get_building_for(structure)
-   stonehearth.build:set_active(building, true)
-
-   -- signal success when the building is completed.
-   radiant.events.listen(building, 'stonehearth:construction:finished_changed', function()
-         if building:get_component('stonehearth:construction_progress'):get_finished() then
-            autotest:success()
-            return radiant.events.UNLISTEN
-         end
-      end)
+   mark_buildings_active(autotest, buildings)
+   succeed_when_buildings_finished(autotest, buildings)
       
    autotest:sleep(60000)
-   autotest:fail('failed to build structure')
+   autotest:fail('failed to build structures')
 end
 
 -- run a teardown test
 --
-local function do_teardown(autotest, cb)
-   autotest.env:create_person(-8,   8, { profession = 'worker' })   
-   autotest.env:create_person(-8,  10, { profession = 'worker' })   
-   autotest.env:create_person(-10,  8, { profession = 'worker' })   
-   autotest.env:create_person(-10, 10, { profession = 'worker' })   
+local function do_teardown(autotest, tester)
+   -- setup the environment for the build
+   create_workers(autotest)
    autotest.env:create_stockpile(-8, 8,  { size = { x = 6, y = 6 }})
 
-   -- now run `cd` to create all the building structures.
-   local session = autotest.env:get_player_session()
-   local structure
-   stonehearth.build:do_command('', nil, function()
-         structure = cb(session)
-      end)
+   local buildings = create_buildings(autotest, tester)
+   for _, building in pairs(buildings) do
+      stonehearth.build:instabuild(building)
+      stonehearth.build:set_teardown(building, true)
+   end
 
-   -- fetch the building and start it up!
-   local building = stonehearth.build:get_building_for(structure)
-   stonehearth.build:instabuild(building)
-   stonehearth.build:set_teardown(building, true)
-   stonehearth.build:set_active(building, true)
+   mark_buildings_active(autotest, buildings)
+   succeed_when_buildings_destroyed(autotest, buildings)
 
-   -- signal success when the building is completed.
-   radiant.events.listen_once(building, 'radiant:entity:pre_destroy', function()
-         autotest:success()
-      end)
-      
    autotest:sleep(60000)
-   autotest:fail('failed to teardown structure')
-end
-
--- a simple floor structure
-function structures_to_test.simple_floor(session)
-   return stonehearth.build:add_floor(session,
-                                      'stonehearth:entities:wooden_floor',
-                                      Cube3(Point3(0, 1, 0), Point3(4, 2, 4)),
-                                      '/stonehearth/entities/build/wooden_floor/wooden_floor_diagonal.qb')
+   autotest:fail('failed to teardown structures')
 end
 
 -- create all the long running build tests.  this is the cross product of the structurE_to_test
 -- with all the different types of tests to run (build, teardown, etc.)
 --
-for fname, fn in pairs(structures_to_test) do   
-   build_longrunning_tests['build_' .. fname] = function(autotest)
-      do_build(autotest, fn)
+for _, script in pairs(all_tester_scripts) do   
+   local cls = require('tests/longrunning/' .. script)
+   build_longrunning_tests['build_' .. script] = function(autotest)
+      do_build(autotest, cls())
    end
-   build_longrunning_tests['teardown_' .. fname] = function(autotest)
-      do_teardown(autotest, fn)
+   build_longrunning_tests['teardown_' .. script] = function(autotest)
+      do_teardown(autotest, cls())
    end
 end
 
