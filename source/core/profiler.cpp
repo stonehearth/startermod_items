0436fd2475633084c05f5082e70eaf290627703a
Tony Cannon <tony@radiant-entertainment.com>
2014-09-15 14:38:48 -0700

    oops.  missed some files!

---
+++ source/core/profiler.cpp
@@ -0,0 +1,36 @@
+#include "radiant.h"
+#include "profiler.h"
+#include "ittnotify.h"
+
+using namespace ::radiant;
+using namespace ::radiant::core;
+
+static bool enabled = false;
+
+bool core::IsProfilerAvailable()
+{
+#if defined(VTUNE_PATH)
+   return true;
+#else
+   return false;
+#endif
+}
+
+bool core::IsProfilerEnabled()
+{
+   return enabled;
+}
+
+void core::SetProfilerEnabled(bool value)
+{
+#if defined(VTUNE_PATH)
+   if (enabled != value) {
+      enabled = value;
+      if (enabled) {
+         __itt_resume();
+      } else {
+         __itt_pause();
+      }
+   }
+#endif
+}
