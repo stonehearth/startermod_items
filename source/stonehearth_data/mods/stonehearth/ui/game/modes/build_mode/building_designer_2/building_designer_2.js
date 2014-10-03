ae107ad99913925b9d71c433b714169b55f1edb6
Tom Cannon <tom@radiant-entertainment.com>
2014-10-03 08:50:52 -0700

    Add confirmation before nuking a building.

--- source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_designer_2/building_designer_2.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_designer_2/building_designer_2.js
@@ -401,7 +401,7 @@ App.StonehearthBuildingDesignerTools = App.View.extend({
 
          App.gameView.addView(App.StonehearthConfirmView, 
             { 
-               title : "Really Start Building?",
+               title : "Really start building?",
                message : "Once you begin building this structure it cannot be edited. Are you sure you want to build now?",
                buttons : [
                   { 
@@ -417,12 +417,30 @@ App.StonehearthBuildingDesignerTools = App.View.extend({
       });
 
       this.$('#removeBuilding').click(function() {
-         radiant.call('radiant:play_sound', {'track' : 'stonehearth:sounds:ui:carpenter_menu:trash'} );
-         var building_entity = self.get('building');
-         if (building_entity) {
-            radiant.call('stonehearth:set_building_teardown', building_entity.__self, true)
-            self.set('context.selection', null);
+         var doRemoveBuilding = function() {
+            radiant.call('radiant:play_sound', {'track' : 'stonehearth:sounds:ui:carpenter_menu:trash'} );
+            var building_entity = self.get('building');
+            if (building_entity) {
+               radiant.call('stonehearth:set_building_teardown', building_entity.__self, true)
+               self.set('context.selection', null);
+            }            
          }
+
+         App.gameView.addView(App.StonehearthConfirmView, 
+            { 
+               title : "Really remove this building",
+               message : "Are you sure you want to remove this entire building?",
+               buttons : [
+                  { 
+                     id: 'confirmRemove',
+                     label: "Keep this building"
+                  },
+                  {
+                     label: "Remove this building",
+                     click: doRemoveBuilding
+                  }
+               ] 
+            });         
       });
 
    },
