550ab3b6e421fb8606089f9226a7e946a6248a74
Albert Wang <albert@radiant-entertainment.com>
2014-10-10 17:14:47 -0700

    Fix vertical combat and entities posing endlessly when the target is unreachable.

--- source/stonehearth_data/mods/stonehearth/ai/actions/combat/get_primary_target_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/combat/get_primary_target_action.lua
@@ -13,10 +13,36 @@ GetPrimaryTarget.version = 2
 GetPrimaryTarget.priority = 1
 
 function GetPrimaryTarget:start_thinking(ai, entity, args)
+   if self:_check_for_primary_target(ai, entity) then
+      return
+   end
+
+   self._target_changed_listener = radiant.events.listen(entity, 'stonehearth:combat:primary_target_changed', function()
+         if self:_check_for_primary_target(ai, entity) then
+            self:_destroy_target_changed_listener()
+         end
+      end)
+end
+
+function GetPrimaryTarget:stop_thinking(ai, entity, args)
+   self:_destroy_target_changed_listener()
+end
+
+function GetPrimaryTarget:_destroy_target_changed_listener()
+   if self._target_changed_listener then
+      self._target_changed_listener:destroy()
+      self._target_changed_listener = nil
+   end
+end
+
+function GetPrimaryTarget:_check_for_primary_target(ai, entity)
    local primary_target = stonehearth.combat:get_primary_target(entity)
 
    if primary_target and primary_target:is_valid() then
       ai:set_think_output({ target = primary_target })
+      return primary_target
+   else
+      return nil
    end
 end
 
