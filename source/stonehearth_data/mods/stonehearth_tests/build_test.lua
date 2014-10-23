d715de36b8919f81f7179a1ac0f7564917de6ba6
Tony Cannon <tony@radiant-entertainment.com>
2014-10-23 08:17:15 -0700

    fix a bad merge

--- source/stonehearth_data/mods/stonehearth_tests/build_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/build_test.lua
@@ -9,17 +9,20 @@ function BuildTest:__init()
    self:create_world()
 
    self:place_citizen(2, 2)
+   self:place_citizen(2, 2)
+   self:place_citizen(2, 2)
+   self:place_citizen(2, 2)
 
    self:place_item_cluster('stonehearth:oak_log', 8, 8, 7, 7)
-   self:place_item_cluster('stonehearth:wooden_door_2', -2, -2, 2, 2)
-   self:place_item_cluster('stonehearth:wooden_wall_lantern', -10, 10, 2, 2)
+   self:place_item_cluster('stonehearth:portals:wooden_door_2', -2, -2, 2, 2)
    if true then return end
    self:place_item_cluster('stonehearth:berry_basket', -8, -8, 2, 2)
-   self:place_item_cluster('stonehearth:wooden_door', -8, 8, 1, 1)
-   self:place_item_cluster('stonehearth:wooden_window_frame', -12, 8, 2, 2)
-   self:place_item_cluster('stonehearth:wooden_diamond_window', -12, 12, 2, 2)
-   self:place_item_cluster('stonehearth:comfy_bed', 2, 2, 2, 2)
-   self:place_item_cluster('stonehearth:green_hanging', 4, 2, 2, 2)
+   self:place_item_cluster('stonehearth:furniture:wooden_wall_lantern', -10, 10, 2, 2)
+   self:place_item_cluster('stonehearth:portals:wooden_door', -8, 8, 1, 1)
+   self:place_item_cluster('stonehearth:portals:wooden_window_frame', -12, 8, 2, 2)
+   self:place_item_cluster('stonehearth:portals:wooden_diamond_window', -12, 12, 2, 2)
+   self:place_item_cluster('stonehearth:furniture:comfy_bed', 2, 2, 2, 2)
+   self:place_item_cluster('stonehearth:decoration:green_hanging', 4, 2, 2, 2)
    
    --self:place_citizen(0, 0)
    if true then return end
