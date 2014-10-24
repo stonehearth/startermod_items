c411cbb2a683a86135245e42f68a82c122338642
Tom Cannon <tom@radiant-entertainment.com>
2014-10-23 17:54:46 -0700

    Fix a comment.

--- source/stonehearth_data/mods/candledark/candledark_server.lua
+++ source/stonehearth_data/mods/candledark/candledark_server.lua
@@ -4,9 +4,9 @@ candledark = {}
 -- Listen for the 'radiant:init' event, which gets triggered whenever the game starts or is loaded
 --
 radiant.events.listen(candledark, 'radiant:init', function()
-   -- We'd like to start on noon of the first day, but the Stonehearth scenario system isn't
-   -- sophisticated enough to handle that yet.  Until that happy day, just wait until noon
-   -- and force the scenario to start.
+   -- We'd like to start four in-game hours after the game starts, but the Stonehearth scenario 
+   -- system isn't sophisticated enough to handle that yet.  Until that happy day, just wait until 
+   -- noon and force the scenario to start.
 
    -- We only want this to happen once, on the first time we load. If we load after 
    -- noon on the 1st day, do not create candledark
