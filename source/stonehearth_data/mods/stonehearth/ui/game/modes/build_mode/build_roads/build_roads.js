b2d8279c2826ad45a0e34afd6d0716ee6563c775
Chris Klochek <klochek@radiant-entertainment.com>
2014-10-28 13:25:49 -0400

    Road UI integration, first pass.  It's...a whole lotta copy-paste.  Time to factor!

--- source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/build_roads/build_roads.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/build_roads/build_roads.js
@@ -3,9 +3,74 @@ App.StonehearthBuildRoadsView = App.View.extend({
    i18nNamespace: 'stonehearth',
    classNames: ['fullScreen', "gui"],
 
+   _buildMaterialPalette: function(materials, materialClassName) {
+      var palette = $('<div>').addClass('brushPalette');
+
+      // for each category
+      var index = 0;
+      $.each(materials, function(i, category) {
+         // for each material
+         $.each(category.items, function(k, material) {
+            var brush = $('<div>')
+                           .addClass('brush')
+                           .attr('brush', material.brush)
+                           .css({ 'background-image' : 'url(' + material.portrait + ')' })
+                           .attr('title', material.name)
+                           .attr('index', index)
+                           .addClass(materialClassName)
+                           .append('<div class=selectBox />'); // for showing the brush when it's selected
+
+            palette.append(brush);
+            
+
+            index += 1;
+         });
+      });
+
+      return palette;
+   },
+
+   _deactivateTool: function(toolTag) {
+      var self = this;
+      return function() {
+         if (self.$(toolTag)) {
+            self.$(toolTag).removeClass('active');
+         }
+         $(top).trigger('radiant_hide_tip');
+      }
+   },
+
    didInsertElement: function() {
       this._super();
       var self = this;
+
+      var activateElement = function(elPath) {
+         return function() {
+            if (self.$(elPath)) {
+               self.$(elPath).addClass('active');   
+            }
+         }
+      };
+
+      // draw floor tool
+      var doDrawRoad = function() {
+         var brush = self.$('#roadMaterialTab .roadMaterial.selected').attr('brush');
+         App.stonehearthClient.buildRoad(brush, activateElement('#drawRoadTool'))
+            .fail(self._deactivateTool('#drawRoadTool'))
+            .done(function() {
+               doDrawRoad();
+            });
+      };
+
+      var doEraseRoad = function() {
+         App.stonehearthClient.eraseFloor(activateElement('#eraseFloorTool'))
+            .fail(self._deactivateTool('#eraseFloorTool'))
+            .done(function() {
+               doEraseFloor(false);
+            });
+      };
+
+
 
       // tab buttons and pages
       this.$('.toolButton').click(function() {
@@ -35,7 +100,37 @@ App.StonehearthBuildRoadsView = App.View.extend({
 
       this.$('#showEditor').click(function() {
          self.showEditor();
-      });      
+      });
+
+      this.$('#drawRoadTool').click(function() {
+         //doDrawRoad();
+      });
+
+      this.$('#eraseRoadTool').click(function() {
+         doEraseRoad(true);
+      });
+
+      $.get('/stonehearth/data/build/building_parts.json')
+         .done(function(json) {
+            self.buildingParts = json;
+            self.$('#roadMaterials').append(self._buildMaterialPalette(self.buildingParts.floorPatterns, 'roadMaterial'));
+            //$(self.$('#roadMaterialTab .roadMaterial')[self._state.roadMaterial]).addClass('selected');
+
+            /*self._addEventHandlers();
+            self._restoreUiState();
+            self._updateControls();*/
+
+            self.$('.roadMaterial').click(function() {
+               self.$('.roadMaterial').removeClass('selected');
+               $(this).addClass('selected');
+
+               //self._state.roadMaterial = $(this).attr('index');
+               //self._saveState();
+
+               // Re/activate the floor tool with the new material
+               doDrawRoad(true);
+            });
+         });
    },
 
    showOverview: function() {
