f86c5feef85c1f794e608bfe28c7127b7bec4f68
Stephanie <stephanie@radiant-entertainment.com>
2014-10-27 18:21:22 -0700

    Fixed additive buffs!

--- source/stonehearth_data/mods/stonehearth/components/attributes/attributes_component.lua
+++ source/stonehearth_data/mods/stonehearth/components/attributes/attributes_component.lua
@@ -267,7 +267,7 @@ function AttributesComponent:_recalculate(name)
             if mods['add'] then
                add_factor = add_factor + mods['add']
                if not modifier:get_private() then
-                  user_visible_add_factor = user_visible_add_factor * mods['multiply']
+                  user_visible_add_factor = user_visible_add_factor * mods['add']
                end
             end
 
