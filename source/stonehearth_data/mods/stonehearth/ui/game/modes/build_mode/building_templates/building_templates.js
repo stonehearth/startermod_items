ce71b4fe8d9bf2c14e01c72a24f0f45208b50292
Tom Cannon <tom@radiant-entertainment.com>
2014-10-30 09:30:52 -0700

    Fix some building designer bugs, and add some code to support the template preview-popup

--- source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_templates/building_templates.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_templates/building_templates.js
@@ -16,7 +16,6 @@ App.StonehearthBuildingTemplatesView = App.View.extend({
       this._super();
       var self = this;
       this._addEventHandlers();
-      this._updateButtons();
    },
 
    show: function() {
@@ -44,29 +43,28 @@ App.StonehearthBuildingTemplatesView = App.View.extend({
          row.addClass('selected');
          self._selectedTemplate = row.attr('template');
          App.stonehearthClient.drawTemplate(null, self._selectedTemplate);
-         
-         //self._updateButtons();
       });
-      
-      /*
-      this.$('#buildTemplateButton').click(function() {
-         App.stonehearthClient.drawTemplate(activateElement('#drawTemplateTool'), self._selectedTemplate);
-      });
-      */
 
+
+      /*
+      self.$().on('mouseenter', '#templatesList .row', function() {        
+            self.$('#buildingTemplatesDetailsPopup').show()
+               .position({
+                  my: "left center",
+                  at: "right+40 center",
+                  of: $(this),
+                  collision: 'fit',
+               });
+         });
+
+      self.$().on('mouseleave', '#templatesList .row', function() {        
+            self.$('#buildingTemplatesDetailsPopup').hide();
+         });
+      */
+   
       self.$('#customBuildingButton').click(function() {
          $(top).trigger('stonehearth_building_editor');
       })      
    },
-
-   _updateButtons: function() {
-      var self = this;
-
-      if (self.$('#templatesList .selected').length == 0) {
-         self.$('#buildTemplateButton').addClass('disabled');
-      } else {
-         self.$('#buildTemplateButton').removeClass('disabled');
-      }
-   }
    
 });
