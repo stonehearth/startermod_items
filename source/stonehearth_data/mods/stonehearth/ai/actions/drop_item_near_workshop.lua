327efe215f367b9cf368792d726abae864cd464a
Stephanie <stephanie@radiant-entertainment.com>
2014-10-30 17:51:39 -0700

    Fix so that the carpenter no longer drops stuff on top of workbench when there is no stockpile.

--- source/stonehearth_data/mods/stonehearth/ai/actions/drop_item_near_workshop.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/drop_item_near_workshop.lua
@@ -1,4 +1,5 @@
 local Entity = _radiant.om.Entity
+local Point3 = _radiant.csg.Point3
 
 local DropItemNearWorkshop = class()
 DropItemNearWorkshop.name = 'drop item near workshop'
@@ -13,8 +14,18 @@ function DropItemNearWorkshop:start(ai, entity, args)
    ai:set_status_text('clearing workshop')
 end
 
+--Find an empty space nearby to drop the item
+--TODO: if this takes a moment, then the carpenter may wander before actually finding the location
+--but at least it fixes the bug where the carpenter will put something back down on his workbench and get stufk
+function DropItemNearWorkshop:start_thinking(ai, entity, args)
+   local origin = radiant.entities.get_world_grid_location(entity)
+   ai.CURRENT.location = radiant.terrain.find_placement_point(origin, 2, 5)
+   if ai.CURRENT.location then
+      ai:set_think_output()
+   end
+end
+
 local ai = stonehearth.ai
 return ai:create_compound_action(DropItemNearWorkshop)
    :execute('stonehearth:pickup_item_on_table', { item = ai.ARGS.item })
-   :execute('stonehearth:wander', { radius_min = 1, radius = 3 })
-   :execute('stonehearth:drop_carrying_now', {})
+   :execute('stonehearth:drop_carrying_at', {location = ai.CURRENT.location})
