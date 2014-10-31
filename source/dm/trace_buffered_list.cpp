569e0977add58e8788944dda35e59c0efd87b912
Tony Cannon <tony@radiant-entertainment.com>
2014-10-31 14:35:21 -0700

    fix an infinite loop in edge cases (thank goodness for autotests!)

--- source/dm/trace_buffered_list.cpp
+++ source/dm/trace_buffered_list.cpp
@@ -18,6 +18,7 @@ bool TraceBufferedList::IsEmpty() const
       if (i->lock()) {
          return false;
       }
+      ++i;
    }
 
    i = _pendingTraces.begin(), end = _pendingTraces.end();
@@ -25,6 +26,7 @@ bool TraceBufferedList::IsEmpty() const
       if (i->lock()) {
          return false;
       }
+      ++i;
    }
    return true;
 }
