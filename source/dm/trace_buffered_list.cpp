052eb468b2d3b120badbb2285ebb90a9c86b5e90
Tony Cannon <tony@radiant-entertainment.com>
2014-10-31 14:05:41 -0700

    some trace bug fixes
    1) only accumulate changes for the undo manager in a transaction
    2) make sure adding a trace on an object while firing a trace on that same object works.

---
+++ source/dm/trace_buffered_list.cpp
@@ -0,0 +1,52 @@
+
+#include "radiant.h"
+#include "radiant_stdutil.h"
+#include "trace_buffered_list.h"
+
+using namespace ::radiant;
+using namespace ::radiant::dm;
+
+TraceBufferedList::TraceBufferedList() :
+   _firing(false)
+{
+}
+
+bool TraceBufferedList::IsEmpty() const
+{
+   auto i = _traces.begin(), end = _traces.end();
+   while (i != end) {
+      if (i->lock()) {
+         return false;
+      }
+   }
+
+   i = _pendingTraces.begin(), end = _pendingTraces.end();
+   while (i != end) {
+      if (i->lock()) {
+         return false;
+      }
+   }
+   return true;
+}
+
+void TraceBufferedList::AddTrace(TraceBufferedRef t)
+{
+   if (!_firing) {
+      _traces.push_back(t);
+   } else {
+      _pendingTraces.push_back(t);
+   }
+}
+
+void TraceBufferedList::ForEachTrace(std::function<void(TraceBufferedPtr)> cb)
+{
+   ASSERT(!_firing);
+   ASSERT(_pendingTraces.empty());
+
+   _firing = true;
+   stdutil::ForEachPrune<TraceBuffered>(_traces, cb);
+   _firing = false;
+
+   _traces.insert(_traces.end(), _pendingTraces.begin(), _pendingTraces.end());
+   _pendingTraces.clear();
+}
