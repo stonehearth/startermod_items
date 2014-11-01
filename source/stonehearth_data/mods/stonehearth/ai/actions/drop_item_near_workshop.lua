a47f8bcd5d3f0253b33b383f5fcd6450ebf01017
Stephanie <stephanie@radiant-entertainment.com>
2014-10-31 17:55:57 -0700

    Fix bug with crafter dropping stuff on own workshop

--- source/stonehearth_data/mods/stonehearth/ai/actions/drop_item_near_workshop.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/drop_item_near_workshop.lua
@@ -14,18 +14,8 @@ function DropItemNearWorkshop:start(ai, entity, args)
    ai:set_status_text('clearing workshop')
 end
 
---Find an empty space nearby to drop the item
---TODO: if this takes a moment, then the carpenter may wander before actually finding the location
---but at least it fixes the bug where the carpenter will put something back down on his workbench and get stufk
-function DropItemNearWorkshop:start_thinking(ai, entity, args)
-   local origin = radiant.entities.get_world_grid_location(entity)
-   ai.CURRENT.location = radiant.terrain.find_placement_point(origin, 2, 5)
-   if ai.CURRENT.location then
-      ai:set_think_output()
-   end
-end
-
 local ai = stonehearth.ai
 return ai:create_compound_action(DropItemNearWorkshop)
    :execute('stonehearth:pickup_item_on_table', { item = ai.ARGS.item })
-   :execute('stonehearth:drop_carrying_at', {location = ai.CURRENT.location})
+   :execute('stonehearth:walk_to_empty_space', {radius = 8, radius_min = 1})
+   :execute('stonehearth:drop_carrying_now')
---
+++ source/stonehearth_data/mods/stonehearth_autotest/tests/regression/sh_82.lua
@@ -0,0 +1,62 @@
+--https://bugs/browse/SH-82
+--Test that when the carpenter makes an object, it does not land on their workbench
+
+local sh_82 = {} 
+
+function sh_82.verify_not_on_bench(autotest)
+   local carpenter = autotest.env:create_person(2, 2, { job = 'carpenter' })
+   local wood = autotest.env:create_entity_cluster(-2, -2, 3, 3, 'stonehearth:oak_log')
+
+   autotest:sleep(1000)
+
+   -- when creating the workshop, be sure to send the size and position of
+   -- the outbox before selecting the workshop location.  otherwise, the
+   -- ui and the server will race to see if it gets there in time!
+   autotest.ui:push_unitframe_command_button(carpenter, 'build_workshop')
+   autotest.ui:click_terrain(4, 4)
+
+   local workshop
+   radiant.events.listen(carpenter, 'stonehearth:crafter:workshop_changed', function (e)
+         workshop = e.workshop:get_entity()
+         autotest:resume()
+         return radiant.events.UNLISTEN
+      end)
+   autotest:suspend()
+   
+   autotest.ui:push_unitframe_command_button(workshop, 'show_workshop')
+   autotest.ui:click_dom_element('#craftWindow #recipeList a[recipe_name="Table for One"]')
+   autotest.ui:click_dom_element('#craftWindow #craftButton')
+   autotest.ui:click_dom_element('#craftWindow #recipeList a[recipe_name="Table for One"]')
+   autotest.ui:click_dom_element('#craftWindow #craftButton')
+   autotest.ui:click_dom_element('#craftWindow #recipeList a[recipe_name="Table for One"]')
+   autotest.ui:click_dom_element('#craftWindow #craftButton')
+   autotest.ui:click_dom_element('#craftWindow #recipeList a[recipe_name="Table for One"]')
+   autotest.ui:click_dom_element('#craftWindow #craftButton')
+   autotest.ui:click_dom_element('#craftWindow #closeButton')
+
+   local num_created = {}
+   radiant.events.listen(radiant, 'radiant:entity:post_create', function (e)
+      if e.entity:get_uri() == 'stonehearth:furniture:table_for_one' then 
+         local table_proxy = e.entity:get_component('stonehearth:entity_forms')
+                                 :get_iconic_entity()
+         table.insert(num_created, table_proxy)
+         if #num_created == 4 then
+            --check that the first 3 items are at ground level
+            for i, table_object in ipairs(num_created) do
+               if i < 3 and radiant.entities.get_world_grid_location(table_object).y > radiant.entities.get_world_grid_location(carpenter).y then
+                  autotest:fail('object is not on the ground')
+               end
+            end
+            --If 1, 2, and 3 are on the ground, then the test is a success
+            autotest:success()
+            return radiant.events.UNLISTEN
+         end
+      end
+
+   end)
+
+   autotest:sleep(120 * 10000)
+   autotest:fail('failed to carpenter')
+end
+
+return sh_82
