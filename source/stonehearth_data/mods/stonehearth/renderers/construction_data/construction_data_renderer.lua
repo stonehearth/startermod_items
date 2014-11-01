276c36c7e29e7d662ede07b8acfae3db5ab2d900
Tony Cannon <tony@radiant-entertainment.com>
2014-11-01 09:56:19 -0700

    fix SH-90.  hide fixtures with height > 0 in rpg mode.

--- source/stonehearth_data/mods/stonehearth/renderers/construction_data/construction_data_renderer.lua
+++ source/stonehearth_data/mods/stonehearth/renderers/construction_data/construction_data_renderer.lua
@@ -8,7 +8,7 @@ local ConstructionDataRenderer = class()
 
 local INFINITE = 1000000
 
-function ConstructionDataRenderer:initialize(render_entity, construction_data)
+function ConstructionDataRenderer:initialize(render_entity, construction_data)   
    self._entity = render_entity:get_entity()
    self._render_entity = render_entity
    self._parent_node = render_entity:get_node()
@@ -22,8 +22,8 @@ function ConstructionDataRenderer:initialize(render_entity, construction_data)
       self._render_tracker = ConstructionRenderTracker(self._entity)
                                  :set_type(construction_data:get_type())
                                  :set_normal(construction_data:get_normal())
-                                 :set_render_region_changed_cb(function(region)
-                                       self:_update_region(region)
+                                 :set_render_region_changed_cb(function(region, visible, mode)
+                                       self:_update_region(region, mode)
                                     end)
                                  :set_visible_changed_cb(function(visible)
                                        self._render_entity:set_visible_override(visible)
@@ -81,14 +81,45 @@ function ConstructionDataRenderer:_trace_collision_shape()
    end
 end
 
-function ConstructionDataRenderer:_update_region(region)
+function ConstructionDataRenderer:_update_region(region, mode) 
+   -- recreate our render node
    if self._render_node then
       self._render_node:destroy()
       self._render_node = nil
    end
-   if region then   
+   if region then
       self._render_node = voxel_brush_util.create_construction_data_node(self._parent_node, self._entity, region, self._construction_data)
    end
+
+   -- if we enter or leave rpg mode, hide all our children that are not
+   -- positioned at the bottom of the structure
+   if mode ~= self._view_mode then
+      self._view_mode = mode
+      local ec = self._entity:get_component('entity_container')
+      if ec then
+         for _, child in ec:each_child() do
+            self:_update_child_visibility(child)
+         end
+      end
+   end
+end
+
+function ConstructionDataRenderer:_update_child_visibility(entity)
+   -- we only want to manually toggle the project, so make sure we skip the
+   -- blueprint and fabricator.
+   if entity:get_component('stonehearth:construction_progress') or
+      entity:get_component('stonehearth:fabricator') then
+      return
+   end
+      
+   local visible = true
+   if self._view_mode == 'rpg' then
+      local location = entity:get_component('mob'):get_location()
+      visible = location.y == 0
+   end
+
+   _radiant.client.get_render_entity(entity)
+                     :set_visible_override(visible)
 end
 
 return ConstructionDataRenderer
