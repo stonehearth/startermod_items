9069d9f6e122f8131a0b83c7a0e14341e1344313
Chris Klochek <klochek@radiant-entertainment.com>
2014-11-02 21:54:12 -0500

    Update sound-test to have a thing (a tree) generate a sound on a loop; useful for playing around with falloffs for different sounds.

--- source/stonehearth_data/mods/stonehearth_tests/sound_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/sound_test.lua
@@ -6,24 +6,8 @@ function SoundTest:__init()
    self[MicroWorld]:__init()
    self:create_world()
 
-   local tree = self:place_tree(-12, -12)
-   local dude = self:place_citizen(12, 12)
-
-   --TODO: move to client-side, for example, sky-renderer would be able to call day/night effects
-   --TODO: also vary based on camera position
-   --Thus this test no longer works. If you are in client-side lua, you can call _radiant.call('stonehearth:play_music', {args})
-   --but since play_music is in client.cpp, you cannot call it from the server. 
-
-   _radiant.audio.play_music('stonehearth:music:title_screen', 'bgm')
-
-   ---[[
-   --Some time in seconds in, play a different background music
-   self:at(20000, function()
-         _radiant.audio.play_music('stonehearth:music:world_start', 'bgm')
-      end)
-   --]]
-
-
+   local e = self:place_item('stonehearth:large_oak_tree', 0, 0)
+   radiant.effects.run_effect(e, '/stonehearth/data/effects/loop_sound_effect')
 end
 
 return SoundTest
