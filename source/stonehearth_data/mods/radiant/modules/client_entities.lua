92483766ae6f525e848fc8c3816bb5c087a4cabd
Chris Klochek <klochek@radiant-entertainment.com>
2014-12-10 16:45:21 -0500

    Make sure client_entities deletes everything about an authored entity (copy-paste to the rescue!  Tony to factor sensibly, later).

--- source/stonehearth_data/mods/radiant/modules/client_entities.lua
+++ source/stonehearth_data/mods/radiant/modules/client_entities.lua
@@ -18,6 +18,42 @@ end
 
 function client_entities.destroy_entity(entity)
    if entity and entity:is_valid() then
+      -- destroy all the children when destorying the parent.  should we do
+      -- this from c++?  if not, entities which get destroyed from the cpp
+      -- layer won't get this behavior.  maybe that's just impossible (i.e. forbid
+      -- it from happening, since the cpp layer knowns nothing about game logic?)
+      local ec = entity:get_component('entity_container')
+      if ec then
+         -- Copy the blueprint's (container's) children into a local var first, because
+         -- _set_teardown_recursive could cause the entity container to be invalidated.
+         local ec_children = {}
+         for id, child in ec:each_child() do
+            ec_children[id] = child
+         end         
+         for id, child in pairs(ec_children) do
+            client_entities.destroy_entity(child)
+         end
+      end
+      --If we're the big one, destroy the little and ghost one
+      local entity_forms = entity:get_component('stonehearth:entity_forms')
+      if entity_forms then
+         local iconic_entity = entity_forms:get_iconic_entity()
+         if iconic_entity then
+             _radiant.client.destroy_authoring_entity(iconic_entity)
+         end
+         local ghost_entity = entity_forms:get_ghost_entity()
+         if ghost_entity then
+             _radiant.client.destroy_authoring_entity(ghost_entity)
+         end
+      end
+      --If we're the little one, call destroy on the big one and exit
+      local iconic_component = entity:get_component('stonehearth:iconic_form')
+      if iconic_component then
+         local full_sized = iconic_component:get_root_entity()
+         client_entities.destroy_entity(full_sized)
+         return 
+      end
+
       _radiant.client.destroy_authoring_entity(entity:get_id())
    end
 end
