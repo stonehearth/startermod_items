3d4f6a69040dcfa64965a31b903238bd904499d3
Tony Cannon <tony@radiant-entertainment.com>
2014-12-15 13:26:54 -0800

    quantity is kinda broken.  ignore for now.

--- source/stonehearth_data/mods/stonehearth/services/server/farming/farming_service.lua
+++ source/stonehearth_data/mods/stonehearth/services/server/farming/farming_service.lua
@@ -113,7 +113,9 @@ end
 function FarmingService:add_crop_type(session, new_crop_type, quantity)
    for i, crop_data in ipairs(self:_get_crop_list(session)) do
       if crop_data.crop_type == new_crop_type then
-         crop_data.quantity = crop_data.quantity + quantity
+         -- ideally we would do this, but no one uses quantity and there are
+         -- some errors in writing it (sometimes crop_data.quantity is nil...)
+         -- crop_data.quantity = crop_data.quantity + quantity
          return
       end
    end
