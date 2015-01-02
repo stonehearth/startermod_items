67f71d7d3dfcb36a54493b8be5dae9aeab749be0
Chris Klochek <klochek@radiant-entertainment.com>
2015-01-02 18:47:14 -0500

    Trivial cleanup.

--- source/stonehearth_data/mods/stonehearth/call_handlers/dynamic_scenario_call_handler.lua
+++ source/stonehearth_data/mods/stonehearth/call_handlers/dynamic_scenario_call_handler.lua
@@ -2,12 +2,12 @@ local DynamicScenarioCallHandler = class()
 
 function DynamicScenarioCallHandler:spawn_scenario(session, request, scenario_uri) 
    stonehearth.dynamic_scenario:force_spawn_scenario(scenario_uri)
-   return true;
+   return true
 end
 
 function DynamicScenarioCallHandler:cl_spawn_scenario(session, request, scenario_uri) 
    _radiant.call('stonehearth:spawn_scenario', scenario_uri)
-   return true;
+   return true
 end
 
 return DynamicScenarioCallHandler
