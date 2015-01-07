a649553da668e30bc8454d43722e641a8f78eba0
Stephanie <stephanie@radiant-entertainment.com>
2015-01-06 17:15:14 -0800

    autotest helpers

--- source/stonehearth_data/mods/stonehearth_autotest/tests/end_to_end/farming_autotests.lua
+++ source/stonehearth_data/mods/stonehearth_autotest/tests/end_to_end/farming_autotests.lua
@@ -19,7 +19,7 @@ function farming_tests.grow_one_turnip(autotest)
       end)
    
    -- create a 1x1 farm
-   autotest.ui:click_dom_element('#startMenu div[hotkey="f"]')
+   autotest.ui:click_dom_element('#startMenu #create_farm') --div[hotkey="f"]')
    autotest.ui:set_next_designation_region(4, 8, 1, 1)
 
    -- when the farm widget pops up, pick the turnip crop
