c955df221e8980f4a857bd22ed10a8984d5fbb9d
Albert Wang <albert@radiant-entertainment.com>
2015-01-20 11:16:52 -0800

    Update mining_test.

--- source/stonehearth_data/mods/stonehearth_tests/mining_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/mining_test.lua
@@ -6,13 +6,16 @@ local Region3 = _radiant.csg.Region3
 local MiningTest = class(MicroWorld)
 
 function MiningTest:__init()
-   self[MicroWorld]:__init(64)
+   self[MicroWorld]:__init(256)
    self:create_world()
+
+   --self:_ore_test()
 
    local session = self:get_session()
 
    self:place_item_cluster('stonehearth:resources:wood:oak_log', 10, 0, 4, 4)
-   self:place_item_cluster('stonehearth:decoration:wooden_garden_lantern', 10, 5, 2, 2)
+   self:place_item_cluster('stonehearth:furniture:comfy_bed', 10, 5, 2, 2)
+   self:place_item_cluster('stonehearth:decoration:wooden_garden_lantern', 0, 5, 2, 2)
    self:place_item('stonehearth:decoration:firepit', 10, 8)
    self:place_item('stonehearth:small_boulder', -1, -1)
 
@@ -22,6 +25,8 @@ function MiningTest:__init()
    local citizen = self:place_citizen(15, 6)
    local citizen = self:place_citizen(15, 8)
    local citizen = self:place_citizen(15, 10)
+   local citizen = self:place_citizen(15, 12)
+   local citizen = self:place_citizen(15, 14)
 
    local point = Point3(20, 9, 4)
    local region = Region3(Cube3(point, point + Point3(1, 1, 1)))
@@ -32,6 +37,12 @@ function MiningTest:__init()
       region:translate(Point3(4, 0, 0))
       stonehearth.mining:dig_out(session.player_id, region)
    end
+end
+
+function MiningTest:_ore_test()
+   local json = radiant.resources.load_json('/stonehearth/scenarios/static/terrain/gold_vein')
+   local ore_network = stonehearth.mining:create_ore_network(Point3(0, 15, 0), "gold_ore", json.data)
+   radiant.terrain.add_region(ore_network)
 end
 
 return MiningTest
