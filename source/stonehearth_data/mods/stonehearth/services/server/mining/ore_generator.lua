b1bfe383a8469de1c5ec5a538cd7f2fd65d88e4a
Albert Wang <albert@radiant-entertainment.com>
2015-01-22 17:30:16 -0800

    Remove vertical jiggle from ore veins.

--- source/stonehearth_data/mods/stonehearth/services/server/mining/ore_generator.lua
+++ source/stonehearth_data/mods/stonehearth/services/server/mining/ore_generator.lua
@@ -58,27 +58,28 @@ function ore_generator._create_mineral_vein(block_type, json, rng)
    -- chooses a principal axis for the vein to follow
    local direction, normal = ore_generator._get_random_direction(rng)
 
-   local segment_start = Point3(0, 2, 0)
-   local segment, segment_end
+   local vein_offset_y = rng:get_int(1, 2)
+   local start = Point3(0, vein_offset_y, 0)
+   local segment, finish
 
    if has_kink then
       local half_min_length = math.floor(json.vein_length_min*0.5)
       local kink_index = rng:get_int(half_min_length, length)
       local kink_length = rng:get_int(12, half_min_length)
 
-      segment, segment_end = ore_generator._create_vein_segment(segment_start, kink_index, direction, normal, vein_radius, block_type, rng)
-      region:add_region(segment)
+      segment, finish = ore_generator._create_vein_segment(start, kink_index, direction, normal, vein_radius, block_type, false)
+      region:add_cube(segment)
 
-      segment_start = segment_end
-      segment, segment_end = ore_generator._create_vein_segment(segment_start, kink_length, normal, direction, vein_radius, block_type, rng)
-      region:add_region(segment)
+      start = finish
+      segment, finish = ore_generator._create_vein_segment(start, kink_length, normal, direction, vein_radius, block_type, true)
+      region:add_cube(segment)
 
-      segment_start = segment_end
-      segment, segment_end = ore_generator._create_vein_segment(segment_start, length - kink_index, direction, normal, vein_radius, block_type, rng)
-      region:add_region(segment)
+      start = finish
+      segment, finish = ore_generator._create_vein_segment(start, length - kink_index, direction, normal, vein_radius, block_type, true)
+      region:add_cube(segment)
    else
-      segment, segment_end = ore_generator._create_vein_segment(segment_start, length, direction, normal, vein_radius, block_type, rng)
-      region:add_region(segment)
+      segment, finish = ore_generator._create_vein_segment(start, length, direction, normal, vein_radius, block_type, false)
+      region:add_cube(segment)
    end
 
    return region
@@ -93,70 +94,17 @@ function ore_generator._get_random_direction(rng)
    return direction, normal
 end
 
-function ore_generator._create_vein_segment(start_location, length, direction, normal, vein_radius, block_type, rng)
-   local horizontal_drift_chance = 0.20
-   local vertical_drift_chance = 0.20
-   local min_drift_interval = 2
-   local last_drift = -min_drift_interval
-   local region = Region3()
-   local location = start_location
-
-   local slice = ore_generator._create_vein_slice(location, normal, vein_radius, block_type)
-   region:add_cube(slice)
-
-   for i=2, length do
-      location = location + direction
-
-      if i - last_drift >= min_drift_interval and
-         i <= length - min_drift_interval then
-         -- horizontal perturbation
-         -- if rng:get_real(0, 1) < horizontal_drift_chance then
-         --    local sn = rng:get_int(0, 1) * 2 - 1
-         --    location = location + normal*sn
-         --    last_drift = i
-         -- end
-
-         -- vertical perturbation
-         if rng:get_real(0, 1) < vertical_drift_chance then
-            local dy = rng:get_int(0, 1) * 2 - 1
-            local elevation = location.y
-            local new_elevation = elevation + dy
-            -- keep the vein in the same slice since it's hard to mine up
-            if ore_generator._stays_within_slice(elevation, new_elevation) then
-               location.y = new_elevation
-               last_drift = i
-            end
-         end
-      end
-
-      slice = ore_generator._create_vein_slice(location, normal, vein_radius, block_type)
-      region:add_cube(slice)
+function ore_generator._create_vein_segment(start, length, direction, normal, vein_radius, block_type, connect)
+   local finish = start + direction * (length-1)
+   if connect then
+      -- back up to fill the void in the corner
+      start = start - direction * vein_radius
    end
 
-   return region, location
-end
-
-function ore_generator._create_vein_slice(location, normal, radius, block_type)
-   local p0 = location - normal * radius
-   local p1 = location + normal * radius
-   return csg_lib.create_cube(p0, p1, block_type)
-end
-
-function ore_generator._stays_within_slice(elevation, new_elevation)
-   local y_cell_size = constants.mining.Y_CELL_SIZE
-   local current_slice = math.floor(elevation / y_cell_size)
-   local new_slice = math.floor(new_elevation / y_cell_size)
-
-   if new_slice ~= current_slice then
-      return false
-   end
-
-   -- exclude the ceiling from the slice since it's hard to mine ceilings
-   if new_elevation % y_cell_size == y_cell_size - 1 then
-      return false
-   end
-
-   return true
+   local corner1 = start - normal * vein_radius
+   local corner2 = finish + normal * vein_radius
+   local segment = csg_lib.create_cube(corner1, corner2, block_type) 
+   return segment, finish
 end
 
 return ore_generator
