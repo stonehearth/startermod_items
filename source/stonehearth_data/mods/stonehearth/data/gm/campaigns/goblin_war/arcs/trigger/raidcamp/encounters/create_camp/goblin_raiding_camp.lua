39946a33d848bf1c737242d12a9681c4188d571b
Tom Cannon <tom@radiant-entertainment.com>
2015-02-04 15:05:10 -0800

    Fix some typs and add gear to the goblin leader

--- source/stonehearth_data/mods/stonehearth/data/gm/campaigns/goblin_war/arcs/trigger/raidcamp/encounters/create_camp/goblin_raiding_camp.lua
+++ source/stonehearth_data/mods/stonehearth/data/gm/campaigns/goblin_war/arcs/trigger/raidcamp/encounters/create_camp/goblin_raiding_camp.lua
@@ -5,8 +5,19 @@ function GoblinRaidingCampScript:start(ctx)
    local boss = ctx.npc_boss_entity
 
    assert(boss and boss:is_valid())
-   
-   radiant.log.write('', 0, 'add customization here!')   
+
+   -- scale the boss up
+   local render_info = boss:get_component('render_info')
+   assert (render_info)
+   render_info:set_scale(.120)
+
+   -- give him the boss gear
+   local crown = radiant.entities.create_entity('stonehearth:monsters:goblins:armor:miniboss_crown')
+   radiant.entities.equip_item(boss, crown)
+
+   -- rename the boss 
+   radiant.entities.set_name(boss, 'Leader Gragg the Magnificant')
+   -- XXX, make this a boss name lookup table
 end
 
 return GoblinRaidingCampScript
