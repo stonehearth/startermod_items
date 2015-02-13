f5ac439560d15a5a56bf7b55719c637648ab677d
Stephanie <stephanie@radiant-entertainment.com>
2015-02-12 16:41:25 -0800

    leases can be nil on load?

--- source/stonehearth_data/mods/stonehearth/components/lease/lease_component.lua
+++ source/stonehearth_data/mods/stonehearth/components/lease/lease_component.lua
@@ -18,7 +18,7 @@ end
 function LeaseComponent:_remove_nonpersistent_leases()
    for _, leases in pairs(self._sv.factions) do
       for lease_name, info in pairs(leases) do
-         if not info.persistent then
+         if not info.persistent and self._sv.leases then
             self._sv.leases[lease_name] = nil
          end
       end
