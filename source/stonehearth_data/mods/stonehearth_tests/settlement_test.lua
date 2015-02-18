9816dea0aa1a2b5906b59408d942aeabd7ff79fa
Tom Cannon <tom@radiant-entertainment.com>
2015-02-18 08:16:00 -0800

    Add more footment to the test

--- source/stonehearth_data/mods/stonehearth_tests/settlement_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/settlement_test.lua
@@ -11,6 +11,10 @@ function SettlementTest:__init()
    -- build the player camp
    self:place_citizen(4, 4, 'footman')
    self:place_citizen(4, 7, 'footman')
+   self:place_citizen(4, 7, 'footman')
+   self:place_citizen(4, 7, 'footman')
+   self:place_citizen(4, 7, 'footman')
+   self:place_citizen(4, 7, 'footman')
    self:place_citizen(7, 4)
    self:place_citizen(7, 7)
 
