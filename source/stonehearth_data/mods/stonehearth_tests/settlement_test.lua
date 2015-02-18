28a98349ec86ee73267417401a2804d1ae734bdc
Tom Cannon <tom@radiant-entertainment.com>
2015-02-18 08:19:11 -0800

    Fix merge error

--- source/stonehearth_data/mods/stonehearth_tests/settlement_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/settlement_test.lua
@@ -11,15 +11,8 @@ function SettlementTest:__init()
    -- build the player camp
    self:place_citizen(4, 4, 'footman')
    self:place_citizen(4, 7, 'footman')
-<<<<<<< HEAD
-   self:place_citizen(4, 7, 'footman')
-   self:place_citizen(4, 7, 'footman')
-   self:place_citizen(4, 7, 'footman')
-   self:place_citizen(4, 7, 'footman')
-=======
    self:place_citizen(10, 7, 'footman')
    self:place_citizen(13, 7, 'footman')
->>>>>>> af359bf2e131f0fd2a8f4dde082e3863416c2bdb
    self:place_citizen(7, 4)
    self:place_citizen(7, 7)
 
