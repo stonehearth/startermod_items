9eee536b5a3275534a698c69d2cfe7986b6535a9
Tony Cannon <tony@radiant-entertainment.com>
2015-02-24 09:42:27 -0800

    spawn thieves at night only!

---
+++ source/stonehearth_data/mods/stonehearth/services/server/game_master/controllers/encounters/wait_for_time_of_day_encounter.lua
@@ -0,0 +1,30 @@
+
+local WaitForTimeOfDayEncounter = class()
+
+function WaitForTimeOfDayEncounter:initialize()
+   self._log = radiant.log.create_logger('game_master.encounters.wait_for_time_of_day')
+end
+
+function WaitForTimeOfDayEncounter:start(ctx, info)
+   assert(info.duration)
+
+   local time = info.time
+   self._sv.time = time
+   self.__saved_variables:mark_changed()
+
+   self._log:info('setting wait_for_time_of_day timer for %s', tostring(time))
+   self._timer = stonehearth.calendar:set_alarm(time, function()
+         self._timer = nil
+         ctx.arc:trigger_next_encounter(ctx)
+      end)
+end
+
+function WaitForTimeOfDayEncounter:stop()
+   if self._timer then
+      self._timer:destroy()
+      self._timer = nil
+   end
+end
+
+return WaitForTimeOfDayEncounter
+
