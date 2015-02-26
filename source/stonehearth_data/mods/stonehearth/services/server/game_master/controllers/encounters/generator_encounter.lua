88bb93722ee78550fbd75f52fabad31c9f4b8f8a
Tony Cannon <tony@radiant-entertainment.com>
2015-02-26 08:49:44 -0800

    stop generators from generating way too much!

--- source/stonehearth_data/mods/stonehearth/services/server/game_master/controllers/encounters/generator_encounter.lua
+++ source/stonehearth_data/mods/stonehearth/services/server/game_master/controllers/encounters/generator_encounter.lua
@@ -55,6 +55,10 @@ function GeneratorEncounter:_start_timer()
    local delay = self._sv.delay
    
    self._log:info('setting generator timer for %s', tostring(delay))  
+
+   if self._timer then
+      self._timer:destroy()
+   end
    self._timer = stonehearth.calendar:set_timer(delay, function()
          self:_spawn_encounter()
       end)
@@ -72,6 +76,15 @@ function GeneratorEncounter:_start_source_listener()
       end)
 end
 
+-- debug commands sent by the ui
+
+function GeneratorEncounter:trigger_now_cmd(session, response)
+   local ctx = self._sv.ctx
+
+   self._log:info('spawning encounter now as requested by ui')
+   self:_spawn_encounter();
+   return true
+end
 
 return GeneratorEncounter
 
