cb5afe9a30ec9c523a0835a762d7c40e20c8fc40
Tony Cannon <tony@radiant-entertainment.com>
2015-04-06 09:15:54 -0700

    method to get the tag at a point in the region.

--- source/csg/region.h
+++ source/csg/region.h
@@ -63,6 +63,7 @@ public:
    Cube GetBounds() const;
    Point GetCentroid() const;
 
+   int GetTag(Point const& pt) const;
    int GetRectCount() const { return (int)cubes_.size(); }
    Cube GetRect(int i) const { return cubes_[i]; }
    int GetCubeCount() const { return (int)cubes_.size(); }
