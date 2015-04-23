ad7761a1a848604c2b6b2356d9b84c9c609f04bf
Tony Cannon <tony@radiant-entertainment.com>
2015-04-23 15:32:31 -0700

    implement attack-move and use it in a new "pillage" mission.
    "pillage" means run around the town and let your ambient combat
    behavior take over.

--- source/stonehearth_data/mods/stonehearth/services/server/game_master/controllers/missions/mission.lua
+++ source/stonehearth_data/mods/stonehearth/services/server/game_master/controllers/missions/mission.lua
@@ -4,6 +4,79 @@ local Point3 = _radiant.csg.Point3
 
 local Mission = class()
 
+function Mission:activate()
+   if self._sv.sighted_bulletin_data then
+      self:_listen_for_sighted()
+   end
+end
+
+function Mission:can_start(ctx, info)
+   return true
+end
+
+function Mission:start(ctx, info)
+   self._sv.ctx = ctx
+   self._sv.info = info
+
+   self._sv.party = self:_create_party(ctx, info)
+
+   if info.sighted_bulletin then
+      self._sv.sighted_bulletin_data = info.sighted_bulletin
+      self.__saved_variables:mark_changed()
+      self:_listen_for_sighted()
+   end
+end
+
+function Mission:stop()
+   if self._sighted_listener then
+      self._sighted_listener:destroy()
+      self._sighted_listener = nil
+   end
+
+   local bulletin = self._sv.sighted_bulletin
+   if bulletin then
+      stonehearth.bulletin_board:remove_bulletin(bulletin)
+      self._sv.sighted_bulletin = nil
+      self.__saved_variables:mark_changed()
+   end      
+end
+
+function Mission:_listen_for_sighted()
+   local ctx = self._sv.ctx
+   local population = stonehearth.population:get_population(ctx.player_id)
+   if population then
+      self._sighted_listener = radiant.events.listen(population, 'stonehearth:population:new_threat', self, self._on_player_new_threat)
+   end
+end
+
+function Mission:_on_player_new_threat(evt)
+   local party = self._sv.party
+   if party and not self._sv.sighted_bulletin then
+      for id, member in party:each_member() do
+         if id == evt.entity_id then
+            self:_create_sighted_bulletin(member)
+            self._sighted_listener:destroy()
+            self._sighted_listener = nil
+            break
+         end
+      end
+   end
+end
+
+function Mission:_create_sighted_bulletin(party_member)
+   if not self._sv.sighted_bulletin then
+      --Send the notice to the bulletin service.
+      local player_id = self._sv.ctx.player_id
+      local bulletin_data = self._sv.sighted_bulletin_data
+      bulletin_data.zoom_to_entity = party_member
+
+      self._sv.sighted_bulletin = stonehearth.bulletin_board:post_bulletin(player_id)
+           :set_type('alert')
+           :set_data(bulletin_data)
+      self.__saved_variables:mark_changed()
+   end
+end
+
 function Mission:_create_party(ctx, info)
    assert(ctx)
    assert(ctx.enemy_location)
