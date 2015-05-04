6911ae4ba0eb411083b499301f554c0e5e5b8ee1
Chris Klochek <klochek@radiant-entertainment.com>
2015-05-04 12:43:21 -0400

    Don't include water geometry in lod (yet?)

--- source/horde3d/Source/Horde3DEngine/egRenderer.h
+++ source/horde3d/Source/Horde3DEngine/egRenderer.h
@@ -260,7 +260,7 @@ protected:
    void drawProjections(SceneId sceneId, std::string const& shaderContext, uint32 userFlags );
    void prioritizeLights(SceneId sceneId, std::vector<LightNode*> *lights);
 	void doForwardLightPass(SceneId sceneId, std::string const& contextSuffix,
-	                        bool noShadows, RenderingOrder::List order, int occSet, bool selectedOnly );
+	                        bool noShadows, RenderingOrder::List order, int occSet, bool selectedOnly, int lodLevel=-1 );
 	void doDeferredLightPass(SceneId sceneId, bool noShadows, MaterialResource* deferredMaterial);
 	
 	void drawRenderables(SceneId sceneId, std::string const& shaderContext, bool debugView,
