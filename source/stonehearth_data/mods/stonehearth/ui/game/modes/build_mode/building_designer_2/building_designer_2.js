d09e7b5b52dde7b5d527dff43f197b6272fb5daf
Tony Cannon <tony@radiant-entertainment.com>
2015-05-05 10:01:15 -0700

    checkpoint of draw voxel tools.  still disabled.

--- source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_designer_2/building_designer_2.js
+++ source/stonehearth_data/mods/stonehearth/ui/game/modes/build_mode/building_designer_2/building_designer_2.js
@@ -40,6 +40,11 @@ App.StonehearthBuildingDesignerTools = App.StonehearthBuildingDesignerBaseTools.
       this.newTool(new GrowWallsTool);
       this.newTool(new GrowRoofTool);
       this.newTool(new DrawRoadTool);
+
+      this.newTool(new DrawVoxelTool({ toolId: 'voxelAdd', mode:'add' }));
+      this.newTool(new DrawVoxelTool({ toolId: 'voxelPaint', mode:'paint' }));
+      this.newTool(new DrawVoxelTool({ toolId: 'voxelRemove', mode:'erase' }));
+
       // this.newTool(new DrawSlabTool);
 
       // Make sure we call super after adding all the tools!
