ce1aa72c233c4b675d622b693888b640139adada
radiantTom <tom@radiant-entertainment.com>
2015-06-04 16:53:56 -0700

    more cook asset refinement

--- source/stonehearth_data/mods/stonehearth_tests/cook_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/cook_test.lua
@@ -7,12 +7,13 @@ function CookTest:__init()
    self[MicroWorld]:__init()
    self:create_world()
 
-   self:place_citizen(-12, 4)
+   self:place_citizen(-12, 4, 'farmer')
    local cook = self:place_citizen(-12, 7, 'cook')
    local player_id = radiant.entities.get_player_id(cook)
 
    -- put some items in the world
    self:place_item_cluster('stonehearth:resources:stone:hunk_of_stone', -10, 0, 2, 2, player_id)
+   self:place_item('stonehearth:cook:talisman', 4, 1, player_id)
 end
 
 return CookTest
