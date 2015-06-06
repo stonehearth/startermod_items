573cfb5f3c66d4eb6ae918c50d56811b5f86fe6a
Yang Shan <yang@radiant-entertainment.com>
2015-06-05 19:08:11 -0700

    Adding a description to all the commands. Unregistering object browser from events after destroy - this fixes javascript errors when object browser is closed.

--- source/stonehearth_data/mods/stonehearth/ui/debug/console/console_commands.js
+++ source/stonehearth_data/mods/stonehearth/ui/debug/console/console_commands.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
          cmdobj.setContent(commands);
          return;
       },
-      description : "Prints out all the registered console commands"
+      description : "Prints out all the registered console commands."
    });
 
    // wire up a generic handler to call any registered route.
@@ -23,6 +23,7 @@ $(document).ready(function(){
          var cmdargs = args.slice(1);
          return radiant.callv(cmd, cmdargs).deferred
       },
+      description : "A generic handler to call any registered route."
    });
 
    radiant.console.register('show_pathfinder_time', {
@@ -41,7 +42,8 @@ $(document).ready(function(){
             entity = selected;
          }
          return radiant.call('stonehearth:destroy_entity', entity);
-      }
+      },
+      description : "Destroy an entity. Arg 0 is id of the entity. If no argument is provided, destroys the currently selected entity. Usage: destroy 12345"
    });
 
    radiant.console.register('kill', {
@@ -53,7 +55,8 @@ $(document).ready(function(){
             entity = selected;
          }
          return radiant.call('stonehearth:kill_entity', entity);
-      }
+      },
+      description : "Kill an entity. Arg 0 is id of the entity. If no argument is provided, kills the currently selected entity. Usage: kill 12345"
    });
 
    // Usage: select 12345  # enity id
@@ -62,6 +65,7 @@ $(document).ready(function(){
          var entity = 'object://game/' + args[0]
          return radiant.call('radiant:client:select_entity', entity);
       },
+      description : "Selects the entity with id = Arg 0. Usage: select 12345"
    });
 
    // Usage: get_config foo.bar.baz
@@ -70,6 +74,7 @@ $(document).ready(function(){
          var key = args[0]
          return radiant.call('radiant:get_config', key);
       },
+      description : "Usage: get_config foo.bar.baz"
    });
 
    // Usage: set_config foo.bar.baz { value = 1 }
@@ -79,6 +84,7 @@ $(document).ready(function(){
          var value = JSON.parse(args[1]);
          return radiant.call('radiant:set_config', key, value);
       },
+      description : "Usage: set_config foo.bar.baz { value = 1 }"
    });
 
    radiant.console.register('ib', {
@@ -86,6 +92,7 @@ $(document).ready(function(){
          var building = args[0] || selected;
          return radiant.call_obj('stonehearth.build', 'instabuild_command', building);
       },
+      description : "Instantly builds the selected building, or arg 0. Usage: ib object://game/12345"
    });
 
    radiant.console.register('get_cost', {
@@ -93,25 +100,29 @@ $(document).ready(function(){
          var building = args[0] || selected;
          return App.stonehearthClient.getCost(building);
       },
+      description : "Get the cost of the selected building, or arg 0. Usage: get_cost object://game/12345"
    });
 
    radiant.console.register('query_pf', {
       call: function(cmdobj, fn, args) {
          return radiant.call_obj('stonehearth.selection', 'query_pathfinder_command');
       },
+      description : "Runs the query pathfinder command. No arguments."
    });
 
    radiant.console.register('spawn_scenario', {
       call: function(cmdobj, fn, args) {
          var scenario_uri = args[0];
          return App.stonehearthClient.spawnScenario(scenario_uri);
-      }
+      },
+      description : "Spawns the specified scenario uri. Usage: spawn_scenario stonehearth:quests:collect_starting_resources"
    });
 
    radiant.console.register('set_time', {
       call: function(cmdobj, fn, args) {
          var time = JSON.parse(args[0]);
          return App.stonehearthClient.setTime(time);
-      }
+      },
+      description : "Sets the time with options passed in through Arg 0 (Note you can't have any spaces and you have to include the quotes so is valid JSON). Usage: set_time {\"hour\":12,\"minute\":0}"
    });
 });
