06cbf2c8e6947132d6f5d0d7528fe7a51a546c0d
Albert Wang <albert@radiant-entertainment.com>
2015-06-09 16:19:39 -0700

    Fix bug #328 - Harvesting berries/plants causes workers to drop their currently held item.

--- source/stonehearth_data/mods/stonehearth/ai/actions/harvest_plant.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/harvest_plant.lua
@@ -12,9 +12,11 @@ HarvestPlant.version = 2
 HarvestPlant.priority = 1
 
 function HarvestPlant:start_thinking(ai, entity, args)
-   local resource_node_component = args.plant:get_component('stonehearth:renewable_resource_node')
-   if resource_node_component and resource_node_component:is_harvestable() then
-      ai:set_think_output()
+   if not ai.CURRENT.carrying then
+      local resource_node_component = args.plant:get_component('stonehearth:renewable_resource_node')
+      if resource_node_component and resource_node_component:is_harvestable() then
+         ai:set_think_output()
+      end
    end
 end
 
