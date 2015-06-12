7e7ed91f0008ecf4ec564731714829a806d6d006
radiantTom <tom@radiant-entertainment.com>
2015-06-11 18:53:07 -0700

    more cook assets, and the stew recipes

--- source/stonehearth_data/mods/stonehearth_tests/cook_test.lua
+++ source/stonehearth_data/mods/stonehearth_tests/cook_test.lua
@@ -7,13 +7,20 @@ function CookTest:__init()
    self[MicroWorld]:__init()
    self:create_world()
 
-   self:place_citizen(-12, 4, 'farmer')
+   local farmer = self:place_citizen(-12, 4, 'farmer')
    local cook = self:place_citizen(-12, 7, 'cook')
    local player_id = radiant.entities.get_player_id(cook)
 
    -- put some items in the world
    self:place_item_cluster('stonehearth:resources:stone:hunk_of_stone', -10, 0, 2, 2, player_id)
+
+   self:place_item_cluster('stonehearth:carrot_basket', -10, 10, 2, 2, player_id)
+   self:place_item_cluster('stonehearth:turnip_basket', -10, 8, 2, 2, player_id)
+   self:place_item_cluster('stonehearth:food:pumpkin:pumpkin_basket', -10, 6, 2, 2, player_id)
+   self:place_item_cluster('stonehearth:rabbit_jerky', -10, 4, 2, 2, player_id)
+
    self:place_item('stonehearth:cook:talisman', 4, 1, player_id)
+
 end
 
 return CookTest
