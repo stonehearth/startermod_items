b1ce3c1e7b87a485ebf9cf8c11b9a9e7ed9621a2
Chris Klochek <klochek@radiant-entertainment.com>
2015-06-16 18:00:55 -0400

    Give the storage component the ability to track items (that are given to it).  Make stockpiles and crates use it.  Hook _that_ up to the UI.

--- source/stonehearth_data/mods/stonehearth/components/backpack/backpack_component.lua
+++ source/stonehearth_data/mods/stonehearth/components/backpack/backpack_component.lua
@@ -87,6 +87,7 @@ function BackpackComponent:add_item(item)
       local player_id = radiant.entities.get_player_id_from_entity(self._entity)
       stonehearth.inventory:get_inventory(player_id):add_item(item)
       stonehearth.inventory:get_inventory(player_id):update_item_container(id, self._entity)
+      self._entity:get_component('stonehearth:storage'):add_item_for_tracking(item)
       self._sv.items[id] = item
       self._sv.num_items = self._sv.num_items + 1
       self.__saved_variables:mark_changed()
@@ -103,6 +104,7 @@ function BackpackComponent:remove_item(item)
       local player_id = radiant.entities.get_player_id_from_entity(self._entity)
       stonehearth.inventory:get_inventory(player_id):remove_item(item)
       stonehearth.inventory:get_inventory(player_id):update_item_container(id, nil)
+      self._entity:get_component('stonehearth:storage'):remove_item_from_tracking(id)
       self._sv.items[id] = nil
       self._sv.num_items = self._sv.num_items - 1
       self.__saved_variables:mark_changed()
