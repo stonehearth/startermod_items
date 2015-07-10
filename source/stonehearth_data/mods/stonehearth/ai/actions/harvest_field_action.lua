3e8bbb98619cd61d2d21283214e7a07e492c0962
Tony Cannon <tony@radiant-entertainment.com>
2015-07-09 17:29:56 -0700

    BFS farming!  No more task-per-field.
    If this works out well, apply it EVERYWHERE!!  For now, just
    test this one case.

--- source/stonehearth_data/mods/stonehearth/ai/actions/harvest_field_action.lua
+++ source/stonehearth_data/mods/stonehearth/ai/actions/harvest_field_action.lua
@@ -4,19 +4,28 @@ local Entity = _radiant.om.Entity
 local HarvestField = class()
 HarvestField.name = 'harvest field'
 HarvestField.status_text = 'harvesting field'
-HarvestField.does = 'stonehearth:harvest_field'
-HarvestField.args = {
-   field = FarmerFieldComponent
-}
+HarvestField.does = 'stonehearth:simple_labor'
+HarvestField.args = {}
 HarvestField.version = 2
-HarvestField.priority = 1
+HarvestField.priority = stonehearth.constants.priorities.simple_labor.HARVEST_FIELD
+
+local function harvest_layer_filter(entity)
+   return entity:get_uri() == 'stonehearth:farmer:field_layer:harvestable'
+end
 
 local ai = stonehearth.ai
 return ai:create_compound_action(HarvestField)
-         :execute('stonehearth:goto_entity', { entity = ai.ARGS.field:get_harvestable_layer() })
-         :execute('stonehearth:reserve_entity_destination', { entity = ai.ARGS.field:get_harvestable_layer(),
+         :execute('stonehearth:find_path_to_entity_type', {
+                  filter_fn = harvest_layer_filter,
+                  description = 'find harvest layer',
+               })
+         :execute('stonehearth:goto_entity', { entity = ai.PREV.destination })
+         :execute('stonehearth:reserve_entity_destination', { entity = ai.BACK(2).destination,
                                                               location = ai.PREV.point_of_interest })
-         :execute('stonehearth:harvest_crop_adjacent', { crop = ai.ARGS.field:crop_at(ai.PREV.location) })
+         :execute('stonehearth:harvest_crop_adjacent', { crop = ai.BACK(3).destination
+                                                                     :get_component('stonehearth:farmer_field_layer')
+                                                                        :get_farmer_field()
+                                                                           :crop_at(ai.PREV.location) })
          :execute('stonehearth:trigger_event', {
             source = stonehearth.personality,
             event_name = 'stonehearth:journal_event',
