6b55fe4c0a00fbdd4c6f3b55ba46c7e682eb6820
Stephanie <stephanie@radiant-entertainment.com>
2015-07-09 19:08:21 -0700

    First cut at a usable item tracker for crafters and building

--- source/stonehearth_data/mods/stonehearth/services/server/inventory/inventory.lua
+++ source/stonehearth_data/mods/stonehearth/services/server/inventory/inventory.lua
@@ -32,6 +32,7 @@ function Inventory:initialize(player_id)
    self:add_item_tracker('stonehearth:basic_inventory_tracker')
    self:add_item_tracker('stonehearth:placeable_item_inventory_tracker')
    self:add_item_tracker('stonehearth:sellable_item_tracker')
+   self:add_item_tracker('stonehearth:usable_item_tracker')
 end
 
 function Inventory:activate()
